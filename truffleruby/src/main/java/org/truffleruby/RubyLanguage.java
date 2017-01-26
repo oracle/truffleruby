a1ef2b5efadcb40af12ce2835760aba6994c8271
Chris Seaton <chris.seaton@oracle.com>
2017-01-26 11:54:27 +0000

    Not yet sure of a solution for revision and compile date, but lets not have junk in there for now

--- truffleruby/src/main/java/org/truffleruby/RubyLanguage.java
+++ truffleruby/src/main/java/org/truffleruby/RubyLanguage.java
@@ -47,8 +47,8 @@ public class RubyLanguage extends TruffleLanguage<RubyContext> {
 
     public static final String PLATFORM = "java";
     public static final String RUBY_VERSION = "2.3.1";
-    public static final int    RUBY_REVISION = 54768;
-    public static final String COMPILE_DATE = "2016-12-03";
+    public static final int    RUBY_REVISION = 0;
+    public static final String COMPILE_DATE = "2017";
     // TODO (pitr-ch 11-Jan-2017): use our value when we are recognized by rubygems
     public static final String ENGINE = "ruby";
 
