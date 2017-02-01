2924cf8597854535f9f88d64d8b8105f5073d37c
Benoit Daloze <eregontp@gmail.com>
2017-02-01 14:04:08 +0100

    Remove `mx ruby`, so we have only bin/truffleruby as a launcher

--- mx.jruby/mx_jruby.py
+++ mx.jruby/mx_jruby.py
@@ -63,94 +63,10 @@ class TruffleRubyDocsProject(ArchiveProject):
 
 # Commands
 
-def extractArguments(args):
-    vmArgs = []
-    rubyArgs = []
-    classpath = []
-    print_command = False
-
-    while args:
-        arg = args.pop(0)
-        if arg.startswith('-J') or arg.startswith('-J:'):
-            if arg.startswith('-J-'):
-                arg = arg[2:]
-            elif arg.startswith('-J:'):
-                arg = '-' + arg[3:]
-            if arg == '-cmd':
-                print_command = True
-            elif arg == '-cp' or arg == '-classpath':
-                cp = args.pop(0)
-                classpath.append(cp)
-            else:
-                vmArgs.append(arg)
-        elif arg == '--':
-            rubyArgs.append(arg)
-            rubyArgs.extend(args)
-            break
-        elif arg[0] == '-':
-            rubyArgs.append(arg)
-        else:
-            rubyArgs.append(arg)
-            rubyArgs.extend(args)
-            break
-    return vmArgs, rubyArgs, classpath, print_command
-
-def setup_ruby_home():
-    rubyZip = mx.distribution('RUBY-ZIP').path
-    assert exists(rubyZip)
-    extractPath = join(_suite.dir, 'mxbuild', 'ruby-zip-extracted')
-    if TimeStampFile(extractPath).isOlderThan(rubyZip):
-        if exists(extractPath):
-            shutil.rmtree(extractPath)
-        with tarfile.open(rubyZip, 'r:') as tf:
-            tf.extractall(extractPath)
-    return extractPath
-
-# Print to stderr, mx.log() outputs to stdout
-def log(msg):
-    print >> sys.stderr, msg
-
-# This launcher runs similarly to GraalVM,
-# with a home only containing files extracted from RUBY-ZIP.
-def ruby_command(args):
-    """runs Ruby"""
-    java_home = os.getenv('JAVA_HOME', '/usr')
-    java = os.getenv('JAVACMD', java_home + '/bin/java')
-    argv0 = java
-
-    vmArgs, rubyArgs, user_classpath, print_command = extractArguments(args)
-    classpath = mx.classpath(['TRUFFLE_API', 'RUBY']).split(':')
-    truffle_api, classpath = classpath[0], classpath[1:]
-    assert os.path.basename(truffle_api) == "truffle-api.jar"
-    # Give precedence to graal classpath and VM options
-    classpath = user_classpath + classpath
-    vmArgs = vmArgs + [
-        # '-Xss2048k',
-        '-Xbootclasspath/a:' + truffle_api,
-        '-cp', ':'.join(classpath),
-        'org.truffleruby.Main'
-    ]
-    ruby_home = setup_ruby_home()
-    rubyArgs = [
-        '-Xhome=' + ruby_home,
-        '-Xlauncher=' + join(_suite.dir, 'bin', 'truffleruby')
-    ] + rubyArgs
-    allArgs = vmArgs + rubyArgs
-
-    if print_command:
-        if mx.get_opts().verbose:
-            log('Environment variables:')
-            env = os.environ
-            for key in sorted(env.keys()):
-                log(key + '=' + env[key])
-        log(java + ' ' + ' '.join(map(pipes.quote, allArgs)))
-    return os.execv(java, [argv0] + allArgs)
-
 def ruby_tck(args):
     mx_unittest.unittest(['--verbose', '--suite', 'jruby'])
 
 mx.update_commands(_suite, {
-    'ruby' : [ruby_command, '[ruby args|@VM options]'],
     'rubytck': [ruby_tck, ''],
     'deploy-binary-if-truffle-head': [deploy_binary_if_truffle_head, ''],
 })
