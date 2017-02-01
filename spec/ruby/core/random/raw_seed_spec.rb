61d94124cd91a965421edc2b5a5ec591ff158e6f
Chris Seaton <chris.seaton@oracle.com>
2017-02-01 20:23:56 +0000

    Specify Random.raw_seed

---
+++ spec/ruby/core/random/raw_seed_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding: binary -*-
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Random.raw_seed" do
+  it "returns a String" do
+    Random.raw_seed(1).should be_an_instance_of(String)
+  end
+
+  it "returns a String of the length given as argument" do
+    Random.raw_seed(15).length.should == 15
+  end
+
+  it "returns an ASCII-8BIT String" do
+    Random.raw_seed(15).encoding.should == Encoding::ASCII_8BIT
+  end
+
+  it "returns a random binary String" do
+    Random.raw_seed(12).should_not == Random.raw_seed(12)
+  end
+end
