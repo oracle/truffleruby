e4af30b9b7051e9d7a2d342cf27031d39327607e
Petr Chalupa <git@pitr.ch>
2017-02-20 17:58:23 +0100

    Fix sinatra-server test

--- test/truffle/gems/sinatra-server.sh
+++ test/truffle/gems/sinatra-server.sh
@@ -6,8 +6,4 @@ set -e
 
 GEM_HOME=${GEM_HOME:-lib/ruby/gems/2.3.0}
 
-jt ruby -I$GEM_HOME/gems/rack-1.6.1/lib \
-        -I$GEM_HOME/gems/tilt-2.0.1/lib \
-        -I$GEM_HOME/gems/rack-protection-1.5.3/lib \
-        -I$GEM_HOME/gems/sinatra-1.4.6/lib \
-        test/truffle/gems/sinatra-server/sinatra-server.rb & test_server
+jt ruby test/truffle/gems/sinatra-server/sinatra-server.rb & test_server
