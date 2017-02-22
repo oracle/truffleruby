8af35d6a90f08e28cd48cd634c7a7dc07feecfe6
Brandon Fish <brandon.fish@oracle.com>
2017-02-22 16:53:06 -0600

    Add nokogiri-stubs

---
+++ lib/truffle/nokogiri-stubs.rb
@@ -0,0 +1,6 @@
+puts '[ruby] PATCH applying nokogiri-stubs'
+
+dir = File.join File.dirname(__FILE__),
+                File.basename(__FILE__, '.*')
+$LOAD_PATH.unshift(dir)
+require "#{dir}/nokogiri"
