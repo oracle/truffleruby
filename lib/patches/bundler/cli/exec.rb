b35828163f40f7a00e7fa59c0f0b67a245e69902
Petr Chalupa <git@pitr.ch>
2017-02-24 11:06:43 +0100

    Bundler patch: recognise our shebang

---
+++ lib/patches/bundler/cli/exec.rb
@@ -0,0 +1,15 @@
+module Bundler
+  class CLI::Exec
+    def ruby_shebang?(file)
+      possibilities = [
+          "#!/usr/bin/env ruby\n",
+          "#!/usr/bin/env jruby\n",
+          # TruffleRuby: added item to the array
+          "#!/usr/bin/env truffleruby\n",
+          "#!#{Gem.ruby}\n",
+      ]
+      first_line = File.open(file, "rb") {|f| f.read(possibilities.map(&:size).max) }
+      possibilities.any? {|shebang| first_line.start_with?(shebang) }
+    end
+  end
+end
