abce140a84a2bcd8ef8ca9f8851cdfbe0f221297
Petr Chalupa <git@pitr.ch>
2017-03-02 15:07:29 +0100

    Add patch for gem_dependenci_api adding truffleruby

---
+++ lib/patches/rubygems/request_set/gem_dependency_api.rb
@@ -0,0 +1,12 @@
+class Gem::RequestSet::GemDependencyAPI
+
+  ENGINE_MAP[:ruby] << 'truffleruby'
+  ENGINE_MAP[:ruby_18] << 'truffleruby'
+  ENGINE_MAP[:ruby_19] << 'truffleruby'
+  ENGINE_MAP[:ruby_20] << 'truffleruby'
+  ENGINE_MAP[:ruby_21] << 'truffleruby'
+  ENGINE_MAP[:truffleruby] << %w[truffleruby]
+
+  PLATFORM_MAP[:truffleruby] = Gem::Platform::RUBY
+
+end
