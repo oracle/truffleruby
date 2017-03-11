eb6e446eda55cd1d634e0d616bc18561022d1ea6
Benoit Daloze <eregontp@gmail.com>
2017-03-11 13:57:05 +0100

    Restore "Support the :pgroup option of #spawn"
    * This reverts commit 893db3f0278ae68827dac5792ad765cf75d8f915.

--- truffleruby/src/main/ruby/core/process_mirror.rb
+++ truffleruby/src/main/ruby/core/process_mirror.rb
@@ -195,6 +195,8 @@ module Rubinius
                 elsif value
                   value = Rubinius::Type.coerce_to value, ::Integer, :to_int
                   raise ArgumentError, "negative process group ID : #{value}" if value < 0
+                else
+                  value = -1
                 end
                 @options[key] = value
               when :chdir
