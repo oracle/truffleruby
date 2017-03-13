20e16be9b3da218f385f72a1ed481182d792fbee
Chris Seaton <chris.seaton@oracle.com>
2017-03-13 09:59:27 +0000

    Restore accidentally removed specs

--- spec/ruby/optional/capi/globals_spec.rb
+++ spec/ruby/optional/capi/globals_spec.rb
@@ -175,4 +175,54 @@ describe "CApiGlobalSpecs" do
       @f.rb_output_fs.should == "foo"
     end
   end
+
+  describe "rb_lastline_set" do
+    it "sets the value of $_" do
+      @f.rb_lastline_set("last line")
+      $_.should == "last line"
+    end
+
+    it "sets a Thread-local value" do
+      $_ = nil
+      running = false
+
+      thr = Thread.new do
+        @f.rb_lastline_set("last line")
+        $_.should == "last line"
+        running = true
+      end
+
+      Thread.pass until running
+      $_.should be_nil
+
+      thr.join
+    end
+  end
+
+  describe "rb_lastline_get" do
+    before do
+      @io = StringIO.new("last line")
+    end
+
+    it "gets the value of $_" do
+      @io.gets
+      @f.rb_lastline_get.should == "last line"
+    end
+
+    it "gets a Thread-local value" do
+      $_ = nil
+      running = false
+
+      thr = Thread.new do
+        @io.gets
+        @f.rb_lastline_get.should == "last line"
+        running = true
+      end
+
+      Thread.pass until running
+      $_.should be_nil
+
+      thr.join
+    end
+  end
 end
