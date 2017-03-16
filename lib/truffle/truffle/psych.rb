772d0cfde26d647c07a8d250a812cc4b145e7fd6
Kevin Menard <kevin.j.menard@oracle.com>
2017-03-16 16:00:07 -0400

    Add in missing functionality to Psych required by the safe_yaml gem.

--- lib/truffle/truffle/psych.rb
+++ lib/truffle/truffle/psych.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved. This
+# Copyright (c) 2015, 2017 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
 #
@@ -8,6 +8,8 @@
 
 Psych = Truffle::Psych
 
+require 'psych/visitors'
+
 module Psych
 
   def self.libyaml_version
@@ -15,12 +17,31 @@ module Psych
     [1, 14, 0]
   end
 
+
   class ClassLoader
 
     def path2class(path)
       eval("::#{path}")
     end
 
+  end
+
+  module Visitors
+    class ToRuby
+
+      def build_exception(klass, mesg)
+        klass.new(mesg)
+      end
+
+    end
+
+    class YAMLTree
+
+      def private_iv_get(target, prop)
+        target.instance_variable_get("@#{prop}")
+      end
+
+    end
   end
 
 end
