02eba5a7ba5cf87815889ed04cfd6a2f978ffabc
Kevin Menard <kevin.j.menard@oracle.com>
2017-03-16 15:59:46 -0400

    Fix deserializing an OpenStruct from YAML.

---
+++ lib/patches/ostruct.rb
@@ -0,0 +1,13 @@
+class OpenStruct
+
+  # This is copied from the original OpenStruct definition, but modified to
+  # check if @table is nil before trying to read from it. OpenStrruct aliases
+  # `allocate` to `new`, which normally would ensure @table is initialized.
+  # However, we're unable to accurately retarget `allocate` and thus must
+  # retain the original definition, which leaves @table uninitialized.
+  def respond_to_missing?(mid, include_private = false)
+    mname = mid.to_s.chomp("=").to_sym
+    (@table && @table.key?(mname)) || super
+  end
+
+end
