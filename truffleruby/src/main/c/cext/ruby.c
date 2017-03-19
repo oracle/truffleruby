223934695219511ef9f90fb6e5c4fdaeb230cc86
Chris Seaton <chris.seaton@oracle.com>
2017-03-19 16:19:41 +0000

    Use vasprintf

--- truffleruby/src/main/c/cext/ruby.c
+++ truffleruby/src/main/c/cext/ruby.c
@@ -916,17 +916,10 @@ VALUE rb_str_buf_new(long capacity) {
 
 VALUE rb_vsprintf(const char *format, va_list args) {
   // TODO CS 7-May-17 this needs to use the Ruby sprintf, not C's
-
-  va_list args_copy;
-  va_copy(args_copy, args);
-  int length = vsnprintf(NULL, 0, format, args_copy);
-  //va_end(args_copy); CS 4-May-17 why does this crash?
-  if (length < 0) {
-    rb_tr_error("vsnprintf error");
+  char *buffer;
+  if (vasprintf(&buffer, format, args) < 0) {
+    rb_tr_error("vasprintf error");
   }
-  // TODO CS 4-May-17 allocate a native Ruby string in the first place?
-  char *buffer = malloc(length + 1);
-  vsnprintf(buffer, length + 1, format, args);
   VALUE string = rb_str_new_cstr(buffer);
   free(buffer);
   return string;
