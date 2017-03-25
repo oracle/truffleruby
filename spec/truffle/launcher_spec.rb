d4f0d0079b692b778ead2866a3cd405548df5e8a
Benoit Daloze <eregontp@gmail.com>
2017-03-25 14:39:43 +0100

    Add a few specs for the launcher

--- spec/truffle/launcher_spec.rb
+++ spec/truffle/launcher_spec.rb
@@ -11,14 +11,40 @@ require_relative '../ruby/spec_helper'
 
 describe "bin/truffleruby" do
 
+  before :each do
+    @java_opts = ENV["JAVA_OPTS"]
+  end
+
+  after :each do
+    ENV["JAVA_OPTS"] = @java_opts
+  end
+
   it "runs when symlinked" do
     require "tmpdir"
     Dir.mktmpdir do |path|
       Dir.chdir(path) do
         `ln -s #{RbConfig.ruby} linktoruby`
         `./linktoruby --version`.should include("truffleruby")
+        $?.success?.should == true
       end
     end
+  end
+
+  it "prints the full java command with -J-cmd" do
+    out = `#{RbConfig.ruby} -J-cmd --version`
+    parts = out.split(' ')
+    parts[0].should == "$"
+    parts[1].should == "java"
+    $?.success?.should == true
+  end
+
+  it "adds flags from $JAVA_OPTS to the command" do
+    option = '-Dfoo.bar=baz'
+    ENV["JAVA_OPTS"] = option
+    out = `#{RbConfig.ruby} -J-cmd --version`
+    parts = out.lines[0].split(' ')
+    parts.should include option
+    $?.success?.should == true
   end
 
 end
