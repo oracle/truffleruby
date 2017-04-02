b9c4007855b9ac76f17e4f36258722c54af01063
Brandon Fish <brandon.fish@oracle.com>
2017-04-02 13:02:53 -0500

    Add another Time#zone bad zone example

--- spec/ruby/core/time/zone_spec.rb
+++ spec/ruby/core/time/zone_spec.rb
@@ -81,5 +81,8 @@ describe "Time#zone" do
     with_timezone("1,2") do
       Time.now.utc_offset.should == 0
     end
+    with_timezone("7") do
+      Time.now.utc_offset.should == 0
+    end
   end
 end
