e227559319c261e673e470ff8925dce134c6719b
Chris Seaton <chris.seaton@oracle.com>
2017-04-08 22:07:32 +0100

    Integration test for logging configuration

---
+++ test/truffle/integration/logging.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source test/truffle/common.sh.inc
+
+set -e
+
+rm -f ruby.log
+
+jt ruby -J-Djava.util.logging.config.file=doc/samples/logging.properties -e 14
+
+if [ ! -f ruby.log ]
+then
+    rm -f ruby.log
+    echo No log file produced
+    exit 1
+fi
+
+if ! grep 'ruby home' ruby.log > /dev/null
+then
+    rm -f ruby.log
+    echo Log not as expected
+    exit 1
+fi
+
+rm -f ruby.log
