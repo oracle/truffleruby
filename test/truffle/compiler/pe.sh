f2536c8bea9e5b53d41cdcc807ac8e33330a913d
Chris Seaton <chris.seaton@oracle.com>
2017-04-10 13:53:23 +0100

    Run PE tests with iterative partial escape, because we look at compilation-constant before later partial escape phases are run

--- test/truffle/compiler/pe.sh
+++ test/truffle/compiler/pe.sh
@@ -2,4 +2,4 @@
 
 source test/truffle/common.sh.inc
 
-jt ruby --graal -J-Dgraal.TruffleCompilationExceptionsAreThrown=true -Xbasic_ops.inline=false test/truffle/compiler/pe/pe.rb
+jt ruby --graal -J-Dgraal.TruffleCompilationExceptionsAreThrown=true -J-Dgraal.TruffleIterativePartialEscape=true -Xbasic_ops.inline=false test/truffle/compiler/pe/pe.rb
