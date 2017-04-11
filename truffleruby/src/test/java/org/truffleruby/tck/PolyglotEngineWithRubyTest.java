6e2b93a72eba08077df01de3fcc7077600a5b5e1
Chris Seaton <chris.seaton@oracle.com>
2017-04-11 20:00:28 +0100

    Run PolyglotEngineWithRuby from Truffle as a unit test

---
+++ truffleruby/src/test/java/org/truffleruby/tck/PolyglotEngineWithRubyTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 1.0
+ * GNU General Public License version 2
+ * GNU Lesser General Public License version 2.1
+ */
+package org.truffleruby.tck;
+
+import com.oracle.truffle.tck.impl.PolyglotEngineWithRuby;
+import org.junit.Test;
+
+public class PolyglotEngineWithRubyTest extends PolyglotEngineWithRuby {
+
+    @Test
+    public void testPlaceholder() {
+        // If we don't have any @Tests here, the inherited tests aren't run either
+    }
+
+}
