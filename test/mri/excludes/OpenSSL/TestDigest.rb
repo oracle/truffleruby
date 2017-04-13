96c3b18ecebadc983558424061adcb81c0dd494c
Chris Seaton <chris.seaton@oracle.com>
2017-04-14 00:54:55 +0100

    Un-exclude failing openssl test

--- test/mri/excludes/OpenSSL/TestDigest.rb
+++ test/mri/excludes/OpenSSL/TestDigest.rb
@@ -1,4 +1,3 @@
 exclude :test_098_features, "needs investigation"
 exclude :test_digest, "needs investigation"
-exclude :test_digest_constants, "needs investigation"
 exclude :test_info, "needs investigation"
