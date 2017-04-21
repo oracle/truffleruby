72ba2b3af963343416180b2a308761a21258b375
Duncan MacGregor <duncan.macgregor@oracle.com>
2017-04-21 14:27:24 +0100

    Tidy up RubiniusLastStringReadNode by reomving unused method.

--- truffleruby/src/main/java/org/truffleruby/core/rubinius/RubiniusLastStringReadNode.java
+++ truffleruby/src/main/java/org/truffleruby/core/rubinius/RubiniusLastStringReadNode.java
@@ -15,14 +15,9 @@ import org.truffleruby.language.RubyNode;
 import org.truffleruby.language.arguments.ReadCallerFrameNode;
 import org.truffleruby.language.threadlocal.ThreadLocalInFrameNode;
 import org.truffleruby.language.threadlocal.ThreadLocalInFrameNodeGen;
-import org.truffleruby.language.threadlocal.ThreadLocalObject;
 
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.frame.Frame;
-import com.oracle.truffle.api.frame.FrameInstance;
-import com.oracle.truffle.api.frame.FrameSlot;
-import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 public class RubiniusLastStringReadNode extends RubyNode {
@@ -38,22 +33,4 @@ public class RubiniusLastStringReadNode extends RubyNode {
         }
         return threadLocalNode.execute(callerFrame.materialize()).get();
     }
-
-    @TruffleBoundary
-    private Object getLastString() {
-        // Rubinius expects $_ to be thread-local, rather than frame-local.  If we see it in a method call, we need
-        // to look to the caller's frame to get the correct value, otherwise it will be nil.
-        final Frame callerFrame = getContext().getCallStack().getCallerFrameIgnoringSend().getFrame(FrameInstance.FrameAccess.READ_ONLY);
-
-        // TODO CS 4-Jan-16 - but it could be in higher frames!
-        final FrameSlot slot = callerFrame.getFrameDescriptor().findFrameSlot("$_");
-
-        try {
-            final ThreadLocalObject threadLocalObject = (ThreadLocalObject) callerFrame.getObject(slot);
-            return threadLocalObject.get();
-        } catch (FrameSlotTypeException e) {
-            throw new UnsupportedOperationException(e);
-        }
-    }
-
 }
