963f9eb58a1d50d781d86de65cdd36f39f1ec45a
Chris Seaton <chris.seaton@oracle.com>
2017-04-24 20:24:59 +0100

    rb_tr_handle_for_managed_leaking instead of truffle_handle_for_managed

--- truffleruby/src/main/c/openssl/ossl_cipher.c
+++ truffleruby/src/main/c/openssl/ossl_cipher.c
@@ -183,7 +183,7 @@ ossl_s_ciphers(VALUE self)
     ary = rb_ary_new();
     OBJ_NAME_do_all_sorted(OBJ_NAME_TYPE_CIPHER_METH,
                     (void(*)(const OBJ_NAME*,void*))add_cipher_name_to_ary,
-                    truffle_handle_for_managed(ary));
+                    rb_tr_handle_for_managed_leaking(ary));
 
     return ary;
 }
