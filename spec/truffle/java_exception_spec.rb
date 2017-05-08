bb5fa5baa7f8c5e11f08479ff2500adc84724559
Benoit Daloze <eregontp@gmail.com>
2017-05-08 23:21:28 +0200

    Fix line numbers in spec

--- spec/truffle/java_exception_spec.rb
+++ spec/truffle/java_exception_spec.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
-# 
+#
 # Eclipse Public License version 1.0
 # GNU General Public License version 2
 # GNU Lesser General Public License version 2.1
@@ -9,30 +9,30 @@
 require_relative '../ruby/spec_helper'
 
 describe "Java exceptions" do
-  
+
   it "formats to include the source information" do
     message = <<~MESSAGE
     message
-    	RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwJavaException(TruffleDebugNodes.java:316)
+    	RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwJavaException(TruffleDebugNodes.java:314)
     MESSAGE
-    
+
     message.chomp!
-    
+
     lambda { Truffle::Debug.throw_java_exception 'message' }.should raise_error { |e|
       e.message.should == message
     }
   end
-  
+
   it "formats to include the source information including cause" do
     message = <<~MESSAGE
     message
-    	RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:327)
-    		caused by cause 1 RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:327)
-    		caused by cause 2 RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:327)
+    	RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:325)
+    		caused by cause 1 RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:325)
+    		caused by cause 2 RuntimeException org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:325)
     MESSAGE
-    
+
     message.chomp!
-    
+
     lambda { Truffle::Debug.throw_java_exception_with_cause 'message' }.should raise_error { |e|
       e.message.should == message
     }
