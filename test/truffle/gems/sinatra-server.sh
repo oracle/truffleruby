aa096878efa647c9d7e25f9fde9f2f6bce694e57
Chris Seaton <chris.seaton@oracle.com>
2017-05-18 01:14:31 +0100

    Fix sinatra test

--- test/truffle/gems/sinatra-server.sh
+++ test/truffle/gems/sinatra-server.sh
@@ -6,4 +6,9 @@ set -e
 
 GEM_HOME=`jt gem-test-pack`/gems/gems
 
-jt ruby test/truffle/gems/sinatra-server/sinatra-server.rb & test_server
+jt ruby \
+  -I`jt gem-test-pack`/gems/gems/rack-1.6.1/lib \
+  -I`jt gem-test-pack`/gems/gems/rack-protection-1.5.3/lib \
+  -I`jt gem-test-pack`/gems/gems/sinatra-1.4.6/lib \
+  -I`jt gem-test-pack`/gems/gems/tilt-2.0.1/lib \
+  test/truffle/gems/sinatra-server/sinatra-server.rb & test_server
