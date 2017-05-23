d42bda31dd3e3df8337e22ffc276662030e79016
Petr Chalupa <git@pitr.ch>
2017-05-23 20:41:33 +0200

    Fix condition in Cext building patch. Build when Sulong is available.

--- lib/patches/stdlib/rubygems/ext.rb
+++ lib/patches/stdlib/rubygems/ext.rb
@@ -19,14 +19,17 @@ class Gem::Ext::Builder
 
     @ran_rake = false # only run rake once
 
+    sulong_present = !!ENV['SULONG_HOME']
     @spec.extensions.each do |extension|
       break if @ran_rake
       # TruffleRuby: build C extensions conditionally
-      puts "WORKAROUND: Skipping build extension:#{extension}, dest_path:#{dest_path}. " +
-               'Support of C extensions is in development, set environment variable TRUFFLERUBY_CEXT_ENABLED to experiment.'
-      build_extension extension, dest_path if ENV['TRUFFLERUBY_CEXT_ENABLED']
+      if sulong_present
+        build_extension extension, dest_path
+      else
+        puts 'SULONG_HOME is not set therefore skipping C extension build.'
+      end
     end
 
-    FileUtils.touch @spec.gem_build_complete_path if ENV['TRUFFLERUBY_CEXT_ENABLED']
+    FileUtils.touch @spec.gem_build_complete_path if sulong_present
   end
 end
