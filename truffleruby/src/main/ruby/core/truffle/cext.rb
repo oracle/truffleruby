c0e93f0c0669cd146ae57d19f12c8dd6cf7fac9f
Duncan MacGregor <duncan.macgregor@oracle.com>
2017-05-25 20:22:29 +0100

    Add comment describing the rb_protect/rb_jump_tag implementation.

--- truffleruby/src/main/ruby/core/truffle/cext.rb
+++ truffleruby/src/main/ruby/core/truffle/cext.rb
@@ -1141,6 +1141,25 @@ class << Truffle::CExt
     Truffle::Interop.execute(function, *args)
   end
 
+  # The idea of rb_protect and rb_jump_tag is to avoid unwinding the
+  # native stack in an uncontrolled manner. To do this we need to be
+  # able to run a piece of code and capture both its result (if it
+  # produces one), and any exception it generates. This is done by
+  # running the requested code in a block as follows
+  #
+  #   e = store_exception { res = requested_function(...) }
+  #
+  # leaving us with e containing any exception thrown from the block,
+  # and res containing the result of the function if it completed
+  # successfully. Since Ruby's API only allows us to use a number to
+  # indicate there was an exception we have to store it in a thread
+  # local array and provide a 1-based index into that array. Then once
+  # the native library has unwound its own stack by whatever method,
+  # and can allow the ruby error to propagate, it can call rb_jump_tag
+  # with the integer produced by rb_protect. rb_jump_tag then gets the
+  # exception out of the thread local and calls raise exception to
+  # throw it and allow normal error handling to continue.
+
   def rb_protect_with_block(function, arg, block)
     res = nil
     pos = 0
