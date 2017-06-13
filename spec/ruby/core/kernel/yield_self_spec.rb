b751592539b46c124e955917fd76714a1c049320
Benoit Daloze <eregontp@gmail.com>
2017-06-13 14:57:31 +0200

    Run the Kernel#yield_self spec on TruffleRuby
    * Slightly hacky guard for now, an improvement in MSpec could make this nicer.

--- spec/ruby/core/kernel/yield_self_spec.rb
+++ spec/ruby/core/kernel/yield_self_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-ruby_version_is "2.5" do
+has_yield_self = VersionGuard.new("2.5").match? || PlatformGuard.implementation?(:truffleruby)
+
+if has_yield_self
   describe "Kernel#yield_self" do
     it "yields self" do
       object = Object.new
