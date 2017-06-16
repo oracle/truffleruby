e3e7feacbaf3984ae04ec8201db0d7d200dbc085
Benoit Daloze <eregontp@gmail.com>
2017-06-16 15:34:07 +0200

    Do not open the Truffle namespace in boot.rb

--- src/main/ruby/core/truffle/boot.rb
+++ src/main/ruby/core/truffle/boot.rb
@@ -8,54 +8,50 @@
 
 TOPLEVEL_BINDING = binding
 
-module Truffle
-  module Boot
+module Truffle::Boot
 
-    def self.main_s
-      $0 = find_s_file(original_input_file)
-      main $0
-    end
-
-    def self.check_syntax
-      inner_check_syntax
-      STDOUT.puts 'Syntax OK'
-      true
-    rescue SyntaxError => e
-      STDERR.puts "SyntaxError in #{e.message}"
-      false
-    end
-
-    def self.find_s_file(name)
-      # Nonstandard lookup
-
-      # added to look up truffleruby own files first when it's not on PATH
-      name_in_ruby_home_bin = "#{RbConfig::CONFIG['bindir']}/#{name}"
-      return name_in_ruby_home_bin if File.exist?(name_in_ruby_home_bin)
-
-      # Standard lookups
-
-      if ENV['RUBYPATH']
-        path = find_in_environment_paths(name, ENV['RUBYPATH']) and return path
-      end
-      path = find_in_environment_paths(name, ENV['PATH']) and return path
-      return name if File.exist?(name)
-
-      # Fail otherwise
-      raise LoadError, "No such file or directory -- #{name}"
-    end
-
-    private_class_method :find_s_file
-
-    def self.find_in_environment_paths(name, env_value)
-      env_value.to_s.split(File::PATH_SEPARATOR).each do |path|
-        name_in_path = "#{path}/#{name}"
-        return name_in_path if File.exist?(name_in_path)
-      end
-      nil
-    end
-
-    private_class_method :find_in_environment_paths
-
+  def self.main_s
+    $0 = find_s_file(original_input_file)
+    main $0
   end
+
+  def self.check_syntax
+    inner_check_syntax
+    STDOUT.puts 'Syntax OK'
+    true
+  rescue SyntaxError => e
+    STDERR.puts "SyntaxError in #{e.message}"
+    false
+  end
+
+  def self.find_s_file(name)
+    # Nonstandard lookup
+
+    # added to look up truffleruby own files first when it's not on PATH
+    name_in_ruby_home_bin = "#{RbConfig::CONFIG['bindir']}/#{name}"
+    return name_in_ruby_home_bin if File.exist?(name_in_ruby_home_bin)
+
+    # Standard lookups
+
+    if ENV['RUBYPATH']
+      path = find_in_environment_paths(name, ENV['RUBYPATH']) and return path
+    end
+    path = find_in_environment_paths(name, ENV['PATH']) and return path
+    return name if File.exist?(name)
+
+    # Fail otherwise
+    raise LoadError, "No such file or directory -- #{name}"
+  end
+  private_class_method :find_s_file
+
+  def self.find_in_environment_paths(name, env_value)
+    env_value.to_s.split(File::PATH_SEPARATOR).each do |path|
+      name_in_path = "#{path}/#{name}"
+      return name_in_path if File.exist?(name_in_path)
+    end
+    nil
+  end
+  private_class_method :find_in_environment_paths
+
 end
 
