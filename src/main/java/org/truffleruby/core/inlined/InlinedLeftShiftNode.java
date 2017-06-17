04d50774839d8930edf9144415a269e755209369
Benoit Daloze <eregontp@gmail.com>
2017-06-17 19:50:26 +0200

    Add inlined Fixnum#{<<,>>}

---
+++ src/main/java/org/truffleruby/core/inlined/InlinedLeftShiftNode.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 1.0
+ * GNU General Public License version 2
+ * GNU Lesser General Public License version 2.1
+ */
+package org.truffleruby.core.inlined;
+
+import org.truffleruby.RubyContext;
+import org.truffleruby.core.numeric.FixnumNodes.LeftShiftNode;
+import org.truffleruby.core.numeric.FixnumNodesFactory.LeftShiftNodeFactory;
+import org.truffleruby.language.dispatch.RubyCallNodeParameters;
+
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+public abstract class InlinedLeftShiftNode extends BinaryInlinedOperationNode {
+
+    @Child LeftShiftNode fixnumLeftShift;
+
+    public InlinedLeftShiftNode(RubyContext context, RubyCallNodeParameters callNodeParameters) {
+        super(callNodeParameters,
+                context.getCoreMethods().fixnumLeftShiftAssumption);
+    }
+
+    @Specialization(assumptions = "assumptions")
+    Object intLeftShift(int a, int b) {
+        return getLeftShiftNode().executeLeftShift(a, b);
+    }
+
+    @Specialization(assumptions = "assumptions")
+    Object longLeftShift(long a, int b) {
+        return getLeftShiftNode().executeLeftShift(a, b);
+    }
+
+    @Specialization
+    Object fallback(VirtualFrame frame, Object a, Object b) {
+        return rewriteAndCall(frame, a, b);
+    }
+
+    private LeftShiftNode getLeftShiftNode() {
+        if (fixnumLeftShift == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            fixnumLeftShift = insert(LeftShiftNodeFactory.create(null));
+        }
+        return fixnumLeftShift;
+    }
+
+}
