e3d8f0fe2b76c951dddc6a43ce812a5986f636b9
Chris Seaton <chris.seaton@oracle.com>
2017-06-20 23:45:01 +0100

    Work around https://github.com/jline/jline2/issues/282

--- test/truffle/compiler/can-we-fold-yet.sh
+++ test/truffle/compiler/can-we-fold-yet.sh
@@ -2,7 +2,8 @@
 
 source test/truffle/common.sh.inc
 
-jt ruby --graal -J-Dgraal.TruffleIterativePartialEscape=true doc/samples/can-we-fold-yet.rb < test/truffle/compiler/can-we-fold-yet/input.txt > actual.txt
+# tr works around https://github.com/jline/jline2/issues/282
+jt ruby --graal -J-Dgraal.TruffleIterativePartialEscape=true doc/samples/can-we-fold-yet.rb < test/truffle/compiler/can-we-fold-yet/input.txt | tr -d '\r' > actual.txt
 
 if ! cmp test/truffle/compiler/can-we-fold-yet/expected.txt actual.txt
 then
