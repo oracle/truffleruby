9bb921be5a91d8cf020fa7dac469bc37bc6d3774
Benoit Daloze <eregontp@gmail.com>
2017-06-30 00:15:42 +0200

    Run mspec directly with truffleruby in mx ruby_testdownstream

--- mx.truffleruby/mx_truffleruby.py
+++ mx.truffleruby/mx_truffleruby.py
@@ -83,11 +83,12 @@ def deploy_binary_if_master(args):
         mx.log('The active branch is "%s". Binaries are deployed only if the active branch is "%s".' % (active_branch, primary_branch))
         return 0
 
-def ruby_testdownstream(args):
-    os.environ['CI'] = 'true'
+def ruby_run_specs(args):
+    mx.run(['bin/truffleruby', 'spec/mspec/bin/mspec', 'run', '--config', 'spec/truffle.mspec', '--format', 'specdoc', '--excl-tag', 'fails'] + args)
 
+def ruby_testdownstream(args):
     build_truffleruby()
-    mx.run(['ruby', 'tool/jt.rb', 'test', 'fast'])
+    ruby_run_specs(['--excl-tag', 'slow'])
 
 def ruby_testdownstream_hello(args):
     build_truffleruby()
