a991711542d0b1a9e6248a96cc69aed6e7a89981
Kevin Menard <kevin.j.menard@oracle.com>
2017-07-03 15:06:05 -0400

    Untag some AOT benchmarks that should work fine.

--- tool/fail-if-any-failed.rb
+++ tool/fail-if-any-failed.rb
@@ -40,8 +40,6 @@ known_failures = [
   ["svm", "default", "jruby", "truffle", "asciidoctor", "asciidoctor:load-file"],
   ["svm", "default", "jruby", "truffle", "asciidoctor", "asciidoctor:string-lines"],
   ["svm", "default", "jruby", "truffle", "classic", "binary-trees"],
-  ["svm", "default", "jruby", "truffle", "classic", "matrix-multiply"],
-  ["svm", "default", "jruby", "truffle", "classic", "red-black"],
   ["svm", "default", "jruby", "truffle", "psd", "psd-compose-color-burn"],
   ["svm", "default", "jruby", "truffle", "psd", "psd-compose-color-dodge"],
   ["svm", "default", "jruby", "truffle", "psd", "psd-compose-exclusion"],
