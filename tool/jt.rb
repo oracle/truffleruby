0694350f5b4819100cd2aa87f6c6cafb06e0589c
Chris Seaton <chris.seaton@oracle.com>
2017-07-05 12:32:49 +0100

    Update for new Sulong options

--- tool/jt.rb
+++ tool/jt.rb
@@ -904,13 +904,13 @@ module Commands
 
     unless no_libxml
       clang '-c', '-emit-llvm', "-I#{LIBXML_INCLUDE}", 'test/truffle/cexts/xml/main.c', '-o', 'test/truffle/cexts/xml/main.bc'
-      out, _ = sulong_run("-Dpolyglot.sulong.dynamicNativeLibraryPath=#{LIBXML_LIB}", 'test/truffle/cexts/xml/main.bc', {capture: true})
+      out, _ = sulong_run("-Dpolyglot.llvm.libraries=#{LIBXML_LIB}", 'test/truffle/cexts/xml/main.bc', {capture: true})
       raise out.inspect unless out == "7\n"
     end
 
     unless no_openssl
       clang '-c', '-emit-llvm', *openssl_cflags, 'test/truffle/cexts/xopenssl/main.c', '-o', 'test/truffle/cexts/xopenssl/main.bc'
-      out, _ = sulong_run("-Dpolyglot.sulong.dynamicNativeLibraryPath=#{openssl_lib}", 'test/truffle/cexts/xopenssl/main.bc', {capture: true})
+      out, _ = sulong_run("-Dpolyglot.llvm.libraries=#{openssl_lib}", 'test/truffle/cexts/xopenssl/main.bc', {capture: true})
       raise out.inspect unless out == "5d41402abc4b2a76b9719d911017c592\n"
     end
 
