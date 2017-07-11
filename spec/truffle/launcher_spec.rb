85d09323be8fad9793b4cd51f1a448eee3ca85a1
Chris Seaton <chris.seaton@oracle.com>
2017-07-11 20:15:01 +0100

    Turn the -Xoptions test from a negative into a positive

--- spec/truffle/launcher_spec.rb
+++ spec/truffle/launcher_spec.rb
@@ -128,10 +128,10 @@ describe "The launcher" do
     out.lines[1].should == "14\n"
   end
 
-  it "doesn't include the polyglot.ruby prefix in the list of options" do
+  it "prints available options for -Xoptions" do
     out = `#{RbConfig.ruby} -Xoptions`
     $?.success?.should == true
-    out.should_not include("polyglot.ruby")
+    out.should include("-Xverbosity=")
   end
 
   it "logs options if -Xoptions.log=true is set" do
