04a4cd1c040cda6d63d53ea8f4dfc33c2ecbe082
Benoit Daloze <eregontp@gmail.com>
2017-07-16 18:02:28 +0200

    Enable debug info for C extensions for now

--- src/main/ruby/core/rbconfig.rb
+++ src/main/ruby/core/rbconfig.rb
@@ -146,7 +146,8 @@ module RbConfig
   opt_passes     = ['-always-inline', '-mem2reg', '-constprop'].join(' ')
   cc             = clang
   cpp            = cc
-  cflags         = ['-Werror=implicit-function-declaration',  # To make missing C ext functions very clear
+  cflags         = ['-g', # Show debug information such as line numbers in backtrace
+                    '-Werror=implicit-function-declaration',  # To make missing C ext functions very clear
                     '-Wno-unknown-warning-option',            # If we're on an earlier version of clang without a warning option, ignore it
                     '-Wno-int-conversion',                    # MRI has VALUE defined as long while we have it as void*
                     '-Wno-int-to-pointer-cast',               # Same as above
