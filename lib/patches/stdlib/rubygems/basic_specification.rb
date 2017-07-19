93a0c7d1affd0ecfb66e173af2041fb5b955e347
Benoit Daloze <eregontp@gmail.com>
2017-07-19 18:41:10 +0200

    Warn about not-built c-exts only if $VERBOSE when cext building is not enabled

---
+++ lib/patches/stdlib/rubygems/basic_specification.rb
@@ -0,0 +1,26 @@
+Truffle::Patching.require_original __FILE__
+
+unless ENV['TRUFFLERUBY_CEXT_ENABLED']
+  module Truffle::Patching::NoWarnIfBuildingCextDisabled
+    def contains_requirable_file? file
+      if @ignored then
+        return false
+      elsif missing_extensions? then
+        @ignored = true
+
+        # Truffle: warn only if verbose when cext building is not enabled
+        if $VERBOSE
+          warn "Ignoring #{full_name} because its extensions are not built.  " +
+            "Try: gem pristine #{name} --version #{version}"
+        end
+        return false
+      end
+
+      super
+    end
+  end
+
+  class Gem::BasicSpecification
+    prepend Truffle::Patching::NoWarnIfBuildingCextDisabled
+  end
+end
