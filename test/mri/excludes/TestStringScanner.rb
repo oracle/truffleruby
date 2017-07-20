7ebc2eea66c99f24f27086b3074d1777f3740d51
Benoit Daloze <eregontp@gmail.com>
2017-07-20 17:46:27 +0200

    Pass TestStringScanner#test_s_allocate

--- test/mri/excludes/TestStringScanner.rb
+++ test/mri/excludes/TestStringScanner.rb
@@ -1,4 +1,3 @@
 exclude :test_AREF, "needs investigation"
 exclude :test_matched, "needs investigation"
 exclude :test_pos_unicode, "needs investigation"
-exclude :test_s_allocate, "needs investigation"
