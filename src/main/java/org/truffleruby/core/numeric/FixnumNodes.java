fc5c67a8fd0826bac06d2c8b673213a1e684fa7c
Benoit Daloze <eregontp@gmail.com>
2017-07-26 15:43:47 +0200

    Cleanup Fixnum#**

--- src/main/java/org/truffleruby/core/numeric/FixnumNodes.java
+++ src/main/java/org/truffleruby/core/numeric/FixnumNodes.java
@@ -31,6 +31,7 @@ import org.truffleruby.core.numeric.FixnumNodesFactory.DivNodeFactory;
 import org.truffleruby.core.rope.LazyIntRope;
 import org.truffleruby.language.NotProvided;
 import org.truffleruby.language.SnippetNode;
+import org.truffleruby.language.WarnNode;
 import org.truffleruby.language.control.RaiseException;
 import org.truffleruby.language.dispatch.CallDispatchHeadNode;
 import org.truffleruby.language.methods.UnsupportedOperationBehavior;
@@ -1261,6 +1262,7 @@ public abstract class FixnumNodes {
 
     @Primitive(name = "fixnum_ulong_from_bignum")
     public static abstract class FixnumULongFromBigNumPrimitiveNode extends PrimitiveArrayArgumentsNode {
+
         @Specialization(guards = "isRubyBignum(b)")
         public long uLongFromBignum(DynamicObject b) {
             return Layouts.BIGNUM.getValue(b).longValue();
@@ -1268,61 +1270,26 @@ public abstract class FixnumNodes {
 
     }
 
-    @Primitive(name = "fixnum_pow")
-    public abstract static class FixnumPowPrimitiveNode extends BignumNodes.BignumCoreMethodNode {
+    @Primitive(name = "fixnum_pow", lowerFixnum = { 0, 1 })
+    public abstract static class PowNode extends BignumNodes.BignumCoreMethodNode {
 
-        @Child private SnippetNode warnNode = new SnippetNode();
+        public abstract Object executePow(Object a, Object b);
 
-        private final ConditionProfile negativeProfile = ConditionProfile.createBinaryProfile();
-        private final ConditionProfile complexProfile = ConditionProfile.createBinaryProfile();
-
-        @Specialization(guards = "canShiftIntoInt(a, b)")
-        public int powTwo(int a, int b) {
+        // Highest bit we can set is the 30th due to sign
+        @Specialization(guards = { "a == 2", "b >= 0", "b <= 30" })
+        public int powTwoInt(int a, int b) {
             return 1 << b;
         }
 
-        @Specialization(guards = "canShiftIntoInt(a, b)")
-        public int powTwo(int a, long b) {
-            return 1 << b;
+        // Highest bit we can set is the 62nd due to sign
+        @Specialization(guards = { "a == 2", "b >= 0", "b <= 62" }, replaces = "powTwoInt")
+        public long powTwoLong(int a, int b) {
+            return 1L << b;
         }
 
         @Specialization
-        public Object pow(int a, int b) {
-            return pow(a, (long) b);
-        }
-
-        @Specialization
-        public Object pow(int a, long b) {
-            return pow((long) a, b);
-        }
-
-        @Specialization
-        public Object pow(int a, double b) {
-            return pow((long) a, b);
-        }
-
-        @Specialization(guards = "isRubyBignum(b)")
-        public Object powBignum(VirtualFrame frame, int a, DynamicObject b) {
-            return powBignum(frame, (long) a, b);
-        }
-
-        @Specialization(guards = "canShiftIntoLong(a, b)")
-        public long powTwo(long a, int b) {
-            return 1 << b;
-        }
-
-        @Specialization(guards = "canShiftIntoLong(a, b)")
-        public long powTwo(long a, long b) {
-            return 1 << b;
-        }
-
-        @Specialization
-        public Object pow(long a, int b) {
-            return pow(a, (long) b);
-        }
-
-        @Specialization
-        public Object pow(long a, long b) {
+        public Object pow(long a, long b,
+                @Cached("createBinaryProfile()") ConditionProfile negativeProfile) {
             if (negativeProfile.profile(b < 0)) {
                 return null; // Primitive failure
             } else {
@@ -1331,14 +1298,9 @@ public abstract class FixnumNodes {
             }
         }
 
-        @TruffleBoundary
-        public BigInteger bigPow(long a, int b) {
-            return BigInteger.valueOf(a).pow(b);
-
-        }
-
         @Specialization
-        public Object pow(long a, double b) {
+        public Object pow(long a, double b,
+                @Cached("createBinaryProfile()") ConditionProfile complexProfile) {
             if (complexProfile.profile(a < 0)) {
                 return null; // Primitive failure
             } else {
@@ -1347,7 +1309,8 @@ public abstract class FixnumNodes {
         }
 
         @Specialization(guards = "isRubyBignum(b)")
-        public Object powBignum(VirtualFrame frame, long a, DynamicObject b) {
+        public Object powBignum(long a, DynamicObject b,
+                @Cached("new()") WarnNode warnNode) {
             if (a == 0) {
                 return 0;
             }
@@ -1368,10 +1331,20 @@ public abstract class FixnumNodes {
                 return null; // Primitive failure
             }
 
-            warnNode.execute(frame, "warn('in a**b, b may be too big')");
+            warnNode.warn("warn('in a**b, b may be too big')");
             // b >= 2**63 && (a > 1 || a < -1) => larger than largest double
             // MRI behavior/bug: always positive Infinity even if a negative and b odd (likely due to libc pow(a, +inf)).
             return Double.POSITIVE_INFINITY;
+        }
+
+        @Specialization(guards = "!isRubyBignum(b)")
+        public Object pow(long a, DynamicObject b) {
+            return null; // Primitive failure
+        }
+
+        @TruffleBoundary
+        public BigInteger bigPow(long a, int b) {
+            return BigInteger.valueOf(a).pow(b);
         }
 
         @TruffleBoundary
@@ -1382,34 +1355,6 @@ public abstract class FixnumNodes {
         @TruffleBoundary
         private int compareTo(BigInteger a, BigInteger b) {
             return a.compareTo(b);
-        }
-
-        @Specialization(guards = "!isRubyBignum(b)")
-        public Object pow(int a, DynamicObject b) {
-            return null; // Primitive failure
-        }
-
-        @Specialization(guards = "!isRubyBignum(b)")
-        public Object pow(long a, DynamicObject b) {
-            return null; // Primitive failure
-        }
-
-        protected static boolean canShiftIntoInt(int a, int b) {
-            return canShiftIntoInt(a, (long) b);
-        }
-
-        protected static boolean canShiftIntoInt(int a, long b) {
-            // Highest bit we can set is the 30th due to sign
-            return a == 2 && b >= 0 && b <= 32 - 2;
-        }
-
-        protected static boolean canShiftIntoLong(long a, int b) {
-            return canShiftIntoLong(a, (long) b);
-        }
-
-        protected static boolean canShiftIntoLong(long a, long b) {
-            // Highest bit we can set is the 30th due to sign
-            return a == 2 && b >= 0 && b <= 64 - 2;
         }
 
     }
