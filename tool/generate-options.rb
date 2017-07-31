9dddfc9aa126a139bb9ad1146d633800d297f183
Petr Chalupa <petr.chalupa@oracle.com>
2017-07-31 16:38:29 +0200

    Small improvements in option generator

--- tool/generate-options.rb
+++ tool/generate-options.rb
@@ -12,53 +12,42 @@ require 'ostruct'
 require 'yaml'
 require 'erb'
 
-options = YAML.load_file('tool/options.yml')
+options_data = YAML.load_file('tool/options.yml')
 
-options = options.map do |constant, (name, type, default, *description)|
+options = options_data.map do |constant, (name, type, default, *description)|
   description = description.join(', ')
 
   case type
     when 'boolean'
       type = 'boolean'
       boxed_type = 'Boolean'
-      type_cons = 'BooleanOptionDescription'
       default = default.to_s
       null_default = false
     when 'verbosity'
       type = 'Verbosity'
       boxed_type = type
-      type_cons = 'VerbosityOptionDescription'
-      default = case default.to_s
-                  when 'nil'
-                    'Verbosity.NIL'
-                  when 'true'
-                    'Verbosity.TRUE'
-                  when 'false'
-                    'Verbosity.FALSE'
-                end
+      default = "Verbosity.#{default.to_s.upcase}"
       null_default = 'Verbosity.FALSE'
     when 'integer'
       type = 'int'
       boxed_type = 'Integer'
-      type_cons = 'IntegerOptionDescription'
       default = default.to_s
       null_default = 0
     when 'string'
       type = 'String'
       boxed_type = type
-      type_cons = 'StringOptionDescription'
+      type_description = 'StringOptionDescription'
       default = default.nil? ? 'null' : "\"#{default.to_s}\""
       null_default = 'null'
     when 'byte-string'
       type = 'byte[]'
       boxed_type = type
-      type_cons = 'ByteStringOptionDescription'
       default = 'null'
       null_default = 'null'
     when 'string-array'
       type = 'String[]'
       boxed_type = type
-      type_cons = 'StringArrayOptionDescription'
+      type_description = 'StringArrayOptionDescription'
       default = "new String[]{#{default.map(&:inspect).join(', ')}}"
       null_default = 'null'
     else
@@ -66,15 +55,15 @@ options = options.map do |constant, (name, type, default, *description)|
   end
 
   OpenStruct.new(
-      :constant => constant,
-      :name => name,
-      :type => type,
-      :boxed_type => boxed_type,
-      :default => default,
-      :reference_default => /[A-Z]/.match(default[0]) && type != 'Verbosity',
-      :null_default => null_default,
-      :description => description,
-      :type_cons => type_cons
+      constant:          constant,
+      name:              name,
+      type:              type,
+      type_description:  type_description || "#{boxed_type}OptionDescription",
+      boxed_type:        boxed_type,
+      default:           default,
+      reference_default: /^[A-Z_]+$/.match(default),
+      null_default:      null_default,
+      description:       description
   )
 end
 
@@ -133,7 +122,10 @@ import javax.annotation.Generated;
 @Generated("tool/generate-options.rb")
 public class OptionsCatalog {
 
-    <% options.each do |o| %>public static final <%= o.type_cons %> <%= o.constant %> = new <%= o.type_cons %>("ruby.<%= o.name %>", "<%= o.description %>", <%= o.reference_default ? o.default + '.getDefaultValue()' : o.default %>);
+    <% options.each do |o| %>public static final <%= o.type_description %> <%= o.constant %> = new <%= o.type_description %>(
+            "ruby.<%= o.name %>", 
+            "<%= o.description %>", 
+            <%= o.reference_default ? o.default + '.getDefaultValue()' : o.default %>);
     <% end %>
     public static OptionDescription<?> fromName(String name) {
         switch (name) {
