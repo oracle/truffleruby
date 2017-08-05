8566b77dd59f732965b01243c237cb1c40796239
Benoit Daloze <eregontp@gmail.com>
2017-08-05 13:44:48 +0200

    Add a couple lint verifications for the cext code

--- tool/lint.sh
+++ tool/lint.sh
@@ -15,3 +15,17 @@ if [ -n "$bad" ]; then
 	echo "$bad"
 	exit 1
 fi
+
+bad=$(grep -Pzo '\)\s*\n\s*\{' src/main/c/cext/ruby.c || exit 0)
+if [ -n "$bad" ]; then
+	echo "The function definition opening brace should be on the same line: ...args) {"
+	echo "$bad"
+	exit 1
+fi
+
+bad=$(egrep '\)\{' src/main/c/cext/ruby.c || exit 0)
+if [ -n "$bad" ]; then
+	echo "There should be a space between ) and {"
+	echo "$bad"
+	exit 1
+fi
