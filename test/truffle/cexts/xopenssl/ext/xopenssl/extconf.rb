5587dc690acb883e5e5361ded19a74612e1d9781
Chris Seaton <chris.seaton@oracle.com>
2017-08-07 12:42:52 +0100

    Move automatic OPENSSL_PREFIX into extconfs, because we don't always compile via jt now
    This functionality used to be there but was lost when we move C
    extension compilation into Makefiles.

--- test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
+++ test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
@@ -1,5 +1,11 @@
 require 'mkmf'
 
+MAC = `uname`.chomp == 'Darwin'
+
+if MAC && !ENV['OPENSSL_PREFIX']
+  ENV['OPENSSL_PREFIX'] = '/usr/local/opt/openssl'
+end
+
 if ENV['OPENSSL_PREFIX']
   $CFLAGS += " -I #{ENV['OPENSSL_PREFIX']}/include"
   $LIBS += " -l #{ENV['OPENSSL_PREFIX']}/lib/libssl.#{RbConfig::CONFIG['NATIVE_DLEXT']}"
