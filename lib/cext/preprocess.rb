b85c7f477780845d3d8d7fa270494402cf67c282
Benoit Daloze <eregontp@gmail.com>
2017-08-18 15:00:27 +0200

    Format the PATCHED_FILES Hash in a easier to read way

--- lib/cext/preprocess.rb
+++ lib/cext/preprocess.rb
@@ -20,7 +20,7 @@ XML_NODE_SET_PATCH = <<-EOF
 EOF
 
 # Found in pg
-PG_BINARY_ENCODER_PATCH = <<-EOF	
+PG_BINARY_ENCODER_PATCH = <<-EOF
 	switch(rb_tr_to_int_const(value)){
 		case Qtrue_int_const : mybool = 1; break;
 		case Qfalse_int_const : mybool = 0; break;
@@ -30,7 +30,7 @@ EOF
 PUMA_HTTP_PARSER_FREE = <<-EOF
 void HttpParser_free(puma_parser* hp) {
   TRACE();
-  
+
   if(hp) {
     rb_tr_release_handle(hp->request);
     rb_tr_release_handle(hp->body);
@@ -39,58 +39,120 @@ void HttpParser_free(puma_parser* hp) {
 }
 EOF
 
-PATCHED_FILES = {'xml_node_set.c'      => {:gem      => 'nokogiri',
-                                           :patches  => 
-                                             [{:match       => /[[:blank:]]*?switch\s*?\(.*?Qnil:/m, 
-                                               :replacement => XML_NODE_SET_PATCH}]},
-                 'pg_binary_encoder.c' => {:gem     => 'pg',
-                                           :patches => 
-                                             [{:match       => /[[:blank:]]*?switch\s*?\(.*?Qfalse\s*?:.*?break;/m,
-                                               :replacement => PG_BINARY_ENCODER_PATCH}]},
-                 'bytebuffer.c'        => {:gem   => 'nio4r',
-                                           :patches => 
-                                             [{:match       => /(static VALUE .*?) = Qnil;/,
-                                               :replacement => '\1;'}]},
-                 'monitor.c'           => {:gem   => 'nio4r',
-                                           :patches => 
-                                             [{:match       => /(static VALUE .*?) = Qnil;/,
-                                               :replacement => '\1;'}]},
-                 'selector.c'          => {:gem   => 'nio4r',
-                                           :patches => 
-                                             [{:match       => /(static VALUE .*?)\s+= Qnil;/,
-                                               :replacement => '\1;'}]},
-                 'websocket_mask.c'    => {:gem   => 'websocket-driver',
-                                           :patches => 
-                                             [{:match       => /(VALUE .*?)\s+= Qnil;/,
-                                               :replacement => '\1;'}]},
-                 'http11_parser.c'    => {:gem   => 'puma_http11',
-                                           :patches => 
-                                             [{:match       => /parser->(.*?) = Qnil/,
-                                               :replacement => 'parser->\1 = rb_tr_handle_for_managed(Qnil)'}]},
-                 'puma_http11.c'      => {:gem   => 'puma_http11',
-                                           :patches => 
-                                             [{:match       => /(define.*?,) Qnil }/,
-                                               :replacement => '\1 NULL }'},
-                                               {:match       => /cf->value = (.*?);/,
-                                               :replacement => 'cf->value = rb_tr_handle_for_managed(\1);'},
-                                               {:match       => /return found \? found->value : Qnil;/,
-                                               :replacement => 'return found ? rb_tr_managed_from_handle(found->value) : Qnil;'},
-                                               {:match       => /return cf->value;/,
-                                               :replacement => 'return rb_tr_managed_from_handle(cf->value);'},
-                                               {:match       => /void HttpParser_mark\(puma_parser\* hp\) {.*?}/m,
-                                               :replacement => 'void HttpParser_mark(puma_parser* hp) { }'},
-                                               {:match       => /void HttpParser_free\(void \*data\) {.*?}.*?}/m,
-                                               :replacement => PUMA_HTTP_PARSER_FREE},
-                                               {:match       => /\(hp->request/,
-                                               :replacement => '(rb_tr_managed_from_handle(hp->request)'},
-                                               {:match       => /return http->body;/,
-                                               :replacement => 'return rb_tr_managed_from_handle(http->body);'},
-                                               {:match       => /http->request = req_hash;/,
-                                               :replacement => 'http->request = rb_tr_handle_for_managed(req_hash);'},
-                                               {:match       => /hp->request = Qnil;/,
-                                               :replacement => 'hp->request = rb_tr_handle_for_managed(Qnil);'},
-                                               {:match       => /hp->body = rb_str_new\(at, length\);/,
-                                               :replacement => 'hp->body = rb_tr_handle_for_managed(rb_str_new(at, length));'}]}}
+PATCHED_FILES = {
+  'xml_node_set.c' => {
+    gem: 'nokogiri',
+    patches: [
+      {
+        match: /[[:blank:]]*?switch\s*?\(.*?Qnil:/m,
+        replacement: XML_NODE_SET_PATCH
+      }
+    ]
+  },
+  'pg_binary_encoder.c' => {
+    gem: 'pg',
+    patches: [
+      {
+        match: /[[:blank:]]*?switch\s*?\(.*?Qfalse\s*?:.*?break;/m,
+        replacement: PG_BINARY_ENCODER_PATCH
+      }
+    ]
+  },
+  'bytebuffer.c' => {
+    gem: 'nio4r',
+    patches: [
+      {
+        match: /(static VALUE .*?) = Qnil;/,
+        replacement: '\1;'
+      }
+    ]
+  },
+  'monitor.c' => {
+    gem: 'nio4r',
+    patches: [
+      {
+        match: /(static VALUE .*?) = Qnil;/,
+        replacement: '\1;'
+      }
+    ]
+  },
+  'selector.c' => {
+    gem: 'nio4r',
+    patches: [
+      {
+        match: /(static VALUE .*?)\s+= Qnil;/,
+        replacement: '\1;'
+      }
+    ]
+  },
+  'websocket_mask.c' => {
+    gem: 'websocket-driver',
+    patches: [
+      {
+        match: /(VALUE .*?)\s+= Qnil;/,
+        replacement: '\1;'
+      }
+    ]
+  },
+  'http11_parser.c' => {
+    gem: 'puma_http11',
+    patches: [
+      {
+        match: /parser->(.*?) = Qnil/,
+        replacement: 'parser->\1 = rb_tr_handle_for_managed(Qnil)'
+      }
+    ]
+  },
+  'puma_http11.c' => {
+    gem: 'puma_http11',
+    patches: [
+      {
+        match: /(define.*?,) Qnil }/,
+        replacement: '\1 NULL }'
+      },
+      {
+        match: /cf->value = (.*?);/,
+        replacement: 'cf->value = rb_tr_handle_for_managed(\1);'
+      },
+      {
+        match: /return found \? found->value : Qnil;/,
+        replacement: 'return found ? rb_tr_managed_from_handle(found->value) : Qnil;'
+      },
+      {
+        match: /return cf->value;/,
+        replacement: 'return rb_tr_managed_from_handle(cf->value);'
+      },
+      {
+        match: /void HttpParser_mark\(puma_parser\* hp\) {.*?}/m,
+        replacement: 'void HttpParser_mark(puma_parser* hp) { }'
+      },
+      {
+        match: /void HttpParser_free\(void \*data\) {.*?}.*?}/m,
+        replacement: PUMA_HTTP_PARSER_FREE
+      },
+      {
+        match: /\(hp->request/,
+        replacement: '(rb_tr_managed_from_handle(hp->request)'
+      },
+      {
+        match: /return http->body;/,
+        replacement: 'return rb_tr_managed_from_handle(http->body);'
+      },
+      {
+        match: /http->request = req_hash;/,
+        replacement: 'http->request = rb_tr_handle_for_managed(req_hash);'
+      },
+      {
+        match: /hp->request = Qnil;/,
+        replacement: 'hp->request = rb_tr_handle_for_managed(Qnil);'
+      },
+      {
+        match: /hp->body = rb_str_new\(at, length\);/,
+        replacement: 'hp->body = rb_tr_handle_for_managed(rb_str_new(at, length));'
+      }
+    ]
+  }
+}
 
 def preprocess(line)
   if line =~ VALUE_LOCALS
@@ -148,7 +210,7 @@ end
 
 if __FILE__ == $0
   puts "#line 1 \"#{ARGF.filename}\""
-  
+
   contents = patch(ARGF.filename, File.read(ARGF.filename), Dir.pwd)
   contents.each_line do |line|
     puts preprocess(line)
