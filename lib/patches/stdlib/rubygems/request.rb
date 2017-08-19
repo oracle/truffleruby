72ba7aef6e2f6dfd3c360ddfcf51f7910d1aa9d9
Benoit Daloze <eregontp@gmail.com>
2017-08-19 14:55:22 +0200

    Simplify patches related to downloading HTTPS
    * The patch in rubygems/remote_fetcher.rb is not needed,
      as removing just goes through the rubygems/request.rb patch.
    * wget is no longer needed.
    * Share code to perform a https request and return a Net::HTTPResponse
      using curl.
    * Only patch the https path, let other requests use the original
      implementation to minimize the patch size.

--- lib/patches/stdlib/rubygems/request.rb
+++ lib/patches/stdlib/rubygems/request.rb
@@ -3,53 +3,19 @@ Truffle::Patching.require_original __FILE__
 if Truffle::Boot.patching_openssl_enabled?
   # TruffleRuby: Use curl when uri has https scheme
 
-  class Gem::Request
-    class CurlResponse < Net::HTTPOK
-      def body
-        @body
-      end
-    end
+  require "truffle/https_downloader"
 
-    HTTP_PROXY_HEADER="HTTP/1.0 200 Connection established\r\n\r\n"
-
-    def fetch
-      request = @request_class.new @uri.request_uri
-
-      unless @uri.nil? || @uri.user.nil? || @uri.user.empty? then
-        request.basic_auth Gem::UriFormatter.new(@uri.user).unescape,
-                           Gem::UriFormatter.new(@uri.password).unescape
-      end
-
-      request.add_field 'User-Agent', @user_agent
-      request.add_field 'Connection', 'keep-alive'
-      request.add_field 'Keep-Alive', '30'
-
-      if @last_modified then
-        request.add_field 'If-Modified-Since', @last_modified.httpdate
-      end
-
-      yield request if block_given?
-
-      # TruffleRuby: start
+  module Truffle::Patching::GemRequestPerformHTTPSRequest
+    def perform_request(request)
       if @uri.scheme == "https"
-        resp = CurlResponse.new("1.1", 200, "OK")
-        resp_raw = if (`curl --help` rescue nil)
-                     `curl -i -s #{@uri}`
-                   else
-                     raise 'curl is missing'
-                   end
-        index_offset = resp_raw.start_with?(HTTP_PROXY_HEADER) ? HTTP_PROXY_HEADER.size : 0
-        blank_line_idx = resp_raw.index("\r\n\r\n", index_offset)
-        header = resp_raw[0, blank_line_idx]
-        resp.body = resp_raw[(blank_line_idx+4)..-1]
-        if m = /ETag: (\"[[:alnum:]]*\")/.match(header)
-          resp["ETag"] = m[1]
-        end
-        resp
+        Truffle::HTTPSDownloader.download(@uri)
       else
-        perform_request request
+        super
       end
-      # TruffleRuby: end
     end
+  end
+
+  class Gem::Request
+    prepend Truffle::Patching::GemRequestPerformHTTPSRequest
   end
 end
