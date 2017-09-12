5008e26056affaf6f1a1068b9f666f1208f5b2e0
Benoit Daloze <eregontp@gmail.com>
2017-09-12 16:02:44 +0200

    Restore support for nil for lines without code in coverage results
    * Reverts 632b7f911ce3bf54f64a4b5f1b3a11fd5df6d70f.
    * Correctly handling line without code is a useful feature, even if no 100% correct.

--- test/truffle/integration/coverage/test.rb
+++ test/truffle/integration/coverage/test.rb
@@ -15,6 +15,6 @@ require_relative 'subject.rb'
 result = Coverage.result
 key = result.keys.find { |k| k.end_with?('subject.rb') }
 data = result[key]
-expected = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 10, 0, 0, 1, 0, 1, 1, 0, 0, 1, 2, 0, 0, 1, 1, 0, 1]
+expected = [nil, nil, nil, nil, nil, nil, nil, nil, 1, 1, nil, nil, 10, nil, nil, 1, nil, 1, 1, nil, nil, 1, 2, nil, nil, 1, 1, nil, 1]
 
-raise "coverage data not as expected: #{data}" unless data == expected
+raise "coverage data:\n#{data}\nnot as expected:\n#{expected}" unless data == expected
