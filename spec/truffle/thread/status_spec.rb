716ea6cce44efa721b6abdf045820b425e6494dc
Benoit Daloze <eregontp@gmail.com>
2017-09-12 12:40:22 +0200

    Add a spec the Thread#status does not change while Regexp matching

---
+++ spec/truffle/thread/status_spec.rb
@@ -0,0 +1,33 @@
+# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
+require_relative '../../ruby/spec_helper'
+
+describe "Thread#status" do
+
+  it "is not changed by regexp matching" do
+    # A regexp taking a long time,
+    # from https://swtch.com/~rsc/regexp/regexp1.html
+    # Takes around 100 ms so the main thread can observe the status
+    # while this thread is matching.
+    n = 23
+    regexp = /#{'a?' * n}#{'a' * n}/
+    string = 'a' * n
+
+    t = Thread.new {
+      regexp =~ string
+    }
+
+    while status = t.status
+      status.should == "run"
+      Thread.pass
+    end
+    t.join
+  end
+
+end
