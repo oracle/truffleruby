7f1ed5093f6ecf253680126a3f8fda22e2db3cbe
Benoit Daloze <eregontp@gmail.com>
2017-09-16 16:27:31 +0200

    Run MRI Fiber tests

---
+++ test/mri/excludes/TestFiber.rb
@@ -0,0 +1,4 @@
+exclude :test_many_fibers, "OOM"
+exclude :test_fatal_in_fiber, "inter-test dependency"
+exclude :test_stack_size, "RubyVM-specific"
+exclude :test_no_valid_cfp, "needs investigation"
