790bead4e9a824632aed1966fbaab6c081311a09
Benoit Daloze <eregontp@gmail.com>
2017-09-18 11:41:04 +0200

    Minimally support continuations so MRI tests can pass
    * Allows #callcc to be called, but prevents to actually call
      the returned Continuation.

--- lib/truffle/continuation.rb
+++ lib/truffle/continuation.rb
@@ -1,1 +1,21 @@
 warn "#{File.basename(__FILE__)}: warning: callcc is obsolete; use Fiber instead"
+
+class Continuation
+  def initialize
+    @fiber = Fiber.current
+  end
+
+  def call
+    if Fiber.current != @fiber
+      raise 'continuation called across fiber'
+    end
+    raise 'Continuations are unsupported on TruffleRuby'
+  end
+end
+
+module Kernel
+  def callcc
+    yield Continuation.new
+  end
+  module_function :callcc
+end
