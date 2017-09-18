ebc0e508062768e354c1fc0495768789b1162839
Benoit Daloze <eregontp@gmail.com>
2017-09-18 11:41:04 +0200

    Minimally support continuations so MRI tests can pass
    * Allows #callcc to be called, but prevents to actually call
      the returned Continuation.

--- test/mri/tests/ruby/test_fiber.rb
+++ test/mri/tests/ruby/test_fiber.rb
@@ -79,12 +79,12 @@ class TestFiber < Test::Unit::TestCase
       f.resume
       f.resume
     }
-    # assert_raise(RuntimeError){
-    #   Fiber.new{
-    #     @c = callcc{|c| @c = c}
-    #   }.resume
-    #   @c.call # cross fiber callcc
-    # }
+    assert_raise(RuntimeError){
+      Fiber.new{
+        @c = callcc{|c| @c = c}
+      }.resume
+      @c.call # cross fiber callcc
+    }
     assert_raise(RuntimeError){
       Fiber.new{
         raise
@@ -345,4 +345,3 @@ class TestFiber < Test::Unit::TestCase
     assert_equal(s1, $_, bug7678)
   end
 end
-
