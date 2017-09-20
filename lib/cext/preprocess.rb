036457f45b8fc38e2cd862b00942ae0b06a079f3
Brandon Fish <brandon.fish@oracle.com>
2017-09-19 23:15:37 -0500

    Combine read and write field in msyql2 patches, preserve whitespace, fix indentation

--- lib/cext/preprocess.rb
+++ lib/cext/preprocess.rb
@@ -61,17 +61,22 @@ EOF
 
 def read_field(struct_var_name, field_name)
   { 
-    match: /\b#{struct_var_name}(\.|->)#{field_name}\s*([\),;]|==|!=)/,
-    replacement: "rb_tr_managed_from_handle(#{struct_var_name}\\1#{field_name}) \\2"
+    match: /\b#{struct_var_name}(\.|->)#{field_name}(\s*(?:[\),;]|==|!=))/,
+    replacement: "rb_tr_managed_from_handle(#{struct_var_name}\\1#{field_name})\\2"
   }
 end
 
 def write_field(struct_var_name, field_name, leaking)
   leaking_str = leaking ? '_leaking': ''
   {
-    match: /\b#{struct_var_name}(\.|->)#{field_name}\s*=\s*(\w.+);\s*$/ ,
-    replacement: "#{struct_var_name}\\1#{field_name} = rb_tr_handle_for_managed#{leaking_str}(\\2);"
+    match: /\b#{struct_var_name}(\.|->)#{field_name}(\s*=\s*)(\w.+);\s*$/ ,
+    replacement: "#{struct_var_name}\\1#{field_name}\\2rb_tr_handle_for_managed#{leaking_str}(\\3);"
   }
+end
+
+def read_write_field(struct_var_name, field_name, leaking)
+  [read_field(struct_var_name, field_name), 
+   write_field(struct_var_name, field_name, leaking)]
 end
 
 PATCHED_FILES = {
@@ -184,14 +189,10 @@ PATCHED_FILES = {
   'client.c' => {
     gem: 'mysql2',
     patches: [
-        read_field('wrapper','active_thread'),
-        write_field('wrapper','active_thread', false),
-        read_field('wrapper','encoding'),
-        write_field('wrapper','encoding', false),
-        read_field('async_args','self'),
-        write_field('async_args','self', true),
-        read_field('args','sql'),
-        write_field('args','sql', true),
+      *read_write_field('wrapper','active_thread', false),
+      *read_write_field('wrapper','encoding', false),
+      *read_write_field('async_args','self', true),
+      *read_write_field('args','sql', true),
       {
         match: /nogvl_close\(wrapper\);/,
         replacement: MYSQL2_FREE_WRAPPER
@@ -201,22 +202,14 @@ PATCHED_FILES = {
   'result.c' => {
     gem: 'mysql2',
     patches: [
-        read_field('wrapper','statement'),
-        write_field('wrapper','statement', true),
-        read_field('wrapper','client'),
-        write_field('wrapper','client', false),
-        read_field('wrapper','encoding'),
-        write_field('wrapper','encoding', false),
-        read_field('wrapper','fields'),
-        write_field('wrapper','fields', false),
-        read_field('wrapper','rows'),
-        write_field('wrapper','rows', false),
-        read_field('args','db_timezone'),
-        write_field('args','db_timezone', true),
-        read_field('args','app_timezone'),
-        write_field('args','app_timezone', true),
-        read_field('args','block_given'),
-        write_field('args','block_given', true),
+      *read_write_field('wrapper','statement', true),
+      *read_write_field('wrapper','client', false),
+      *read_write_field('wrapper','encoding', false),
+      *read_write_field('wrapper','fields', false),
+      *read_write_field('wrapper','rows', false),
+      *read_write_field('args','db_timezone', true),
+      *read_write_field('args','app_timezone', true),
+      *read_write_field('args','block_given', true),
       {
         match: /xfree\(wrapper\);/,
         replacement: MYSQL2_FREE_RESULT_WRAPPER
@@ -226,12 +219,10 @@ PATCHED_FILES = {
   'statement.c' => {
     gem: 'mysql2',
     patches: [
-        read_field('wrapper','encoding'),
-        write_field('wrapper','active_thread', false),
-        read_field('stmt_wrapper','client'),
-        write_field('stmt_wrapper','client', false),
-        read_field('args','sql'),
-        write_field('args','sql', true),
+      read_field('wrapper','encoding'),
+      write_field('wrapper','active_thread', false),
+      *read_write_field('stmt_wrapper','client', false),
+      *read_write_field('args','sql', true),
       {
         match: /if \(stmt_wrapper->refcount == 0\) {/,
         replacement: MYSQL2_FREE_STATEMENT
