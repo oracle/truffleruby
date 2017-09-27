f4aa6645775db98923a0189fd16bdf75c3bf8ee4
Brandon Fish <brandon.fish@oracle.com>
2017-09-26 23:54:31 -0500

    Copy MRI 2.3.3 headers

--- lib/cext/ruby/backward/rubyio.h
+++ lib/cext/ruby/backward/rubyio.h
@@ -1,1 +1,6 @@
-#include "../../ruby.h"
+#if   defined __GNUC__
+#warning use "ruby/io.h" instead of "rubyio.h"
+#elif defined _MSC_VER
+#pragma message("warning: use \"ruby/io.h\" instead of \"rubyio.h\"")
+#endif
+#include "ruby/io.h"
