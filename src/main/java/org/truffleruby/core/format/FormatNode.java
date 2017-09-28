e9ecdcf3e0d6a57394c33ea0ecf7a63f792039fb
Kevin Menard <kevin.j.menard@oracle.com>
2017-09-28 13:28:16 -0400

    Ensure packed strings have the correct code range.

--- src/main/java/org/truffleruby/core/format/FormatNode.java
+++ src/main/java/org/truffleruby/core/format/FormatNode.java
@@ -15,9 +15,11 @@ import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.profiles.ConditionProfile;
+import org.jcodings.specific.USASCIIEncoding;
 import org.truffleruby.core.array.ArrayUtils;
 import org.truffleruby.core.format.exceptions.TooFewArgumentsException;
 import org.truffleruby.core.rope.CodeRange;
+import org.truffleruby.core.rope.RopeOperations;
 import org.truffleruby.language.RubyBaseNode;
 
 import java.nio.ByteBuffer;
@@ -166,6 +168,7 @@ public abstract class FormatNode extends RubyBaseNode {
         final int outputPosition = getOutputPosition(frame);
         System.arraycopy(values, 0, output, outputPosition, valuesLength);
         setOutputPosition(frame, outputPosition + valuesLength);
+        setStringCodeRange(frame, RopeOperations.isAsciiOnly(values, USASCIIEncoding.INSTANCE) ? CodeRange.CR_7BIT : CodeRange.CR_VALID);
         increaseStringLength(frame, valuesLength);
     }
 
