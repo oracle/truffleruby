c7fe0a721073de3ecc58f26d00b52518f6c29bdb
Duncan MacGregor <duncan.macgregor@oracle.com>
2017-10-03 16:00:47 +0100

    Changes based on review.

--- src/main/ruby/core/rbconfig.rb
+++ src/main/ruby/core/rbconfig.rb
@@ -36,11 +36,15 @@ module RbConfig
 
   host_os  = Truffle::System.host_os
   host_cpu = Truffle::System.host_cpu
-
-  # host_os for linux systems is reported as linux-gnu rather than simply linux.
-  host_os = 'linux-gnu' if host_os == 'linux'
-
   host_vendor = 'unknown'
+  # Some config entries report linux-gnu rather than simply linux.
+  if host_os == 'linux'
+    host_os_full = 'linux-gnu'
+  else
+    host_os_full = host_os
+  end
+  # Host should match the target triplet for clang or GCC, otherwise some C extension builds will fail.
+  host         = "#{host_cpu}-#{host_vendor}-#{host_os_full}"
 
   ruby_install_name = 'truffleruby'
 
@@ -52,13 +56,13 @@ module RbConfig
   ruby_version   = ruby_api_version
 
   arch         = "#{host_cpu}-#{host_os}"
-  # Host should match the target triplet for clang or GCC, otherwise some C extension builds will fail.
-  host         = "#{host_cpu}-#{host_vendor}-#{host_os}"
   cppflags     = ''
   libs         = ''
 
   CONFIG = {
       'arch'              => arch,
+      'build'             => host,
+      'build_os'          => host_os_full,
       'configure_args'    => ' ',
       'ARCH_FLAG'         => '',
       'CPPFLAGS'          => cppflags,
@@ -68,7 +72,7 @@ module RbConfig
       'DLEXT'             => 'su',
       'NATIVE_DLEXT'      => RUBY_PLATFORM.include?('darwin') ? 'dylib' : 'so',
       'host_alias'        => '',
-      'host_os'           => host_os,
+      'host_os'           => host_os_full,
       'host_cpu'          => host_cpu,
       'host'              => host,
       'LIBEXT'            => 'a',
@@ -95,6 +99,7 @@ module RbConfig
       'ruby_version'      => ruby_api_version,
       'RUBY_BASE_NAME'    => ruby_base_name,
       'target_cpu'        => host_cpu,
+      'target_os'         => host_os,
   }
 
   MAKEFILE_CONFIG = CONFIG.dup
@@ -187,8 +192,8 @@ module RbConfig
   mkconfig['COMPILE_C'] = "ruby #{libdir}/cext/preprocess.rb $< | $(CC) $(INCFLAGS) $(CPPFLAGS) $(CFLAGS) $(COUTFLAG) -xc - -o $@ && #{OPT} #{opt_passes} $@ -o $@",
   expanded['LINK_SO'] = "#{ruby_launcher} -Xgraal.warn_unless=false -e Truffle::CExt::Linker.main -- -o $@ $(OBJS) #{libs}"
   mkconfig['LINK_SO'] = "#{ruby_launcher} -Xgraal.warn_unless=false -e Truffle::CExt::Linker.main -- -o $@ $(OBJS) $(LIBS)"
-  expanded['TRY_LINK'] = "#{CLANG} -o conftest ${CPPFLAGS} #{libdir}/cext/ruby.bc #{libdir}/cext/trufflemock.bc $(src) $(INCFLAGS) #{linkflags} #{libs}"
-  mkconfig['TRY_LINK'] = "#{CLANG} -o conftest ${CPPFLAGS} #{libdir}/cext/ruby.bc #{libdir}/cext/trufflemock.bc $(src) $(INCFLAGS) #{linkflags} $(LIBS)"
+  expanded['TRY_LINK'] = "#{CLANG} -o conftest $(CPPFLAGS) #{libdir}/cext/ruby.bc #{libdir}/cext/trufflemock.bc $(src) $(INCFLAGS) #{linkflags} #{libs}"
+  mkconfig['TRY_LINK'] = "#{CLANG} -o conftest $(CPPFLAGS) #{libdir}/cext/ruby.bc #{libdir}/cext/trufflemock.bc $(src) $(INCFLAGS) #{linkflags} $(LIBS)"
 
   def self.ruby
     Truffle::Boot.ruby_launcher or
