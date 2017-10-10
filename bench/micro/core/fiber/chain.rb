95fa2ac98ad12bddcda1e1ed3993935007744aff
Brandon Fish <brandon.fish@oracle.com>
2017-10-10 17:52:39 -0500

    Add fiber micro benchmark

---
+++ bench/micro/core/fiber/chain.rb
@@ -0,0 +1,21 @@
+# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
+def create_chain(size)
+  last = Fiber.new { loop { Fiber.yield 42 } }
+  (size - 1).times.inject(last) { |nxt, _|
+    Fiber.new { loop { Fiber.yield nxt.resume } }
+  }
+end
+
+small = create_chain(10)
+
+benchmark 'fiber-chain-small' do
+  small.resume
+end
+
