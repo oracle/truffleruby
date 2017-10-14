8717d8fcff667fe33b0d7e76e9ea7e7885065b07
Brandon Fish <brandon.fish@oracle.com>
2017-10-14 18:57:55 -0500

    Patch more mysql2 statement.c

--- lib/cext/preprocess.rb
+++ lib/cext/preprocess.rb
@@ -79,9 +79,11 @@ Init_threads_table(VALUE mByebug)
   locker = Qnil;
 EOF
 
+NO_ASSIGNMENT = /(?:[\),;]|==|!=)/
+
 def read_field(struct_var_name, field_name)
   { 
-    match: /\b#{struct_var_name}(\.|->)#{field_name}(\s*(?:[\),;]|==|!=))/,
+    match: /\b#{struct_var_name}(\.|->)#{field_name}(\s*#{NO_ASSIGNMENT})/,
     replacement: "rb_tr_managed_from_handle(#{struct_var_name}\\1#{field_name})\\2"
   }
 end
@@ -97,6 +99,26 @@ end
 def read_write_field(struct_var_name, field_name, leaking)
   [read_field(struct_var_name, field_name), 
    write_field(struct_var_name, field_name, leaking)]
+end
+
+def read_array(name)
+  { 
+    match: /\b#{name}\[(\w+)\](\s*#{NO_ASSIGNMENT})/,
+    replacement: "rb_tr_managed_from_handle(#{name}[\\1])\\2"
+  }
+end
+
+def write_array(name, leaking)
+  leaking_str = leaking ? '_leaking': ''
+  {
+    match: /#{name}\[(\w+)\](\s*=\s*)(\w.+);\s*$/,
+    replacement: "#{name}[\\1]\\2rb_tr_handle_for_managed#{leaking_str}(\\3);"
+  }
+end
+
+def read_write_array(name, leaking)
+  [read_array(name), 
+   write_array(name, leaking)]
 end
 
 PATCHED_FILES = {
@@ -246,7 +268,8 @@ PATCHED_FILES = {
       {
         match: /if \(stmt_wrapper->refcount == 0\) {/,
         replacement: MYSQL2_FREE_STATEMENT
-      }
+      },
+      *read_write_array('params_enc', true)
     ]
   },
   'byebug.c' => {
