ae2680c5528063233321e97e1f52af237b5e5885
Benoit Daloze <eregontp@gmail.com>
2017-10-17 12:07:36 +0200

    Add a helper to resolve platform-dependent types

--- src/main/ruby/core/posix.rb
+++ src/main/ruby/core/posix.rb
@@ -16,14 +16,14 @@ module Truffle::POSIX
     :uint => :uint32,
   }
 
-  def self.resolve_type(type)
+  def self.to_nfi_type(type)
     TYPES.fetch(type, type)
   end
 
   def self.attach_function(name, argument_types, return_type)
     func = LIBC[name]
-    return_type = resolve_type(return_type)
-    argument_types = argument_types.map { |type| resolve_type(type) }
+    return_type = to_nfi_type(return_type)
+    argument_types = argument_types.map { |type| to_nfi_type(type) }
     bound_func = func.bind("(#{argument_types.join(',')}):#{return_type}")
 
     define_singleton_method(name) { |*args|
@@ -31,7 +31,11 @@ module Truffle::POSIX
     }
   end
 
-  mode_t = Rubinius::Config['rbx.platform.typedef.mode_t'].to_sym
+  def self.resolve_type(type)
+    Rubinius::Config["rbx.platform.typedef.#{type}"].to_sym
+  end
+
+  mode_t = resolve_type('mode_t')
 
   attach_function :access, [:string, :int], :int
   attach_function :chmod, [:string, mode_t], :int
