e4cd754a76a3c9e108d1c12385a24858f6ed489d
Kevin Menard <kevin.j.menard@oracle.com>
2017-10-19 22:38:31 -0400

    Add tests for the unf_ext gem (native extension using C++).

---
+++ test/truffle/cexts/unf_ext/test.rb
@@ -0,0 +1,9 @@
+require 'unf_ext'
+
+normalizer = UNF::Normalizer.new
+str = 'garçon'
+
+abort unless normalizer.normalize(str, :nfc).bytes == [103, 97, 114, 195, 167, 111, 110]
+abort unless normalizer.normalize(str, :nfkc).bytes == [103, 97, 114, 195, 167, 111, 110]
+abort unless normalizer.normalize(str, :nfd).bytes == [103, 97, 114, 99, 204, 167, 111, 110]
+abort unless normalizer.normalize(str, :nfkd).bytes == [103, 97, 114, 99, 204, 167, 111, 110]
