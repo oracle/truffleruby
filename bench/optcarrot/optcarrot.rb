78c32fc855906c5c12fc461ac4cf4b44d8f98e21
Chris Seaton <chris.seaton@oracle.com>
2017-10-25 01:01:18 +0100

    Rubinius needs some shims to run optcarrot

--- bench/optcarrot/optcarrot.rb
+++ bench/optcarrot/optcarrot.rb
@@ -1,5 +1,8 @@
 require_relative File.expand_path('../lib/optcarrot', __FILE__)
 
+# Accomodates Rubinius
+require_relative 'tools/shim' if RUBY_ENGINE == 'rbx'
+
 rom = File.expand_path('../examples/Lan_Master.nes', __FILE__)
 nes = Optcarrot::NES.new ['--headless', rom]
 nes.reset
