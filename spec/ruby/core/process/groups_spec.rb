eece9415f0f1bad9ca8c6241a861e1dd65264b1f
Benoit Daloze <eregontp@gmail.com>
2017-11-12 12:44:57 +0100

    Organize the Process.groups= spec

--- spec/ruby/core/process/groups_spec.rb
+++ spec/ruby/core/process/groups_spec.rb
@@ -15,16 +15,19 @@ end
 
 describe "Process.groups=" do
   platform_is_not :windows do
-    # NOTE: This is kind of sketchy.
-    it "sets the list of gids of groups in the supplemental group access list" do
-      groups = Process.groups
-      if Process.uid == 0
+    as_superuser do
+      it "sets the list of gids of groups in the supplemental group access list" do
+        groups = Process.groups
         Process.groups = []
         Process.groups.should == []
         Process.groups = groups
         Process.groups.sort.should == groups.sort
-      else
-        platform_is :aix do
+      end
+    end
+
+    as_user do
+      platform_is :aix do
+        it "sets the list of gids of groups in the supplemental group access list" do
           # setgroups() is not part of the POSIX standard,
           # so its behavior varies from OS to OS.  AIX allows a non-root
           # process to set the supplementary group IDs, as long as
@@ -34,6 +37,7 @@ describe "Process.groups=" do
           # it should no longer be able to set any supplementary
           # group IDs, even if it originally belonged to them.
           # It should only be able to set its primary group ID.
+          groups = Process.groups
           Process.groups = groups
           Process.groups.sort.should == groups.sort
           Process.groups = []
@@ -45,8 +49,14 @@ describe "Process.groups=" do
             lambda { Process.groups = supplementary }.should raise_error(Errno::EPERM)
           end
         end
-        platform_is_not :aix do
-          lambda { Process.groups = [] }.should raise_error(Errno::EPERM)
+      end
+
+      platform_is_not :aix do
+        it "raises Errno::EPERM" do
+          groups = Process.groups
+          lambda {
+            Process.groups = groups
+          }.should raise_error(Errno::EPERM)
         end
       end
     end
