6e5a8fc6ec95c0f6c030ecfd3c937cd7db7a2634
Benoit Daloze <eregontp@gmail.com>
2017-11-19 20:44:25 +0100

    Fix IO#sysread to not retry read()

--- src/main/ruby/core/io.rb
+++ src/main/ruby/core/io.rb
@@ -2005,7 +2005,7 @@ class IO
     end
 
     begin
-      str = Truffle::POSIX.read_string(@descriptor, size, true)
+      str = Truffle::POSIX.read_string(@descriptor, size, false)
     rescue Errno::EAGAIN
       if exception
         raise EAGAINWaitReadable
@@ -2446,7 +2446,7 @@ class IO
     flush
     raise IOError unless @ibuffer.empty?
 
-    str = Truffle::POSIX.read_string(@descriptor, number_of_bytes)
+    str = Truffle::POSIX.read_string(@descriptor, number_of_bytes, false)
     raise EOFError if str.nil?
 
     unless undefined.equal? buffer
