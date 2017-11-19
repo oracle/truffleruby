2fb719027839b18651e9cdf7402d78b47fb31129
Benoit Daloze <eregontp@gmail.com>
2017-11-19 20:05:30 +0100

    Add specs that IO#{read,write}_nonblock sets the IO to nonblock mode

--- spec/ruby/core/io/write_nonblock_spec.rb
+++ spec/ruby/core/io/write_nonblock_spec.rb
@@ -73,4 +73,10 @@ describe 'IO#write_nonblock' do
     end
   end
 
+  it 'sets the IO in nonblock mode' do
+    require 'io/nonblock'
+    @write.nonblock?.should == false
+    @write.write_nonblock('a')
+    @write.nonblock?.should == true
+  end
 end
