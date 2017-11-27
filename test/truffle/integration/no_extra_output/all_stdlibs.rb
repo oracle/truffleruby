2d0af48b71d904bcf60578d26db59f1aeb542f25
Benoit Daloze <eregontp@gmail.com>
2017-11-27 16:13:42 +0100

    Add an integration test verifying there is no extra output

---
+++ test/truffle/integration/no_extra_output/all_stdlibs.rb
@@ -0,0 +1,35 @@
+def glob(pattern)
+  files = Dir.glob(pattern)
+  raise "no libraries found with #{pattern}" if files.empty?
+  files
+end
+
+stdlibs = glob('lib/mri/*.{rb,su}').map { |file|
+  File.basename(file, '.*')
+}
+
+glob('lib/truffle/*.rb').map { |file|
+  lib = File.basename(file, '.*')
+  stdlibs << lib unless lib.end_with? '-stubs'
+}
+
+glob('lib/mri/net/*.rb').map { |file| File.basename(file, '.*') }.each { |file|
+  stdlibs << "net/#{file}"
+}
+
+glob('lib/rubysl/rubysl-*') { |dir|
+  stdlibs << File.basename(dir).sub(/^rubysl-/, '')
+}
+
+stdlibs += %w[json]
+
+ignore = %w[continuation debug mathn profile psych_jars shell]
+ignore += %w[java]
+
+stdlibs -= ignore
+
+stdlibs.uniq!
+
+stdlibs.each { |lib| require lib }
+
+puts 3 * 4
