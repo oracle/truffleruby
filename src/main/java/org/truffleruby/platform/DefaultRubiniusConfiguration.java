32ed51e0ee5926f6ccbc7ed89916ec11179b8923
Benoit Daloze <eregontp@gmail.com>
2017-11-29 13:52:52 +0100

    Remove redundant config which already exists in every platform-specifig config

--- src/main/java/org/truffleruby/platform/DefaultRubiniusConfiguration.java
+++ src/main/java/org/truffleruby/platform/DefaultRubiniusConfiguration.java
@@ -47,13 +47,8 @@ import org.truffleruby.core.rope.RopeOperations;
 import org.truffleruby.core.string.StringOperations;
 
 import java.math.BigInteger;
-import java.util.Arrays;
 
 public abstract class DefaultRubiniusConfiguration {
-
-    private static final int SIZE_OF_INT = 4;
-    private static final int SIZE_OF_LONG = 8;
-    private static final int SIZE_OF_POINTER = 8;
 
     public static void load(RubiniusConfiguration configuration, RubyContext context) {
         for (Fcntl fcntl : Fcntl.values()) {
@@ -67,72 +62,6 @@ public abstract class DefaultRubiniusConfiguration {
                 configuration.config("rbx.platform.file." + openFlag.name(), openFlag.intValue());
             }
         }
-
-        configuration.config("rbx.platform.fcntl.O_ACCMODE", OpenFlags.O_RDONLY.intValue()
-                | OpenFlags.O_WRONLY.intValue() | OpenFlags.O_RDWR.intValue());
-
-        configuration.config("rbx.platform.typedef.time_t", context.getSymbolTable().getSymbol("long"));
-
-        configuration.config("rbx.platform.timeval.sizeof", 2 * SIZE_OF_LONG);
-        configuration.config("rbx.platform.timeval.tv_sec.offset", 0);
-        configuration.config("rbx.platform.timeval.tv_sec.size", SIZE_OF_LONG);
-        configuration.config("rbx.platform.timeval.tv_sec.type", context.getSymbolTable().getSymbol("time_t"));
-        configuration.config("rbx.platform.timeval.tv_usec.offset", SIZE_OF_LONG);
-        configuration.config("rbx.platform.timeval.tv_usec.size", SIZE_OF_LONG);
-        configuration.config("rbx.platform.timeval.tv_usec.type", context.getSymbolTable().getSymbol("time_t"));
-
-        /*
-         * struct linger {
-         *     int              l_onoff;
-         *     int              l_linger;
-         * };
-         */
-
-        int lingerOffset = 0;
-
-        for (String field : Arrays.asList("l_onoff", "l_linger")) {
-            configuration.config("rbx.platform.linger." + field + ".offset", lingerOffset);
-            configuration.config("rbx.platform.linger." + field + ".size", SIZE_OF_INT);
-            configuration.config("rbx.platform.linger." + field + ".type", context.getSymbolTable().getSymbol("int"));
-            lingerOffset += SIZE_OF_INT;
-        }
-
-        configuration.config("rbx.platform.linger.sizeof", lingerOffset);
-
-        /*
-         * struct servent {
-         *     char  *s_name;
-         *     char **s_aliases;
-         *     int    s_port;
-         *     char  *s_proto;
-         * };
-         */
-
-        configuration.config("rbx.platform.servent.sizeof", 3 * SIZE_OF_POINTER + SIZE_OF_INT);
-
-        configuration.config("rbx.platform.servent.s_name.offset", 0);
-        configuration.config("rbx.platform.servent.s_name.size", SIZE_OF_POINTER);
-        configuration.config("rbx.platform.servent.s_name.type", context.getSymbolTable().getSymbol("pointer"));
-
-        configuration.config("rbx.platform.servent.s_aliases.offset", SIZE_OF_POINTER);
-        configuration.config("rbx.platform.servent.s_aliases.size", SIZE_OF_POINTER);
-        configuration.config("rbx.platform.servent.s_aliases.type", context.getSymbolTable().getSymbol("pointer"));
-
-        configuration.config("rbx.platform.servent.s_port.offset", 2 * SIZE_OF_POINTER);
-        configuration.config("rbx.platform.servent.s_port.size", SIZE_OF_INT);
-        configuration.config("rbx.platform.servent.s_port.type", context.getSymbolTable().getSymbol("int"));
-
-        configuration.config("rbx.platform.servent.s_proto.offset", 2 * SIZE_OF_POINTER + SIZE_OF_INT);
-        configuration.config("rbx.platform.servent.s_proto.size", SIZE_OF_POINTER);
-        configuration.config("rbx.platform.servent.s_proto.type", context.getSymbolTable().getSymbol("pointer"));
-
-        configuration.config("rbx.platform.io.SEEK_SET", 0);
-        configuration.config("rbx.platform.io.SEEK_CUR", 1);
-        configuration.config("rbx.platform.io.SEEK_END", 2);
-
-        configuration.config("rbx.platform.socket.AI_PASSIVE", string(context, "1"));
-        configuration.config("rbx.platform.socket.AF_UNSPEC", string(context, "0"));
-        configuration.config("rbx.platform.socket.SOCK_STREAM", string(context, "1"));
     }
 
     protected static DynamicObject newBignum(RubyContext context, String value) {
