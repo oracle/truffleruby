88210832f5f6e0e388e64d2182989575013589b5
Chris Seaton <chris.seaton@oracle.com>
2017-12-04 22:05:50 +0000

    Remove dead code in complex

--- lib/rubysl/complex.rb
+++ lib/rubysl/complex.rb
@@ -26,11 +26,6 @@
 
 require 'cmath'
 
-unless defined?(Math.exp!)
-  Object.instance_eval{remove_const :Math}
-  Math = CMath
-end
-
 def Complex.generic? (other)
   other.kind_of?(Integer) ||
   other.kind_of?(Float)   ||
