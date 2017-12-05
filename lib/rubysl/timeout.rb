9e960afaef6f83ed70c2a89d786010617a0a358f
Chris Seaton <chris.seaton@oracle.com>
2017-12-05 00:28:44 +0000

    Remove dead test code in rubysl/timeout

--- lib/rubysl/timeout.rb
+++ lib/rubysl/timeout.rb
@@ -173,25 +173,3 @@ end
 # earlier versions of timeout.rb.
 
 TimeoutError = Timeout::Error # :nodoc:
-
-if __FILE__ == $0
-  p timeout(5) {
-    45
-  }
-  p timeout(5, TimeoutError) {
-    45
-  }
-  p timeout(nil) {
-    54
-  }
-  p timeout(0) {
-    54
-  }
-  p timeout(5) {
-    loop {
-      p 10
-      sleep 1
-    }
-  }
-end
-
