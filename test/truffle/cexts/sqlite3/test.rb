d7c9331cdbc7a716f7c691f9fb7497513766e0c3
Kevin Menard <kevin.j.menard@oracle.com>
2017-12-07 08:59:46 -0500

    Tidy some things up.

--- test/truffle/cexts/sqlite3/test.rb
+++ test/truffle/cexts/sqlite3/test.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 
 db = SQLite3::Database.new(':memory:')
 
-db.execute( 'CREATE TABLE test(id INTEGER, name VARCHAR(20) NOT NULL)')
+db.execute('CREATE TABLE test(id INTEGER, name VARCHAR(20) NOT NULL)')
 db.execute('INSERT INTO test VALUES(1, "Alice")')
 db.execute('INSERT INTO test VALUES(2, "Bob")')
 
