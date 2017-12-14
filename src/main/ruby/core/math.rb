16a32eed5001cb1ebfc071b3436a3262117694b6
Benoit Daloze <eregontp@gmail.com>
2017-12-14 15:01:34 +0100

    Optimize Math.hypot for integers
    * The native hypot() is slow and useless since we have exact numbers.
    * Use sqrt(a*a + b*b) instead for integers.
    * Around a 10x speedup (11s => 1.2s) on
      https://github.com/eregon/adventofcode/commit/edabad52

--- src/main/ruby/core/math.rb
+++ src/main/ruby/core/math.rb
@@ -15,6 +15,15 @@ module Math
 
   module_function
 
+  def hypot(a, b)
+    if Integer === a and Integer === b
+      # Much faster (~10x) than calling the Math.hypot() / hypot(3)
+      Math.sqrt(a*a + b*b)
+    else
+      Truffle.invoke_primitive :math_hypot, a, b
+    end
+  end
+
   def frexp(x)
     Truffle.primitive :math_frexp
     frexp Rubinius::Type.coerce_to_float(x)
