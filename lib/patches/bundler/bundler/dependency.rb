f9c2a259c538570b3550c481f63c6df2ef4cbdeb
Benoit Daloze <eregontp@gmail.com>
2017-12-15 23:16:32 +0100

    Fix warnings when using bundler
    * Also verifies the constants exist as we expect.

--- lib/patches/bundler/bundler/dependency.rb
+++ lib/patches/bundler/bundler/dependency.rb
@@ -4,9 +4,10 @@ class Bundler::Dependency
 
   # TruffleRuby: add record for truffleruby
   const_set :PLATFORM_MAP,
-            PLATFORM_MAP.merge(truffleruby: Gem::Platform::RUBY)
+            remove_const(:PLATFORM_MAP).merge(truffleruby: Gem::Platform::RUBY)
 
   # TruffleRuby: recompute REVERSE_PLATFORM_MAP
+  remove_const :REVERSE_PLATFORM_MAP
   const_set(:REVERSE_PLATFORM_MAP, {}.tap do |reverse_platform_map|
     Bundler::Dependency::PLATFORM_MAP.each do |key, value|
       reverse_platform_map[value] ||= []
@@ -17,4 +18,3 @@ class Bundler::Dependency
   end.freeze)
 
 end
-
