53ec643895ce9a930b23c24bfdeca71848fb9806
Benoit Daloze <eregontp@gmail.com>
2017-12-15 23:34:23 +0100

    Update copyrights

--- test/truffle/integration/finalizers/define_finalizer.rb
+++ test/truffle/integration/finalizers/define_finalizer.rb
@@ -1,3 +1,11 @@
+# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
 # Getting MRI to run a finalizer for a test seems problematic, due to the
 # conservative GC and the separate C and Ruby stacks.
 
