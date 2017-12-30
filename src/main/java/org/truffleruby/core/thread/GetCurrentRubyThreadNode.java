102cf39890b6b9177099fa8d578a5e232110497a
Benoit Daloze <eregontp@gmail.com>
2017-12-30 18:32:57 +0100

    Fix Thread.current when using a Fiber pool
    * Fixes https://github.com/graalvm/truffleruby/issues/903.

--- src/main/java/org/truffleruby/core/thread/GetCurrentRubyThreadNode.java
+++ src/main/java/org/truffleruby/core/thread/GetCurrentRubyThreadNode.java
@@ -9,6 +9,7 @@
  */
 package org.truffleruby.core.thread;
 
+import org.truffleruby.Layouts;
 import org.truffleruby.language.RubyNode;
 
 import com.oracle.truffle.api.dsl.Cached;
@@ -24,13 +25,24 @@ public abstract class GetCurrentRubyThreadNode extends RubyNode {
 
     public abstract DynamicObject executeGetRubyThread(VirtualFrame frame);
 
-    @Specialization(
-            guards = "getCurrentJavaThread(frame) == cachedJavaThread",
-            limit = "getCacheLimit()"
-    )
+    /*
+     * Note: the frame argument is used as a dynamic parameter here. Otherwise, The TruffleDSL
+     * assumes cached parameters or calls with only cached arguments (including calls with no
+     * arguments) never change.
+     */
+    /*
+     * Note: we need to check that the Fiber is still running on a Java thread to cache based on the
+     * Java thread. If the Fiber finished its execution, the Java thread can be reused for another
+     * Fiber belonging to another Ruby Thread, due to using a thread pool for Fibers.
+     */
+    @Specialization(guards = {
+            "getCurrentJavaThread(frame) == cachedJavaThread",
+            "hasThread(frame, cachedFiber)"
+    }, limit = "getCacheLimit()")
     protected DynamicObject getRubyThreadCached(VirtualFrame frame,
             @Cached("getCurrentJavaThread(frame)") Thread cachedJavaThread,
-            @Cached("getCurrentRubyThread(frame)") DynamicObject cachedRubyThread) {
+            @Cached("getCurrentRubyThread(frame)") DynamicObject cachedRubyThread,
+            @Cached("getCurrentFiber(cachedRubyThread)") DynamicObject cachedFiber) {
         return cachedRubyThread;
     }
 
@@ -45,6 +57,14 @@ public abstract class GetCurrentRubyThreadNode extends RubyNode {
 
     protected DynamicObject getCurrentRubyThread(VirtualFrame frame) {
         return getContext().getThreadManager().getCurrentThread();
+    }
+
+    protected DynamicObject getCurrentFiber(DynamicObject currentRubyThread) {
+        return Layouts.THREAD.getFiberManager(currentRubyThread).getCurrentFiber();
+    }
+
+    protected boolean hasThread(VirtualFrame frame, DynamicObject fiber) {
+        return Layouts.FIBER.getThread(fiber) != null;
     }
 
     protected int getCacheLimit() {
