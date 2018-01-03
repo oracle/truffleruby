b21cd1e5cade6f2e3a0b2a07d6e85f1de6cb1bf4
Chris Seaton <chris.seaton@oracle.com>
2018-01-03 11:29:23 +0000

    Remove old comment

--- src/main/java/org/truffleruby/core/rope/Rope.java
+++ src/main/java/org/truffleruby/core/rope/Rope.java
@@ -95,7 +95,6 @@ public abstract class Rope {
     @TruffleBoundary
     public int hashCode() {
         if (!isHashCodeCalculated()) {
-            // FIXME CS 2-Jan-18 RopeOperations.hashForRange uses Hashing.rawHash which isn't randomised per-process!
             hashCode = RopeOperations.hashForRange(this, 1, 0, byteLength);
         }
 
