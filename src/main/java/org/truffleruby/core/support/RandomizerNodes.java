52b06739e3022d6d259f4620b20292d359e3591c
Chris Seaton <chris.seaton@oracle.com>
2018-01-03 22:23:46 +0000

    Move the comment on /dev/urandom

--- src/main/java/org/truffleruby/core/support/RandomizerNodes.java
+++ src/main/java/org/truffleruby/core/support/RandomizerNodes.java
@@ -291,7 +291,6 @@ public abstract class RandomizerNodes {
 
         @TruffleBoundary
         public static BigInteger randomSeedBigInteger(RubyContext context) {
-            // MRI uses /dev/urandom each time for this, but Java only makes it easy to use /dev/random which blocks too much in practice
             byte[] seed = context.getRandomSeedBytes(DEFAULT_SEED_CNT * 4);
             return new BigInteger(seed).abs();
         }
