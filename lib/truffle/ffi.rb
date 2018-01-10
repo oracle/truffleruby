dc91cf33093ecacf0c57fc44cc03d2e182d6e507
Benoit Daloze <eregontp@gmail.com>
2018-01-10 17:21:26 +0100

    Move FFI::Struct out of core

--- lib/truffle/ffi.rb
+++ lib/truffle/ffi.rb
@@ -6,6 +6,8 @@
 # GNU General Public License version 2
 # GNU Lesser General Public License version 2.1
 
+require_relative 'truffle/ffi/ffi_struct'
+
 # Minimal support needed to run ffi/library
 module FFI
   Platform = Truffle::FFI::Platform
