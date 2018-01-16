8087b683b18c2fe02947f2747857f00d89c4b922
Duncan MacGregor <duncan.macgregor@oracle.com>
2018-01-16 13:30:45 +0000

    Refactor global variable write.

--- src/main/java/org/truffleruby/language/globals/WriteGlobalVariableNode.java
+++ src/main/java/org/truffleruby/language/globals/WriteGlobalVariableNode.java
@@ -15,10 +15,8 @@ import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 import org.truffleruby.Layouts;
-import org.truffleruby.core.basicobject.BasicObjectNodes.ReferenceEqualNode;
 import org.truffleruby.core.binding.BindingNodes;
 import org.truffleruby.language.RubyNode;
-import org.truffleruby.language.objects.shared.WriteBarrierNode;
 import org.truffleruby.language.yield.YieldNode;
 
 @NodeChild(value = "value")
@@ -26,43 +24,15 @@ public abstract class WriteGlobalVariableNode extends RubyNode {
 
     private final String name;
 
-    @Child protected ReferenceEqualNode referenceEqualNode = ReferenceEqualNode.create();
-    @Child protected WriteBarrierNode writeBarrierNode = WriteBarrierNode.create();
-
     public WriteGlobalVariableNode(String name) {
         this.name = name;
     }
 
-    @Specialization(guards = { "storage.isSimple()", "referenceEqualNode.executeReferenceEqual(value, previousValue)" },
-                    assumptions = "storage.getUnchangedAssumption()")
-    public Object writeTryToKeepConstant(Object value,
-                    @Cached("getStorage()") GlobalVariableStorage storage,
-                    @Cached("storage.getValue()") Object previousValue) {
-        // NOTE: we still do the volatile write to get the proper memory barrier,
-        // as the global variable could be used as a publication mechanism.
-        storage.setValueInternal(value);
-        return previousValue;
-    }
-
-    @Specialization(guards = { "storage.isSimple()", "storage.isAssumeConstant()" },
-                    assumptions = "storage.getUnchangedAssumption()")
-    public Object writeAssumeConstant(Object value,
-                    @Cached("getStorage()") GlobalVariableStorage storage) {
-        if (getContext().getSharedObjects().isSharing()) {
-            writeBarrierNode.executeWriteBarrier(value);
-        }
-        storage.setValueInternal(value);
-        storage.updateAssumeConstant(getContext());
-        return value;
-    }
-
-    @Specialization(guards = { "storage.isSimple()", "!storage.isAssumeConstant()" }, replaces = "writeAssumeConstant")
-    public Object write(Object value,
-                    @Cached("getStorage()") GlobalVariableStorage storage) {
-        if (getContext().getSharedObjects().isSharing()) {
-            writeBarrierNode.executeWriteBarrier(value);
-        }
-        storage.setValueInternal(value);
+    @Specialization(guards = "storage.isSimple()")
+    public Object write(VirtualFrame frame, Object value,
+            @Cached("getStorage()") GlobalVariableStorage storage,
+            @Cached("createSimpleNode()") WriteSimpleGlobalVariableNode simpleNode) {
+        simpleNode.execute(value);
         return value;
     }
 
@@ -86,6 +56,10 @@ public abstract class WriteGlobalVariableNode extends RubyNode {
 
     protected int setterArity(GlobalVariableStorage storage) {
         return Layouts.PROC.getSharedMethodInfo(storage.getSetter()).getArity().getArityNumber();
+    }
+
+    protected WriteSimpleGlobalVariableNode createSimpleNode() {
+        return WriteSimpleGlobalVariableNodeGen.create(getStorage(), null);
     }
 
     protected GlobalVariableStorage getStorage() {
