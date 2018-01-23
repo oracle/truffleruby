0793ec84c51cc964e9293a97cef1c13c24b54139
Petr Chalupa <petr.chalupa@oracle.com>
2018-01-23 14:41:54 +0100

    Enable MRI zlib tests

---
+++ test/mri/excludes/TestZlibGzipWriter.rb
@@ -0,0 +1,4 @@
+exclude :test_invalid_new, "needs investigation"
+exclude :test_open, "needs investigation"
+exclude :test_write, "needs investigation"
+exclude :test_writer_wrap, "needs investigation"
