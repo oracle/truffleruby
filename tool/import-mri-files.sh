5cf9b6b77278c446fb54f25cbe43bf8b0a0902f7
Chris Seaton <chris.seaton@oracle.com>
2018-01-30 00:17:33 +0000

    Update import MRI tool for zlib

--- tool/import-mri-files.sh
+++ tool/import-mri-files.sh
@@ -12,6 +12,7 @@ rm lib/mri/weakref.rb
 rm src/main/c/openssl/*.{c,h}
 cp ../ruby/ext/openssl/*.{c,h} src/main/c/openssl
 cp ../ruby/ext/syslog/*.{c,rb} src/main/c/syslog
+cp ../ruby/ext/zlib/*.{c,rb} src/main/c/zlib
 cp -r ../ruby/ext/openssl/lib/* lib/mri
 cp -r ../ruby/ext/bigdecimal/lib/bigdecimal lib/mri
 cp -r ../ruby/ext/psych/lib/psych lib/mri
