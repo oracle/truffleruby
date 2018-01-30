da64ad627b9268645700b5f2b3103ec90883c309
Chris Seaton <chris.seaton@oracle.com>
2018-01-30 22:22:16 +0000

    More specs for TOPLEVEL_BINDING

--- spec/ruby/language/predefined/toplevel_binding_spec.rb
+++ spec/ruby/language/predefined/toplevel_binding_spec.rb
@@ -1,15 +1,24 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "The TOPLEVEL_BINDING constant" do
+  before :all do
+    @binding_toplevel_variables = eval(ruby_exe(fixture(__FILE__, "toplevel_binding_variables.rb")))
+    @binding_toplevel_id = eval(ruby_exe(fixture(__FILE__, "toplevel_binding_id.rb")))
+  end
+  
   it "includes local variables defined in the same file" do
-    eval(ruby_exe(fixture(__FILE__, "toplevel_binding.rb"))).should include(:foo)
+    @binding_toplevel_variables.should include(:foo)
   end
 
   it "does not include local variables defined in a required file" do
-    eval(ruby_exe(fixture(__FILE__, "toplevel_binding.rb"))).should_not include(:bar)
+    @binding_toplevel_variables.should_not include(:bar)
   end
 
   it "does not include local variables defined in eval" do
-    eval(ruby_exe(fixture(__FILE__, "toplevel_binding.rb"))).should_not include(:baz)
+    @binding_toplevel_variables.should_not include(:baz)
+  end
+
+  it "is always the same object for all top levels" do
+    @binding_toplevel_id.uniq.size.should == 1
   end
 end
