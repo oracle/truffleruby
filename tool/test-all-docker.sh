b9047e8c238ca29d924c8b5aea394bababe3a403
Chris Seaton <chris.seaton@oracle.com>
2018-02-04 14:41:20 +0000

    Add a script to run all Docker tests

--- tool/test-all-docker.sh
+++ tool/test-all-docker.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-docker rmi $(docker images -q)
-docker rm $(docker ps -a -q)
+echo "You may want to wipe your Docker caches before running these"
 
 for c in oraclelinux ubuntu fedora rbenv chruby rvm; do
   pushd test/truffle/docker/$c
-  docker build -t truffleruby-test-$c . --build-arg GRAALVM_VERSION=0.31-dev --build-arg TEST_BRANCH=master
+  docker build -t truffleruby-test-$c . --build-arg GRAALVM_VERSION=$GRAALVM_VERSION --build-arg TEST_BRANCH=master
   popd
 done
 
 for c in oraclelinux ubuntu fedora; do
-  pushd test/truffle/docker/$c
+  pushd tool/docker/$c
   docker build -t truffleruby-dev-$c .
   popd
 done
