7cace0798a4c19156919dd3a79e25c1921e89bc0
Benoit Daloze <eregontp@gmail.com>
2018-02-09 14:57:34 +0100

    Load patching, lazy-rubygems and did_you_mean during context pre-initialization
    * Fix $LOAD_PATH and $LOADED_FEATURES to reflect the new home.

--- src/main/ruby/post-boot/post-boot.rb
+++ src/main/ruby/post-boot/post-boot.rb
@@ -9,46 +9,53 @@
 Truffle::Boot.delay do
   wd = Truffle::Boot.get_option('working_directory')
   Dir.chdir(wd) unless wd.empty?
+end
 
-  unless Truffle::Boot.ruby_home.nil?
-    # Always provided features: ruby --disable-gems -e 'puts $"'
-    begin
-      require 'enumerator'
-      require 'thread'
-      require 'rational'
-      require 'complex'
-      require 'unicode_normalize'
-      if Truffle::Boot.get_option('patching')
-        Truffle::Boot.print_time_metric :'before-patching'
-        require 'truffle/patching'
-        Truffle::Patching.insert_patching_dir 'stdlib', "#{Truffle::Boot.ruby_home}/lib/mri"
-        Truffle::Boot.print_time_metric :'after-patching'
-      end
-    rescue LoadError => e
-      Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
+unless Truffle::Boot.ruby_home.nil?
+  # Always provided features: ruby --disable-gems -e 'puts $"'
+  begin
+    require 'enumerator'
+    require 'thread'
+    require 'rational'
+    require 'complex'
+    require 'unicode_normalize'
+    if Truffle::Boot.get_option('patching')
+      Truffle::Boot.print_time_metric :'before-patching'
+      require 'truffle/patching'
+      # TODO
+      Truffle::Patching.insert_patching_dir 'stdlib', "#{Truffle::Boot.ruby_home}/lib/mri"
+      Truffle::Boot.print_time_metric :'after-patching'
     end
+  rescue LoadError => e
+    Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
+  end
 
-    if Truffle::Boot.get_option 'rubygems'
+  if Truffle::Boot.get_option 'rubygems'
+    Truffle::Boot.delay do
       if Truffle::Boot.resilient_gem_home?
         ENV.delete 'GEM_HOME'
         ENV.delete 'GEM_PATH'
         ENV.delete 'GEM_ROOT'
       end
+    end
 
+    begin
+      Truffle::Boot.print_time_metric :'before-rubygems'
       begin
-        Truffle::Boot.print_time_metric :'before-rubygems'
-        begin
-          if Truffle::Boot.get_option('rubygems.lazy')
-            require 'truffle/lazy-rubygems'
-          else
+        if Truffle::Boot.get_option('rubygems.lazy')
+          require 'truffle/lazy-rubygems'
+        else
+          Truffle::Boot.delay do
             require 'rubygems'
           end
-        ensure
-          Truffle::Boot.print_time_metric :'after-rubygems'
         end
-      rescue LoadError => e
-        Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
-      else
+      ensure
+        Truffle::Boot.print_time_metric :'after-rubygems'
+      end
+    rescue LoadError => e
+      Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
+    else
+      Truffle::Boot.delay do
         # TODO (pitr-ch 17-Feb-2017): remove the warning when we can integrate with ruby managers
         if gem_home = ENV['GEM_HOME']
           bad_gem_home = false
@@ -70,16 +77,30 @@ Truffle::Boot.delay do
                      '`rvm use system`, `rbenv system`, or `chruby system` to clear the environment.'
           end
         end
+      end
 
-        if Truffle::Boot.get_option 'did_you_mean'
-          Truffle::Boot.print_time_metric :'before-did-you-mean'
-          begin
-            $LOAD_PATH << "#{Truffle::Boot.ruby_home}/lib/ruby/gems/#{Truffle::RUBY_BASE_VERSION}/gems/did_you_mean-1.0.0/lib"
-            require 'did_you_mean'
-          rescue LoadError => e
-            Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
-          ensure
-            Truffle::Boot.print_time_metric :'after-did-you-mean'
+      if Truffle::Boot.get_option 'did_you_mean'
+        Truffle::Boot.print_time_metric :'before-did-you-mean'
+        begin
+          $LOAD_PATH << "#{Truffle::Boot.ruby_home}/lib/ruby/gems/#{Truffle::RUBY_BASE_VERSION}/gems/did_you_mean-1.0.0/lib"
+          require 'did_you_mean'
+        rescue LoadError => e
+          Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
+        ensure
+          Truffle::Boot.print_time_metric :'after-did-you-mean'
+        end
+      end
+    end
+  end
+
+  if Truffle::Boot.preinitializing?
+    old_home = Truffle::Boot.ruby_home
+    Truffle::Boot.delay do
+      new_home = Truffle::Boot.ruby_home
+      [$LOAD_PATH, $LOADED_FEATURES].each do |array|
+        array.each do |path|
+          if path.start_with?(old_home)
+            path.replace(new_home + path[old_home.size..-1])
           end
         end
       end
