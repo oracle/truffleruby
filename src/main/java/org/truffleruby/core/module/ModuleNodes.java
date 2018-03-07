ce51ccbc0f8de7d6f3a6992e36f133d3ddc62a1b
Kevin Menard <kevin.j.menard@oracle.com>
2018-03-07 11:54:46 -0500

    Replace newline padding code with something far more efficient and with clearer intent.

--- src/main/java/org/truffleruby/core/module/ModuleNodes.java
+++ src/main/java/org/truffleruby/core/module/ModuleNodes.java
@@ -620,9 +620,9 @@ public abstract class ModuleNodes {
             Encoding encoding = Layouts.STRING.getRope(rubySource).getEncoding();
 
             // TODO (pitr 15-Oct-2015): fix this ugly hack, required for AS, copy-paste
-            final String s = new String(new char[Math.max(line - 1, 0)]);
-            final String space = StringUtils.replace(s, "\0", "\n");
-            Source source = Source.newBuilder(space + code).name(file).mimeType(RubyLanguage.MIME_TYPE).build();
+            final char[] emptyLines = new char[Math.max(line - 1, 0)];
+            Arrays.fill(emptyLines, '\n');
+            Source source = Source.newBuilder(new String(emptyLines) + code).name(file).mimeType(RubyLanguage.MIME_TYPE).build();
 
             final RubyRootNode rootNode = getContext().getCodeLoader().parse(source, encoding, ParserContext.MODULE, callerFrame, true, this);
             final DeclarationContext declarationContext = new DeclarationContext(Visibility.PUBLIC, new FixedDefaultDefinee(module));
