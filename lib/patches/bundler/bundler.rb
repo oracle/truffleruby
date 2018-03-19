3d98b02c9420efda4f24d6f9071346e48968c12a
Petr Chalupa <petr.chalupa@oracle.com>
2018-03-19 12:52:04 +0100

    Update bundler patches to 1.16.x

--- lib/patches/bundler/bundler.rb
+++ lib/patches/bundler/bundler.rb
@@ -1,5 +1,5 @@
 Truffle::Patching.require_original __FILE__
 
-unless Bundler::VERSION =~ /^1\.14\./
-  raise "unsupported bundler version #{Bundler::VERSION}, please use 1.14.6"
+unless Bundler::VERSION =~ /^1\.16\./
+  raise "unsupported bundler version #{Bundler::VERSION}, please use 1.16.x"
 end
