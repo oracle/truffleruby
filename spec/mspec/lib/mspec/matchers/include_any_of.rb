c5c3dd585db23dd19abd08f0855dcdc255b6b481
Chris Seaton <chris.seaton@oracle.com>
2018-03-22 16:45:57 +0000

    include_any_of matcher for MSpec

---
+++ spec/mspec/lib/mspec/matchers/include_any_of.rb
@@ -0,0 +1,31 @@
+class IncludeAnyOfMatcher
+  def initialize(*expected)
+    @expected = expected
+  end
+
+  def matches?(actual)
+    @actual = actual
+    @matched = []
+    @expected.each do |e|
+      if @actual.include?(e)
+        @matched.push e
+      end
+    end
+    return !@matched.empty?
+  end
+
+  def failure_message
+    ["Expected #{@actual.inspect}", "to include any of #{@expected.inspect}"]
+  end
+
+  def negative_failure_message
+    ["Expected #{@actual.inspect}", "not to include any of #{@matched.inspect}"]
+  end
+end
+
+# Cannot override #include_any_of at the toplevel in MRI
+module MSpecMatchers
+  private def include_any_of(*expected)
+    IncludeAnyOfMatcher.new(*expected)
+  end
+end
