b3b8dc1bbf2ea10697c849e06cb7bde2eb8cfabc
Benoit Daloze <eregontp@gmail.com>
2018-03-29 19:51:05 +0200

    Add initial implementation of the new TCK

---
+++ src/test/ruby/recursion.rb
@@ -0,0 +1,39 @@
+# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
+def fact(n)
+  raise ArgumentError if n < 0
+  (1..n).reduce(1, :*)
+end
+
+def fib(n)
+  raise ArgumentError if n < 0
+  if n < 2
+    n
+  else
+    fib(n-2) + fib(n-1)
+  end
+end
+
+def ack(m, n)
+  if m == 0
+    n + 1
+  elsif n == 0
+    ack(m - 1, 1)
+  else
+    ack(m - 1, ack(m, n - 1))
+  end
+end
+
+-> {
+  [
+    fact(10),
+    fib(10),
+    ack(3, 4)
+  ]
+}
