03c0a1273dfb47a925fe3cc41361013858eabd13
Chris Seaton <chris.seaton@oracle.com>
2018-03-31 19:59:09 +0100

    Make TRUFFLERUBY_CEXT_ENABLED set by default.

--- test/truffle/cexts/unf_ext/unf_ext.sh
+++ test/truffle/cexts/unf_ext/unf_ext.sh
@@ -4,7 +4,6 @@ source test/truffle/common.sh.inc
 
 jt gem-test-pack
 
-export TRUFFLERUBY_CEXT_ENABLED=true
 jt ruby -S gem install --local "$(jt gem-test-pack)/gem-cache/unf_ext-0.0.7.4.gem" -V -N --backtrace
 
 jt ruby test/truffle/cexts/unf_ext/test.rb
