64b8be6acaf1f1111b20a6e82c0b5b906af27cbf
Chris Seaton <chris.seaton@oracle.com>
2018-04-01 02:26:15 +0100

    Update Sulong.

--- lib/cext/truffle.h
+++ lib/cext/truffle.h
@@ -61,14 +61,14 @@ void *truffle_assign_managed(void *dst, void *managed);
 
 void *truffle_address_to_function(void *address);
 
-// Invoke: deprecated, use ployglot_get_member + function pointer call instead
-void *truffle_invoke(void *object, const char *name, ...);
-int truffle_invoke_i(void *object, const char *name, ...);
-long truffle_invoke_l(void *object, const char *name, ...);
-char truffle_invoke_c(void *object, const char *name, ...);
-float truffle_invoke_f(void *object, const char *name, ...);
-double truffle_invoke_d(void *object, const char *name, ...);
-bool truffle_invoke_b(void *object, const char *name, ...);
+// Invoke:
+void *truffle_invoke(void *object, const char *name, ...);    // renamed to polyglot_invoke
+int truffle_invoke_i(void *object, const char *name, ...);    // deprecated, use polyglot_as_i32(polyglot_invoke(...))
+long truffle_invoke_l(void *object, const char *name, ...);   // deprecated, use polyglot_as_i64(polyglot_invoke(...))
+char truffle_invoke_c(void *object, const char *name, ...);   // deprecated, use polyglot_as_i8(polyglot_invoke(...))
+float truffle_invoke_f(void *object, const char *name, ...);  // deprecated, use polyglot_as_float(polyglot_invoke(...))
+double truffle_invoke_d(void *object, const char *name, ...); // deprecated, use polyglot_as_double(polyglot_invoke(...))
+bool truffle_invoke_b(void *object, const char *name, ...);   // deprecated, use polyglot_as_boolean(polyglot_invoke(...))
 
 #if defined(__cplusplus)
 }
