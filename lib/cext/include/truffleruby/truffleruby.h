4896fbae1ba7c3517248473393afd45c7ecd66bd
Chris Seaton <chris.seaton@oracle.com>
2018-04-10 18:58:12 +0100

    Handle functions that pass through native pointers

--- lib/cext/include/truffleruby/truffleruby.h
+++ lib/cext/include/truffleruby/truffleruby.h
@@ -38,8 +38,11 @@ int rb_tr_flags(VALUE value);
 
 void *rb_tr_handle_for_managed(VALUE managed);
 void *rb_tr_handle_for_managed_leaking(VALUE managed);
+void *rb_tr_handle_if_managed(VALUE managed);
+void *rb_tr_handle_if_managed_leaking(VALUE managed);
 VALUE rb_tr_managed_from_handle_or_null(void *handle);
 VALUE rb_tr_managed_from_handle(void *handle);
+VALUE rb_tr_managed_if_handle(void *handle);
 void rb_tr_release_handle(void *handle);
 
 bool rb_tr_obj_taintable_p(VALUE object);
