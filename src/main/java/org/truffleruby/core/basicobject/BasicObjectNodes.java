c387c8e2e2e95449e3dde7c482adb660de41c7e5
Chris Seaton <chris.seaton@oracle.com>
2018-04-23 22:55:32 +0100

    Use a helper to create sources for eval

--- src/main/java/org/truffleruby/core/basicobject/BasicObjectNodes.java
+++ src/main/java/org/truffleruby/core/basicobject/BasicObjectNodes.java
@@ -293,7 +293,7 @@ public abstract class BasicObjectNodes {
         }
 
         private Source loadFragment(String fragment, String name) {
-            return Source.newBuilder(fragment).name(name.intern()).mimeType(RubyLanguage.MIME_TYPE).build();
+            return KernelNodes.EvalNode.createEvalSource(fragment, name);
         }
 
     }
