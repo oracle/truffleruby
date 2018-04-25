61a51d1ec8478939a3ce62c834a8167cf929c5d4
Kevin Menard <kevin.j.menard@oracle.com>
2018-04-25 15:24:21 -0400

    Don't rely on load order to define CommonPatches.

--- lib/cext/patches/nio4r_patches.rb
+++ lib/cext/patches/nio4r_patches.rb
@@ -6,6 +6,8 @@
 # GNU General Public License version 2
 # GNU Lesser General Public License version 2.1
 
+require_relative 'common_patches'
+
 class Nio4RPatches < CommonPatches
   PATCHES = {
     gem: 'nio4r',
