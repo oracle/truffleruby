51ba137732958feecdbf3478c303fa575e8e4335
Benoit Daloze <eregontp@gmail.com>
2018-04-26 17:26:46 +0200

    Adapt envutil for MRI tests to pick up the right values for Fixnum min/max

--- src/main/ruby/core/truffle/platform.rb
+++ src/main/ruby/core/truffle/platform.rb
@@ -10,8 +10,8 @@ module Truffle::Platform
   # Used by Truffle::FFI
   L64 = true
 
-  # Used by Range#hash
-  LONG_MAX = 0x7fffffffffffffff
+  LONG_MIN = -0x8000000000000000
+  LONG_MAX =  0x7fffffffffffffff
 
   # jnr-posix hard codes this value
   PATH_MAX = 1024
