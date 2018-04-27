cc31cc567f99dde2b14aa0cc653ce36c9429ba09
Benoit Daloze <eregontp@gmail.com>
2018-04-27 11:12:41 +0200

    Add more specs for -I include_dir

--- spec/ruby/command_line/dash_upper_i_spec.rb
+++ spec/ruby/command_line/dash_upper_i_spec.rb
@@ -8,4 +8,16 @@ describe "The -I command line option" do
   it "adds the path to the load path ($:)" do
     ruby_exe(@script, options: "-I fixtures").should include("fixtures")
   end
+
+  it "adds the path at the front of $LOAD_PATH" do
+    ruby_exe(@script, options: "-I fixtures").lines[0].should include("fixtures")
+  end
+
+  it "adds the path expanded from CWD to $LOAD_PATH" do
+    ruby_exe(@script, options: "-I fixtures").lines[0].should == "#{Dir.pwd}/fixtures\n"
+  end
+
+  it "expands a path from CWD even if it does not exist" do
+    ruby_exe(@script, options: "-I not_exist/not_exist").lines[0].should == "#{Dir.pwd}/not_exist/not_exist\n"
+  end
 end
