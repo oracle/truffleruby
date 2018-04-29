b38c9ac3f31cb2070cf5254909e231f0d261d524
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-04-29 22:25:43 +0200

    Exclude failing MRI tests

--- test/mri/excludes/TestRipper/Generic.rb
+++ test/mri/excludes/TestRipper/Generic.rb
@@ -1,1 +1,4 @@
-exclude :test_parse_files, "needs investigation"
+exclude :"test_parse_files:ext", "needs investigation"
+exclude :"test_parse_files:lib", "needs investigation"
+exclude :"test_parse_files:sample", "needs investigation"
+exclude :"test_parse_files:test", "needs investigation"
