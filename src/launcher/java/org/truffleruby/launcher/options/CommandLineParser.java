cad03b9916b0cccf3e92236ba4c4f85e5953d3c1
Chris Seaton <chris.seaton@oracle.com>
2018-05-04 17:03:52 +0100

    Lint

--- src/launcher/java/org/truffleruby/launcher/options/CommandLineParser.java
+++ src/launcher/java/org/truffleruby/launcher/options/CommandLineParser.java
@@ -53,7 +53,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.BiConsumer;
-import java.util.logging.Level;
 
 public class CommandLineParser {
 
