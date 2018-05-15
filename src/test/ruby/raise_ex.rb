d0de04d619b79c6be2d358079aa1f4228124daa6
Chris Seaton <chris.seaton@oracle.com>
2018-05-15 14:44:26 +0100

    Comment test code

--- src/test/ruby/raise_ex.rb
+++ src/test/ruby/raise_ex.rb
@@ -1,9 +1,19 @@
+# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
 def shortArg(n)
   if n > 65535
-    raise ArgumentError, "Number too big", caller
+    raise ArgumentError, "Number too big", caller # Using a custom backtrace
   end
 end
+
 def throwMsg(msg)
   raise msg
 end
-shortArg(10)
+
+shortArg 10
