00af0949bb0922010cbf230f25f340e5c9cbab1b
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-06-19 14:16:21 +0200

    Update to latest Truffle

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -8,7 +8,7 @@ suite = {
               # We want tools to be reliably available with TruffleRuby, even with "mx build", so this is a static import.
                 "name": "tools",
                 "subdir": True,
-                "version": "f4658461494b50cafa3c89c6e2f0a11d8a3bfb15",
+                "version": "ff6b87789b5990692333bf1fe4f9a6cabbff0bbd",
                 "urls": [
                     {"url": "https://github.com/graalvm/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
