266203ef4a602dfe623bd1e9d595bbe1373cd5c3
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-07-07 18:07:25 +0200

    Spec that Thread::Backtrace::Location#absolute_path returns a canonical path

--- spec/ruby/core/thread/backtrace/location/absolute_path_spec.rb
+++ spec/ruby/core/thread/backtrace/location/absolute_path_spec.rb
@@ -9,4 +9,34 @@ describe 'Thread::Backtrace::Location#absolute_path' do
   it 'returns the absolute path of the call frame' do
     @frame.absolute_path.should == File.realpath(__FILE__)
   end
+
+  platform_is_not :windows do
+    before :each do
+      @file = fixture(__FILE__, "absolute_path.rb")
+      @symlink = tmp("symlink.rb")
+      File.symlink(@file, @symlink)
+      ScratchPad.record []
+    end
+
+    after :each do
+      rm_r @symlink
+    end
+
+    it "returns a canonical path without symlinks, even when __FILE__ does not" do
+      realpath = File.realpath(@symlink)
+      realpath.should_not == @symlink
+
+      load @symlink
+      ScratchPad.recorded.should == [@symlink, realpath]
+    end
+
+    it "returns a canonical path without symlinks, even when __FILE__ is removed" do
+      realpath = File.realpath(@symlink)
+      realpath.should_not == @symlink
+
+      ScratchPad << -> { rm_r(@symlink) }
+      load @symlink
+      ScratchPad.recorded.should == [@symlink, realpath]
+    end
+  end
 end
