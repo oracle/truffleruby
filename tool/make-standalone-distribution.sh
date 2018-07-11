7b316a3b3aea5022cb8e96f1e03a6f2318fb1987
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-07-11 18:59:01 +0200

    Update documentataion in make-standalone-distribution.sh

--- tool/make-standalone-distribution.sh
+++ tool/make-standalone-distribution.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# This script creates a standalone native distribution of TruffleRuby and Sulong
-# by using the vm suite and the Ruby installable.
+# This script creates a standalone native-only distribution of TruffleRuby by
+# building a small GraalVM with just Ruby and Sulong using the vm suite.
+# It renames the resulting archive and directory to create a unique name based
+# on the current commit SHA-1 to allow uploading to a common directory.
 
 # To ensure Bash reads the file at once and allow concurrent editing, run with:
 # cat tool/make-standalone-distribution.sh | JAVA_HOME=... bash
@@ -14,27 +16,15 @@ PREFIX="../release"
 
 # Check platform
 case $(uname) in
-  Linux)
-    os=linux
-    ;;
-  Darwin)
-    os=darwin
-    ;;
-  *)
-    echo "unknown platform $(uname)" 1>&2
-    exit 1
-    ;;
+  Linux) os=linux ;;
+  Darwin) os=darwin ;;
+  *) echo "unknown platform $(uname)" 1>&2; exit 1 ;;
 esac
 
 # Check architecture
 case $(uname -m) in
-  x86_64)
-    arch=amd64
-    ;;
-  *)
-    echo "unknown architecture $(uname -m)" 1>&2
-    exit 1
-    ;;
+  x86_64) arch=amd64 ;;
+  *) echo "unknown architecture $(uname -m)" 1>&2; exit 1 ;;
 esac
 
 if [ -z "$JAVA_HOME" ]; then
@@ -55,8 +45,8 @@ PREFIX=$(cd "$PREFIX" && pwd -P)
 mkdir -p "$PREFIX/build"
 cd "$PREFIX/build"
 
-# Always make a copy of the repository, because we do not want any extra file
-# that might be in the current truffleruby repository.
+# Always make a copy of the repository, because we do not want any extra files
+# (e.g., extra gems) that might be in the current truffleruby repository.
 git clone "$original_repo" truffleruby
 
 # Shortcut when running the script locally
@@ -84,6 +74,7 @@ mx --disable-polyglot --disable-libpolyglot --force-bash-launchers=lli,native-im
 archive_basename="truffleruby-$version-$os-$arch"
 
 release_home="$PREFIX/$archive_basename"
+# Rename the Ruby standalone distribution created by the vm suite
 cp -R mxbuild/$os-$arch/TRUFFLERUBY*/truffleruby* "$release_home"
 
 # Create archive
@@ -99,8 +90,11 @@ fi
 # Test the post-install hook
 "$release_home/lib/truffle/post_install_hook.sh"
 
-# Test it
+# Test the built distribution
+
+# Use the bin/ruby symlink to test that works too
 "$release_home/bin/ruby" -v
 
+# Run all specs
 cd "$build_home"
 AOT_BIN="$release_home/bin/truffleruby" tool/jt.rb test --native --no-home :all
