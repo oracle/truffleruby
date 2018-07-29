e5b74add94cf7db5f25fe3856e42db860ad61bf3
Chris Seaton <chris.seaton@oracle.com>
2018-07-29 01:21:31 +0100

    -X...=true is implicit

--- spec/truffle/options/run_finalizers_on_exit_spec.rb
+++ spec/truffle/options/run_finalizers_on_exit_spec.rb
@@ -1,6 +1,14 @@
+# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0, or
+# GNU General Public License version 2, or
+# GNU Lesser General Public License version 2.1.
+
 require_relative '../../ruby/spec_helper'
 
-describe "The -Xfinalizers.run_on_exit=true option" do
+describe "The -Xfinalizers.run_on_exit option" do
   it "runs finalizers on exit" do
     code = <<~RUBY
     OBJ = Object.new
@@ -10,6 +18,6 @@ describe "The -Xfinalizers.run_on_exit=true option" do
     })
     p :end
     RUBY
-    ruby_exe(code, options: "-Xfinalizers.run_on_exit=true").should == ":end\nfinalized\n"
+    ruby_exe(code, options: "-Xfinalizers.run_on_exit").should == ":end\nfinalized\n"
   end
 end
