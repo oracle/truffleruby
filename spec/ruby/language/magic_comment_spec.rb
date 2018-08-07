26de8bbee150ea428f56a2340eb7712bd9afbb52
Chris Seaton <chris.seaton@oracle.com>
2018-08-07 23:20:10 +0100

    Improve and generalise magic comment specs
    Leave the eval specs in place, because they work very differently and
    talk about in-process constants and objects.

--- spec/ruby/language/magic_comment_spec.rb
+++ spec/ruby/language/magic_comment_spec.rb
@@ -1,69 +1,74 @@
 require_relative '../spec_helper'
 
 describe "Magic comments" do
-  it "are optional" do
-    eval("__ENCODING__").should be_an_instance_of(Encoding)
-  end
+  
+  [:main, :load, :require, :eval].each do |context|
+    find_file = proc do |file|
+      File.expand_path("fixtures/" + file, File.dirname(__FILE__))
+    end
+    
+    case context
+    when :main
+      description = "in the main file"
+      run = proc do |file, encoding='utf-8'|
+        ruby_exe(File.read(find_file.call(file), encoding: encoding).gsub(/^\$magic_comment_result = /, 'print '))
+      end
+    when :load
+      description = "in a loaded file"
+      run = proc do |file|
+        load find_file.call(file)
+        $magic_comment_result
+      end
+    when :require
+      description = "in a required file"
+      run = proc do |file|
+        require find_file.call(file)
+        $magic_comment_result
+      end
+    when :eval
+      description = "in an eval"
+      run = proc do |file, encoding='utf-8'|
+        eval(File.read(find_file.call(file), encoding: encoding).gsub(/$magic_comment_result = /, ''))
+      end
+    end
+    
+    describe description do
+      it "are optional" do
+        run.call('no_magic_comment.rb').should == Encoding::UTF_8.name
+      end
 
-  it "determine __ENCODING__" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-# encoding: ASCII-8BIT
-__ENCODING__
-EOS
-  end
+      it "are case-insensitive" do
+        run.call('case_magic_comment.rb').should == Encoding::Big5.name
+      end
 
-  it "are case-insensitive" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-# CoDiNg:   aScIi-8bIt
-__ENCODING__
-EOS
-  end
+      it "must be at the first line" do
+        run.call('second_line_magic_comment.rb').should == Encoding::UTF_8.name
+      end
 
-  it "must be at the first line" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::US_ASCII
+      it "must be the first token of the line" do
+        run.call('second_token_magic_comment.rb').should == Encoding::UTF_8.name
+      end
 
-# encoding: ASCII-8BIT
-__ENCODING__
-EOS
-  end
+      it "can be after the shebang" do
+        run.call('shebang_magic_comment.rb').should == Encoding::Big5.name
+      end
 
-  it "must be the first token of the line" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::US_ASCII
-1+1 # encoding: ASCII-8BIT
-__ENCODING__
-EOS
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-  # encoding: ASCII-8BIT
-__ENCODING__
-EOS
-  end
+      it "can take Emacs style" do
+        run.call('emacs_magic_comment.rb').should == Encoding::Big5.name
+      end
 
-  it "can be after the shebang" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-#!/usr/bin/ruby -Ku
-# encoding: ASCII-8BIT
-__ENCODING__
-EOS
-  end
+      it "can take vim style" do
+        run.call('vim_magic_comment.rb').should == Encoding::Big5.name
+      end
 
-  it "can take Emacs style" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-# -*- encoding: ascii-8bit -*-
-__ENCODING__
-EOS
-  end
+      it "determine __ENCODING__" do
+        run.call('magic_comment.rb').should == Encoding::Big5.name
+      end
 
-  it "can take vim style" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-# vim: filetype=ruby, fileencoding=ascii-8bit, tabsize=3, shiftwidth=3
-__ENCODING__
-EOS
+      it "do not cause bytes to be mangled by passing them through the wrong encoding" do
+        run.call('bytes_magic_comment.rb', 'ascii-8bit').should == [167, 65, 166, 110].inspect
+      end
+    end
   end
-
-  it "determine __ENCODING__" do
-    eval(<<EOS.force_encoding("US-ASCII")).should == Encoding::ASCII_8BIT
-# encoding: ASCII-8BIT
-__ENCODING__
-EOS
-  end
+    
 end
