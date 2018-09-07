8eca6ee03125b5b783b65370ccf52fd52dc05a30
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-09-07 17:38:04 +0200

    Do not try to load did_you_mean if rubygems was disabled
    * Similar to MRI's behavior.
    * Move logic after the hooks, so RubyGems is fully initialized.

--- lib/patches/rubygems.rb
+++ lib/patches/rubygems.rb
@@ -1,14 +1,5 @@
 require 'rubygems'
 
-# Because did_you_mean was required directly without RubyGems
-if Truffle::Boot.get_option 'did_you_mean'
-  begin
-    gem 'did_you_mean'
-  rescue LoadError => e
-    Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
-  end
-end
-
 # Add TruffleRuby rubygems hooks to install and uninstall executables from additional
 # GraalVM bin directories (./bin, ./jre/bin)
 unless RbConfig::CONFIG['extra_bindirs'].empty?
@@ -21,3 +12,12 @@ require 'rubygems/gem_dirs_verification'
 Gem::GemDirsVerification.verify(Gem.path)
 Gem::GemDirsVerification.install_hook
 
+# Because did_you_mean was required directly without RubyGems.
+# did_you_mean is only required if --disable-gems was not passed.
+if Truffle::Boot.get_option 'did_you_mean' and Truffle::Boot.get_option 'rubygems'
+  begin
+    gem 'did_you_mean'
+  rescue LoadError => e
+    Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
+  end
+end
