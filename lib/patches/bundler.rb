4d9b080ee92a3c91bbbbd137dd2b1bafc7b3341f
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-09-18 15:47:13 +0200

    Use String#split as it is simpler

--- lib/patches/bundler.rb
+++ lib/patches/bundler.rb
@@ -1,12 +1,6 @@
 require 'bundler'
 
-unless /^(?<major>\d+)\.(?:(?<minor>\d+)\.(?<tiny>\d+))?/ =~ Bundler::VERSION
-  raise "could not parse Bundler::VERSION #{Bundler::VERSION}"
-end
-
-major = major.to_i
-minor = minor.to_i
-tiny = tiny.to_i
+major, minor, tiny = Bundler::VERSION.split('.').map(&:to_i)
 
 unless (major > 1) || (major == 1 && minor > 16) || (major == 1 && minor == 16 && tiny >= 5)
   raise "unsupported bundler version #{Bundler::VERSION}, please use 1.16.5 or more recent"
