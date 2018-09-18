6082f7a508c46ff40ffc152344360299c06a0a36
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-09-18 15:44:30 +0200

    Improve version check for Bundler: require 1.16.5 or more recent

--- lib/patches/bundler.rb
+++ lib/patches/bundler.rb
@@ -1,5 +1,13 @@
 require 'bundler'
 
-unless Bundler::VERSION.start_with?("1.16.") || Bundler::VERSION.start_with?("2.")
-  raise "unsupported bundler version #{Bundler::VERSION}, please use 1.16.x"
+unless /^(?<major>\d+)\.(?:(?<minor>\d+)\.(?<tiny>\d+))?/ =~ Bundler::VERSION
+  raise "could not parse Bundler::VERSION #{Bundler::VERSION}"
+end
+
+major = major.to_i
+minor = minor.to_i
+tiny = tiny.to_i
+
+unless (major > 1) || (major == 1 && minor > 16) || (major == 1 && minor == 16 && tiny >= 5)
+  raise "unsupported bundler version #{Bundler::VERSION}, please use 1.16.5 or more recent"
 end
