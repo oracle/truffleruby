af45a8b18af1aa1c70a875c27946d814bf586122
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-09-19 14:32:50 +0200

    Refactor version check by using Array#<=>

--- lib/patches/bundler.rb
+++ lib/patches/bundler.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 
-major, minor, tiny = Bundler::VERSION.split('.').map(&:to_i)
+version = Bundler::VERSION.split('.').map(&:to_i)
 
-unless (major > 1) || (major == 1 && minor > 16) || (major == 1 && minor == 16 && tiny >= 5)
+unless (version <=> [1, 16, 5]) >= 0
   raise "unsupported bundler version #{Bundler::VERSION}, please use 1.16.5 or more recent"
 end
