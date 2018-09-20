2279162f370416282d233ca0e2adbe75f75a63ab
Duncan MacGregor <duncan.macgregor@oracle.com>
2018-09-20 18:45:45 +0100

    Remove exclusion for working MRI test.

--- test/mri/excludes/TestConditionVariable.rb
+++ test/mri/excludes/TestConditionVariable.rb
@@ -1,4 +1,3 @@
-exclude :test_condvar_wait_deadlock_2, "timeout"
 exclude :test_condvar_wait_deadlock, "spawn"
 exclude :test_dup, "needs investigation"
 exclude :test_dump, "copy of class methods of a class"
