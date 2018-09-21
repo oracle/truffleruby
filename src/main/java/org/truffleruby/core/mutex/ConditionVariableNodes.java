33fc39bb1a1d3b71e4848ed35ce3df3770176716
Duncan MacGregor <duncan.macgregor@oracle.com>
2018-09-21 15:39:38 +0100

    Remove setting the interrupt mode.

--- src/main/java/org/truffleruby/core/mutex/ConditionVariableNodes.java
+++ src/main/java/org/truffleruby/core/mutex/ConditionVariableNodes.java
@@ -19,7 +19,6 @@ import org.truffleruby.builtins.CoreMethod;
 import org.truffleruby.builtins.CoreMethodArrayArgumentsNode;
 import org.truffleruby.builtins.Primitive;
 import org.truffleruby.builtins.PrimitiveArrayArgumentsNode;
-import org.truffleruby.core.InterruptMode;
 import org.truffleruby.core.thread.GetCurrentRubyThreadNode;
 import org.truffleruby.core.thread.ThreadManager.BlockingAction;
 import org.truffleruby.language.NotProvided;
@@ -87,63 +86,57 @@ public abstract class ConditionVariableNodes {
                 endNanoTime = 0;
             }
 
-            InterruptMode interruptMode = Layouts.THREAD.getInterruptMode(thread);
+            getConditionAndReleaseMutex(mutexLock, condLock, thread);
             try {
-                getConditionAndReleaseMutex(mutexLock, condLock, thread);
-                try {
-                    Layouts.CONDITION_VARIABLE.setWaiters(self, Layouts.CONDITION_VARIABLE.getWaiters(self) + 1);
-                    getContext().getThreadManager().runUntilResultWithResumeAction(this, () -> {
-                        /*
-                         * Working with ConditionVariables is tricky because of safepoints. To call
-                         * await or signal on a condition variable we must hold the lock, and that
-                         * lock is released when we start waiting. However if the wait is
-                         * interrupted then the lock will be reacquired before control returns to
-                         * us. If we are interrupted for a safepoint then we must release the lock
-                         * so that all threads can enter the safepoint, and acquire it again before
-                         * resuming waiting.
-                         */
-                        try {
-                            if (signalConsumed(self)) {
-                                return BlockingAction.SUCCESS;
-                            }
-                            if (durationInNanos >= 0) {
-                                final long currentTime = System.nanoTime();
-                                if (currentTime < endNanoTime) {
-                                    condition.await(endNanoTime - currentTime, TimeUnit.NANOSECONDS);
-                                } else {
-                                    Layouts.CONDITION_VARIABLE.setWaiters(self, Layouts.CONDITION_VARIABLE.getWaiters(self) - 1);
-                                    return BlockingAction.SUCCESS;
-                                }
-                            } else {
-                                condition.await();
-                            }
-                            if (signalConsumed(self)) {
-                                return BlockingAction.SUCCESS;
-                            } else {
-                                return null;
-                            }
-                        } finally {
-                            condLock.unlock();
-                        }
-                    }, () -> {
-                        condLock.lock();
-                    });
-                } catch (Error | RuntimeException e) {
-                    // Remove ourselves as a waiter and consume a signal if there is one.
+                Layouts.CONDITION_VARIABLE.setWaiters(self, Layouts.CONDITION_VARIABLE.getWaiters(self) + 1);
+                getContext().getThreadManager().runUntilResultWithResumeAction(this, () -> {
+                    /*
+                     * Working with ConditionVariables is tricky because of safepoints. To call
+                     * await or signal on a condition variable we must hold the lock, and that lock
+                     * is released when we start waiting. However if the wait is interrupted then
+                     * the lock will be reacquired before control returns to us. If we are
+                     * interrupted for a safepoint then we must release the lock so that all threads
+                     * can enter the safepoint, and acquire it again before resuming waiting.
+                     */
                     try {
-                        MutexOperations.internalLockEvenWithException(condLock, this, getContext());
-                    } finally {
-                        if (!signalConsumed(self)) {
-                            Layouts.CONDITION_VARIABLE.setWaiters(self, Layouts.CONDITION_VARIABLE.getWaiters(self) - 1);
+                        if (signalConsumed(self)) {
+                            return BlockingAction.SUCCESS;
                         }
+                        if (durationInNanos >= 0) {
+                            final long currentTime = System.nanoTime();
+                            if (currentTime < endNanoTime) {
+                                condition.await(endNanoTime - currentTime, TimeUnit.NANOSECONDS);
+                            } else {
+                                Layouts.CONDITION_VARIABLE.setWaiters(self, Layouts.CONDITION_VARIABLE.getWaiters(self) - 1);
+                                return BlockingAction.SUCCESS;
+                            }
+                        } else {
+                            condition.await();
+                        }
+                        if (signalConsumed(self)) {
+                            return BlockingAction.SUCCESS;
+                        } else {
+                            return null;
+                        }
+                    } finally {
                         condLock.unlock();
                     }
-                    throw e;
+                }, () -> {
+                    condLock.lock();
+                });
+            } catch (Error | RuntimeException e) {
+                // Remove ourselves as a waiter and consume a signal if there is one.
+                try {
+                    MutexOperations.internalLockEvenWithException(condLock, this, getContext());
                 } finally {
-                    MutexOperations.internalLockEvenWithException(mutexLock, this, getContext());
+                    if (!signalConsumed(self)) {
+                        Layouts.CONDITION_VARIABLE.setWaiters(self, Layouts.CONDITION_VARIABLE.getWaiters(self) - 1);
+                    }
+                    condLock.unlock();
                 }
+                throw e;
             } finally {
-                Layouts.THREAD.setInterruptMode(thread, interruptMode);
+                MutexOperations.internalLockEvenWithException(mutexLock, this, getContext());
             }
         }
 
@@ -170,7 +163,6 @@ public abstract class ConditionVariableNodes {
                 }
             }
 
-            Layouts.THREAD.setInterruptMode(thread, InterruptMode.ON_BLOCKING);
             getContext().getThreadManager().runUntilResult(this, () -> {
                 condLock.lockInterruptibly();
                 return BlockingAction.SUCCESS;
