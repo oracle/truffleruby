76420f44cb7b72325dddbb6b35db30facc43429a
Kevin Menard <kevin.j.menard@oracle.com>
2018-10-22 16:51:33 -0400

    Fix `BigDecimal#to_s` to allow `f` as a format flag to indicate conventional floating point notation.
    Previously only `F` was allowed.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,8 @@ Bug fixes
   format strings, instead of the incorrect `ASCII-8BIT`.
 * Updated `BigDecimal#to_s` to use `e` instead of `E` for exponent
   notation.
+* Fixed `BigDecimal#to_s` to allow `f` as a format flag to indicate
+  conventional floating point notation. Previously only `F` was allowed.
 
 Changes:
 
--- lib/truffle/bigdecimal.rb
+++ lib/truffle/bigdecimal.rb
@@ -205,7 +205,7 @@ class Truffle::BigDecimal < Numeric
 
   def to_s(format = 'E')
     if finite?
-      float_format    = format[-1] == 'F'
+      float_format    = format[-1] == 'F' || format[-1] == 'f'
       space_frequency = format.to_i
       prefix          = if self > 0 && [' ', '+'].include?(format[0])
                           format[0]
