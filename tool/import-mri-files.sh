ed156bf084ff5be21cd95a34f8ce05da77092d5a
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-11-03 08:46:17 +0100

    Organize the import script, group files by where they come from

--- tool/import-mri-files.sh
+++ tool/import-mri-files.sh
@@ -13,29 +13,42 @@ topdir=$(cd ../ruby && pwd -P)
   rm Makefile
 )
 
+# lib/
 rm -r lib/mri
 cp -r ../ruby/lib lib/mri
 rm lib/mri/racc/rdoc/grammar.en.rdoc
 rm lib/mri/securerandom.rb
 rm lib/mri/timeout.rb
 rm lib/mri/weakref.rb
-rm src/main/c/openssl/*.{c,h}
+
+# *.c
 cp ../ruby/st.c src/main/c/cext/st.c
-cp ../ruby/ext/openssl/*.{c,h,rb} src/main/c/openssl
-cp ../ruby/ext/syslog/*.{c,rb} src/main/c/syslog
-cp ../ruby/ext/zlib/*.{c,rb} src/main/c/zlib
+
+# ext/, sorted alphabetically
+cp -r ../ruby/ext/bigdecimal/lib/bigdecimal lib/mri
+
 cp ../ruby/ext/etc/*.{c,rb} src/main/c/etc
+
+rm src/main/c/openssl/*.{c,h}
+cp ../ruby/ext/openssl/*.{c,h,rb} src/main/c/openssl
+cp -r ../ruby/ext/openssl/lib/* lib/mri
+
 cp ../ruby/ext/psych/*.{c,h,rb} src/main/c/psych
 cp ../ruby/ext/psych/yaml/*.{c,h} src/main/c/psych/yaml
 cp ../ruby/ext/psych/yaml/LICENSE src/main/c/psych/yaml
-cp ../ruby/ext/rbconfig/sizeof/*.{c,rb} src/main/c/rbconfig-sizeof
-cp -r ../ruby/ext/openssl/lib/* lib/mri
-cp -r ../ruby/ext/bigdecimal/lib/bigdecimal lib/mri
-cp -r ../ruby/ext/pty/lib/*.rb lib/mri
-cp -r ../ruby/ext/psych/lib/psych lib/mri
 cp -r ../ruby/ext/psych/lib/psych.rb lib/mri
+cp -r ../ruby/ext/psych/lib/psych lib/mri
+
+cp -r ../ruby/ext/pty/lib/*.rb lib/mri
+
+cp ../ruby/ext/rbconfig/sizeof/*.{c,rb} src/main/c/rbconfig-sizeof
+
+cp ../ruby/ext/syslog/*.{c,rb} src/main/c/syslog
 cp -r ../ruby/ext/syslog/lib/syslog lib/mri/syslog
 
+cp ../ruby/ext/zlib/*.{c,rb} src/main/c/zlib
+
+# test/
 rm -rf test/mri/tests
 cp -r ../ruby/test test/mri/tests
 rm -rf test/mri/tests/excludes
@@ -49,9 +62,11 @@ rm -rf test/mri/excludes
 git checkout -- test/mri/excludes
 git checkout -- test/mri/tests/runner.rb
 
+# Licences
 cp ../ruby/BSDL doc/legal/ruby-bsdl.txt
 cp ../ruby/COPYING doc/legal/ruby-licence.txt
 
+# include/
 rm -rf lib/cext/include/ruby lib/cext/include/ccan
 git checkout lib/cext/include/ruby/config.h
 cp -r ../ruby/include/. lib/cext/include
