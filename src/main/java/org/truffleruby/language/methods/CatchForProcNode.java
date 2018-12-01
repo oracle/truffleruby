72e863579d29af6dd0901d4ddd78613f5d2736f0
Dirk Gadsden <dirk@esherido.com>
2018-11-30 17:02:37 -0800

    Raise a `LocalJumpError` if a proc tries to return
    Trying to `return` from within a `proc { ... }` should result in a
    `LocalJumpError` being raised.
    
    Fixes #1488.

--- src/main/java/org/truffleruby/language/methods/CatchForProcNode.java
+++ src/main/java/org/truffleruby/language/methods/CatchForProcNode.java
@@ -16,6 +16,7 @@ import org.truffleruby.language.control.NextException;
 import org.truffleruby.language.control.RaiseException;
 import org.truffleruby.language.control.RedoException;
 import org.truffleruby.language.control.RetryException;
+import org.truffleruby.language.control.ReturnException;
 
 public class CatchForProcNode extends RubyNode {
 
@@ -24,6 +25,7 @@ public class CatchForProcNode extends RubyNode {
     private final BranchProfile redoProfile = BranchProfile.create();
     private final BranchProfile nextProfile = BranchProfile.create();
     private final BranchProfile retryProfile = BranchProfile.create();
+    private final BranchProfile returnProfile = BranchProfile.create();
 
     public CatchForProcNode(RubyNode body) {
         this.body = body;
@@ -44,6 +46,9 @@ public class CatchForProcNode extends RubyNode {
             } catch (RetryException e) {
                 retryProfile.enter();
                 throw new RaiseException(getContext(), coreExceptions().syntaxErrorInvalidRetry(this));
+            } catch (ReturnException e) {
+                returnProfile.enter();
+                throw new RaiseException(getContext(), coreExceptions().unexpectedReturn(this));
             }
         }
     }
