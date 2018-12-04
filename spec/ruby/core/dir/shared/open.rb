24270292d68b55681d15a8ae9c9f9349f238c6b5
Kevin Menard <kevin.j.menard@oracle.com>
2018-12-03 19:37:29 -0500

    Remove an unnecessary guard wrapper.

--- spec/ruby/core/dir/shared/open.rb
+++ spec/ruby/core/dir/shared/open.rb
@@ -61,7 +61,7 @@ describe :dir_open, shared: true do
     dir.should be_kind_of(Dir)
   end
 
-  guard -> { platform_is_not :windows } do
+  platform_is_not :windows do
     it 'sets the close-on-exec flag for the directory file descriptor' do
       Dir.send(@method, DirSpecs.mock_dir) do |dir|
         io = IO.for_fd(dir.fileno)
