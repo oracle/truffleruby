71458b44a588f31ecb23a822aaa17b209fcf6ff4
Duncan MacGregor <duncan.macgregor@oracle.com>
2018-12-07 17:19:05 +0000

    Remove almost all remaining patches.

--- lib/cext/patches/json_patches.rb
+++ lib/cext/patches/json_patches.rb
@@ -13,68 +13,6 @@ class JsonPatches < CommonPatches
     gem: 'json',
     patches: {
       ['parser', 'parser.c'] => [
-        *read_write_field('json','Vsource', false),
-        *read_write_field('json','create_id', false),
-        *read_write_field('json','object_class', false),
-        *read_write_field('json','array_class', false),
-        *read_write_field('json','decimal_class', false),
-        *read_write_field('json','match_string', false),
-        { # cParser_initialize
-          match: /if \(rb_tr_managed_from_handle\(json->Vsource\)\)/,
-          replacement: 'if (json->Vsource)'
-        },
-        { # cParser_parse
-          match: /VALUE result = Qnil;/,
-          replacement: "VALUE result_array_for_address[1];\nresult_array_for_address[0] = Qnil;"
-        },
-        { # cParser_parse
-          match: /char \*np = JSON_parse_value\(json, p, pe, &result, 0\);/,
-          replacement: 'char *np = JSON_parse_value(json, p, pe, result_array_for_address, 0);'
-        },
-        {
-          match: /if \(cs >= JSON_first_final && p == pe\) {\s+return result;/m,
-          replacement: "if (cs >= JSON_first_final && p == pe) {\n return result_array_for_address[0];"
-        },
-        { # JSON_parse_object
-          match: /VALUE last_name = Qnil;/,
-          replacement: "VALUE last_name_array_for_address[1];\nlast_name_array_for_address[0] = Qnil;"
-        },
-        { # JSON_parse_object
-          match: /VALUE v = Qnil;/,
-          replacement: "VALUE v_array_for_address[1];\nv_array_for_address[0] = Qnil;"
-        },
-        { # JSON_parse_object
-          match: /np = JSON_parse_string\(json, p, pe, &last_name\);/,
-          replacement: 'np = JSON_parse_string(json, p, pe, last_name_array_for_address);'
-        },
-        { # JSON_parse_object
-          match: /rb_hash_aset\(\*result, last_name, v\);/,
-          replacement: 'rb_hash_aset(*result, last_name_array_for_address[0], v_array_for_address[0]);'
-        },
-        { # JSON_parse_object
-          match: /rb_funcall\(\*result, i_aset, 2, last_name, v\);/,
-          replacement: 'rb_funcall(*result, i_aset, 2, last_name_array_for_address[0], v_array_for_address[0]);'
-        },
-        { # JSON_parse_object
-          match: /char \*np = JSON_parse_value\(json, p, pe, &v, current_nesting\);/,
-          replacement: 'char *np = JSON_parse_value(json, p, pe, v_array_for_address, current_nesting);'
-        },
-        { # JSON_parse_object
-          match: /rb_ary_push\(\*result, v\);/,
-          replacement: 'rb_ary_push(*result, v_array_for_address[0]);'
-        },
-        { # JSON_parse_object
-          match: /rb_funcall\(\*result, i_leftshift, 1, v\);/,
-          replacement: 'rb_funcall(*result, i_leftshift, 1, v_array_for_address[0]);'
-        },
-        { # JSON_parse_object
-          match: /char \*np = JSON_parse_value\(json, p, pe, &v\);/,
-          replacement: 'char *np = JSON_parse_value(json, p, pe, v_array_for_address);'
-        },
-        { # JSON_parse_string
-          match: /if \(json->create_additions && RTEST\(match_string = rb_tr_managed_from_handle\(json->match_string\)\)\) {/,
-          replacement: 'if (json->create_additions && RTEST(match_string = rb_tr_managed_from_handle_or_null(json->match_string))) {'
-        },
         { # JSON_parse_string
           # Work around a bug in the json extension where it tries to call `rb_str_resize` on non-String objects.
           # We remove it entirely because the string resize is an MRI-optimization to reduce large preallocated strings
@@ -82,36 +20,6 @@ class JsonPatches < CommonPatches
           match: /rb_str_resize\(\*result, RSTRING_LEN\(\*result\)\);/,
           replacement: ''
         },
-        { # cParser_parse_strict
-          match: /np = JSON_parse_array\(json, p, pe, &result\);/,
-          replacement: 'np = JSON_parse_array(json, p, pe, result_array_for_address);'
-        },
-        { # cParser_parse_strict
-          match: /np = JSON_parse_object\(json, p, pe, &result\);/,
-          replacement: 'np = JSON_parse_object(json, p, pe, result_array_for_address);'
-        },
-        { # cParser_parse_quirks_mode
-          match: /if \(cs >= JSON_quirks_mode_first_final && p == pe\) {\s+return result;/m,
-          replacement: "if (cs >= JSON_quirks_mode_first_final && p == pe) {\n return result_array_for_address[0];"
-        },
-        { # cParser_parse_quirks_mode
-          match: /char \*np = JSON_parse_value\(json, p, pe, &result\);/,
-          replacement: 'char *np = JSON_parse_value(json, p, pe, result_array_for_address);'
-        }
-      ],
-      ['generator', 'generator.c'] => [
-        { # generate_json
-          match: /if \(obj == Qnil\)/,
-          replacement: 'if (NIL_P(obj))'
-        },
-        { # generate_json
-          match: /if \(obj == Qfalse\)/,
-          replacement: 'if (rb_tr_obj_equal(Qfalse, obj))'
-        },
-        { # generate_json
-          match: /if \(obj == Qtrue\)/,
-          replacement: 'if (rb_tr_obj_equal(Qtrue, obj))'
-        }
       ]
     }
   }
--- lib/cext/patches/nokogiri_patches.rb
+++ lib/cext/patches/nokogiri_patches.rb
@@ -32,62 +32,6 @@ static int dealloc_node_i(st_data_t a, st_data_t b, st_data_t c, int errorState)
   xmlDocPtr doc = (xmlDocPtr)c;
 EOF
 
-  STRUCT_REF = /#{ID}(->#{ID})*/
-
-  NOKOGIRI_DOC_RUBY_OBJECT_ORIG = /(DOC_RUBY_OBJECT\(#{STRUCT_REF}\))/
-  NOKOGIRI_DOC_RUBY_OBJECT_NEW = '(rb_tr_managed_from_handle_or_null(\1))'
-
-  NOKOGIRI_DOC_NODE_CACHE_ORIG = /(DOC_NODE_CACHE\(#{STRUCT_REF}\))/
-  NOKOGIRI_DOC_NODE_CACHE_NEW = '(rb_tr_managed_from_handle_or_null(\1))'
-
-  def self.tuple_new_patch(ctx, slf)
-    {
-      match: "NOKOGIRI_SAX_TUPLE_NEW(#{ctx}, #{slf})",
-      replacement: "NOKOGIRI_SAX_TUPLE_NEW(#{ctx}, rb_tr_handle_for_managed(#{slf}))" }
-  end
-
-  def self.cast_value_for_native(name, suffix = '', type = :local)
-    # Generate a patch for a simple cast of a VALUE to a (void *) based
-    # on the lifetime of the native use. A type of :local indicates the
-    # native use will only be for the duration of the function call,
-    # :global indicates a reference must be created that will prevent
-    # the VALUE being garbage collected, and :weak indicates a reference
-    # must be created that will _not_ prevent the VALUE being garbage
-    # collected. Suffix specifies a string following the variable which
-    # may be used to limit matches (e.g. if 'err' should be cast, but
-    # not 'error_list'.
-    #
-    # type is not currently used but is there to help preserve the semantic intent.
-    {
-      match: /\(void\ \*\)(#{Regexp.quote(name)})#{Regexp.quote(suffix)}/,
-      replacement: "rb_tr_handle_for_managed(\\1)#{suffix}"
-    }
-  end
-
-  def self.cast_native_for_value(name, suffix = '', type = :local)
-    # Generate a patch to convert a native pointer back into a
-    # VALUE. See cast_native_for_value for the meaning of the type
-    # argument.
-
-    # Type is not currently used but is there to help preserve the
-    # semantic intent.
-    {
-      match: /\(VALUE\)(#{Regexp.quote(name)})#{Regexp.quote(suffix)}/,
-      replacement: "rb_tr_managed_from_handle_or_null(\\1)#{suffix}"
-    }
-  end
-
-  def self.force_cast_native_for_value(name, suffix = '', type = :local)
-    # Like cast_native_for_value but for cases where the original code
-    # did not include and explicit cast to VALUE.
-
-    # type is not currently used but is there to help preserve the semantic intent.
-    {
-      match: /(#{Regexp.quote(name)})#{Regexp.quote(suffix)}/,
-      replacement: "rb_tr_managed_from_handle_or_null(\\1)#{suffix}"
-    }
-  end
-
   PATCHES = {
     gem: 'nokogiri',
     patches: {
@@ -102,100 +46,23 @@ EOF
           match: 'static VALUE to_array(VALUE self, VALUE rb_node)',
           replacement: 'static VALUE to_array(VALUE self)'
         },
-        cast_value_for_native('io')
       ],
       'xml_io.c' => [
-        cast_native_for_value('ctx', ';')
       ],
       'xslt_stylesheet.c' => [
-        {
-          match: 'rb_ary_new()',
-          replacement: 'rb_tr_handle_for_managed(rb_ary_new())'
-        },
-        cast_value_for_native('self'),
-        cast_value_for_native('errstr'),
-        cast_value_for_native('inst'),
-        cast_native_for_value('xsltGetExtData(transform, functionURI)', ';'),
-        force_cast_native_for_value('ctxt->style->_private'),
-        {
-          match: '(wrapper->func_instances',
-          replacement: '(rb_tr_managed_from_handle_or_null(wrapper->func_instances)'
-        },
         { # It is not currently possible to pass var args from native
           # functions to sulong, so we work round the issue here.
           match: 'va_list args;',
-          replacement: 'va_list args; rb_str_cat2(rb_tr_managed_from_handle_or_null(ctx), "Generic error"); return;'
+          replacement: 'va_list args; rb_str_cat2(ctx, "Generic error"); return;'
         }
       ],
-      'html_document.c' => [
-        cast_value_for_native('error_list'),
-        cast_value_for_native('io')
-
-      ],
-      'html_sax_push_parser.c' => [
-        tuple_new_patch('ctx', 'self')
-      ],
-      'xml_sax_push_parser.c' => [
-        tuple_new_patch('ctx', 'self')
-      ],
       'xml_document.c' => [
-        cast_value_for_native('error_list'),
-        cast_value_for_native('io'),
-        cast_native_for_value('ctx'),
-        cast_value_for_native('rb_block_proc()', ';'),
-        write_field('tuple', 'doc', false),
-        write_field('tuple', 'node_cache', false),
         {
           match: NOKOGIRI_DEALLOC_DECL_ORIG,
           replacement: NOKOGIRI_DEALLOC_DECL_NEW
-        },
-        {
-          match: NOKOGIRI_DOC_RUBY_OBJECT_ORIG,
-          replacement: NOKOGIRI_DOC_RUBY_OBJECT_NEW
-        },
-        {
-          match: NOKOGIRI_DOC_NODE_CACHE_ORIG,
-          replacement: NOKOGIRI_DOC_NODE_CACHE_NEW
         }
       ],
-      'xml_dtd.c' => [
-        cast_value_for_native('error_list'),
-        cast_native_for_value('data', ';'),
-        cast_value_for_native('hash')
-      ],
-      'xml_node.c' => [
-        cast_value_for_native('err', ','),
-        cast_value_for_native('error_list'),
-        cast_value_for_native('io'),
-        {
-          match: NOKOGIRI_DOC_RUBY_OBJECT_ORIG,
-          replacement: NOKOGIRI_DOC_RUBY_OBJECT_NEW
-        },
-        {
-          match: NOKOGIRI_DOC_NODE_CACHE_ORIG,
-          replacement: NOKOGIRI_DOC_NODE_CACHE_NEW
-        },
-        cast_native_for_value('node->_private', ';'),
-        cast_value_for_native('rb_node'),
-      ],
-      'xml_namespace.c' => [
-        {
-          match: NOKOGIRI_DOC_RUBY_OBJECT_ORIG,
-          replacement: NOKOGIRI_DOC_RUBY_OBJECT_NEW
-        },
-        cast_value_for_native('ns'),
-        cast_native_for_value('node->_private', ';')
-      ],
-      'xml_reader.c' => [
-        cast_value_for_native('error_list'),
-        cast_value_for_native('io'),
-        cast_value_for_native('rb_io')
-      ],
       'xml_sax_parser.c' => [
-        {
-          match: 'NOKOGIRI_SAX_SELF(ctx)',
-          replacement: 'rb_tr_managed_from_handle_or_null(NOKOGIRI_SAX_SELF(ctx))'
-        },
         { # It is not currently possible to pass var args from native
           # functions to sulong, so we work round the issue here.
           match: /va_list args;[^}]*id_warning, 1, ruby_message\);/,
@@ -207,94 +74,25 @@ EOF
           replacement: 'rb_funcall(doc, id_error, 1, NOKOGIRI_STR_NEW2("Warning."));'
         }
       ],
-      'xml_sax_parser_context.c' => [
-        cast_value_for_native('error_list'),
-        cast_value_for_native('io'),
-        tuple_new_patch('ctxt', 'sax_handler')
-      ],
-      'html_sax_parser_context.c' => [
-        tuple_new_patch('ctxt', 'sax_handler')
-      ],
-      'xml_syntax_error.c' => [
-        cast_native_for_value('ctx'),
-        { # rb_class_new_instance takes a pointer to an array of arguments, and forcing it to inlined simply pushes the issue to
-          match: 'VALUE msg',
-          replacement: 'VALUE msg[1]'
-        },
-        {
-          match: 'msg = ',
-          replacement: 'msg[0] = '
-        },
-        {
-          match: '&msg',
-          replacement: 'msg'
-        },
-      ],
       'xml_xpath_context.c' => [
-        cast_native_for_value('ctx'),
-        {
-          match: NOKOGIRI_DOC_RUBY_OBJECT_ORIG,
-          replacement: NOKOGIRI_DOC_RUBY_OBJECT_NEW
-        },
         { # It is not currently possible to pass var args from native
           # functions to sulong, so we work round the issue here.
           match: 'va_list args;',
           replacement: 'va_list args; rb_raise(rb_eRuntimeError, "%s", "Exception:"); return;'
         },
-        cast_value_for_native('xpath_handler'),
         {
           match: 'VALUE thing = Qnil;',
           replacement: "VALUE thing = Qnil;\n  VALUE errors = rb_ary_new();"
         },
         {
           match: 'xmlSetStructuredErrorFunc(NULL, Nokogiri_error_raise);',
-          replacement: 'xmlSetStructuredErrorFunc(rb_tr_handle_for_managed(errors), Nokogiri_error_array_pusher);'
+          replacement: 'xmlSetStructuredErrorFunc(errors, Nokogiri_error_array_pusher);'
         },
         {
           match: 'if(xpath == NULL)',
           replacement: "if (RARRAY_LEN(errors) > 0) { rb_exc_raise(rb_ary_entry(errors, 0)); }\nif(xpath == NULL)"
         },
-        cast_native_for_value('(ctx->context->userData)', ';'),
-        { # The following patches change
-          # Nokogiri_marshal_xpath_funcall_and_return_values to marshal
-          # arguments into a ruby array and pass call the function using
-          # rb_apply. This is the easiest way to handle passing an
-          # unknown, and potentially large set of arguments to the ruby
-          # function.
-          match: 'VALUE *argv;',
-          replacement: 'VALUE argv;'
-        },
-        {
-          match: /argv =.*$/,
-          replacement: 'argv = rb_ary_new_capa(nargs);'
-        },
-        {
-          match: /rb_gc_register_address.*$/,
-          replacement: ''
-        },
-        {
-          match: /rb_gc_unregister_address.*$/,
-          replacement: ''
-        },
-        {
-          match: /argv\[i\]\ =\ ([^;]+);/,
-          replacement: 'rb_ary_store(argv, i, \1);'
-        },
-        {
-          match: 'rb_funcall2(handler, rb_intern((const char*)function_name), nargs, argv);',
-          replacement: 'rb_apply(handler, rb_intern((const char*)function_name), argv);'
-        },
-        {
-          match: 'free(argv)',
-          replacement: ''
-        },
       ],
-      'xml_schema.c' => [
-        cast_value_for_native('errors')
-      ],
-      'xml_relax_ng.c' => [
-        cast_value_for_native('errors')
-      ]
     }
   }
 end
