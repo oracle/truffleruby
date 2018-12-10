48a65145f9977a6a926f7b05141e0f556646540f
Benoit Daloze <benoit.d.daloze@oracle.com>
2018-12-10 16:11:11 +0100

    Do not define rb_scan_args twice to avoid warnings
    * We have our own version of rb_scan_args.

--- lib/cext/include/ruby/ruby.h
+++ lib/cext/include/ruby/ruby.h
@@ -1884,6 +1884,7 @@ unsigned long ruby_strtoul(const char *str, char **endptr, int base);
 PRINTF_ARGS(int ruby_snprintf(char *str, size_t n, char const *fmt, ...), 3, 4);
 int ruby_vsnprintf(char *str, size_t n, char const *fmt, va_list ap);
 
+#ifndef TRUFFLERUBY
 #if defined(HAVE_BUILTIN___BUILTIN_CHOOSE_EXPR_CONSTANT_P) && defined(__OPTIMIZE__)
 # define rb_scan_args(argc,argvp,fmt,...) \
     __builtin_choose_expr(__builtin_constant_p(fmt), \
@@ -2159,6 +2160,7 @@ rb_scan_args_set(int argc, const VALUE *argv,
     return argc;
 }
 #endif
+#endif // TRUFFLERUBY
 
 #ifndef RUBY_DONT_SUBST
 #include "ruby/subst.h"
