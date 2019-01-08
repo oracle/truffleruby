4c2c56245ccad44fd741734f7a86f2ab8ab4f5ef
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-01-08 18:34:39 +0100

    Ignore if did_you_mean cannot be registered as a gem
    * Fixes issue when running TruffleRuby in TravisCI with the default
      `bundle install --jobs=3 --retry=3 --deployment`.
    
    (cherry picked from commit 5e9ff3f80528b338297933b5b80d23a7f207bd4d)

--- lib/patches/rubygems.rb
+++ lib/patches/rubygems.rb
@@ -17,7 +17,11 @@ Gem::GemDirsVerification.install_hook
 if Truffle::Boot.get_option 'did_you_mean' and Truffle::Boot.get_option 'rubygems'
   begin
     gem 'did_you_mean'
-  rescue LoadError => e
-    Truffle::Debug.log_warning "#{File.basename(__FILE__)}:#{__LINE__} #{e.message}"
+  rescue LoadError
+    # Ignore, this happens when GEM_HOME and GEM_PATH are set and do not include
+    # the default gem home. In such a case, did_you_mean was loaded already, but
+    # it is no longer possible to register the Gem with RubyGems. This happens
+    # for instance with 'bundle exec' after `bundle install --deployment`.
+    nil
   end
 end
