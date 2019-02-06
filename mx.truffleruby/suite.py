e107139744fe5063a0801a3ad0035f9cd60d464e
Benoit Daloze <benoit.daloze@oracle.com>
2019-02-06 14:28:42 +0100

    Update Truffle, Sulong and SubstrateVM

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "526f76f44f59943be852c07e6999d08401b40962",
+                "version": "892e0854c0b1af7e29540eadb862f221d113aaf0",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "526f76f44f59943be852c07e6999d08401b40962",
+                "version": "892e0854c0b1af7e29540eadb862f221d113aaf0",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
