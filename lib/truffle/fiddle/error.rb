0519edea909fee3312c80b5e9fdfcab35d58813d
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-02-06 23:51:50 +0100

    Raise Fiddle::DLError if a library is not found
    * See https://github.com/oracle/truffleruby/issues/1572

--- lib/truffle/fiddle/error.rb
+++ lib/truffle/fiddle/error.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
 #
@@ -6,7 +6,7 @@
 # GNU General Public License version 2, or
 # GNU Lesser General Public License version 2.1.
 
-require_relative 'fiddle/fiddle'
-require_relative 'fiddle/error'
-require_relative 'fiddle/handle'
-require_relative 'fiddle/function'
+module Fiddle
+  class DLError < StandardError
+  end
+end
