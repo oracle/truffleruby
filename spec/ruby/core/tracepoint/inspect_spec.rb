32ea9e4bf4bf8280901ade85f3aa3028f2a4dc65
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-02-13 22:39:30 +0100

    Improve TracePoint#enable and TracePoint#disable specs
    * Use the simpler :line event.

--- spec/ruby/core/tracepoint/inspect_spec.rb
+++ spec/ruby/core/tracepoint/inspect_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../spec_helper'
 
 describe 'TracePoint#inspect' do
   it 'returns a string containing a human-readable TracePoint status' do
-    TracePoint.new(:call) {}.inspect.should ==
+    TracePoint.new(:line) {}.inspect.should ==
       '#<TracePoint:disabled>'
   end
 
