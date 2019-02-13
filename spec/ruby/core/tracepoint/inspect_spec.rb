284db367dd9659eef772ea1a9707ae10694c0cda
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-02-14 00:00:07 +0100

    Add an extra spec for TracePoint#inspect

--- spec/ruby/core/tracepoint/inspect_spec.rb
+++ spec/ruby/core/tracepoint/inspect_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../../spec_helper'
+require_relative 'fixtures/classes'
 
 describe 'TracePoint#inspect' do
   it 'returns a string containing a human-readable TracePoint status' do
@@ -11,6 +12,16 @@ describe 'TracePoint#inspect' do
     line = __LINE__
     TracePoint.new(:line) { |tp| inspect = tp.inspect }.enable do
       inspect.should == "#<TracePoint:line@#{__FILE__}:#{line+2}>"
+    end
+  end
+
+  it 'returns a String showing the event, path and line for a :class event' do
+    inspect = nil
+    line = __LINE__
+    TracePoint.new(:class) { |tp| inspect = tp.inspect }.enable do
+      class TracePointSpec::C
+      end
+      inspect.should == "#<TracePoint:class@#{__FILE__}:#{line+2}>"
     end
   end
 end
