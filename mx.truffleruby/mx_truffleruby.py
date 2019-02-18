d081980e3ffab2fd96ae40590deaee9f8e7e6a61
Benoit Daloze <benoit.daloze@oracle.com>
2019-02-18 11:29:43 +0100

    Call to jt.rb to run specs, instead of hardcoding commands in other places

--- mx.truffleruby/mx_truffleruby.py
+++ mx.truffleruby/mx_truffleruby.py
@@ -248,14 +248,14 @@ def ruby_run_ruby(args):
     jt = join(root, 'tool/jt.rb')
     os.execlp(jt, jt, "ruby", *args)
 
-def ruby_run_specs(launcher, format, args):
+def ruby_run_specs(args):
     with VerboseMx():
-        mx.run([launcher, 'spec/mspec/bin/mspec', 'run', '--config', 'spec/truffle.mspec', '--format', format, '--excl-tag', 'fails'] + args, cwd=root)
+        jt('test', 'specs', *args)
 
 def ruby_testdownstream(args):
     """Run fast specs"""
     build_truffleruby()
-    ruby_run_specs('bin/truffleruby', 'specdoc', ['--excl-tag', 'slow'] + args)
+    ruby_run_specs(['--format', 'specdoc', '--excl-tag', 'slow'] + args)
 
 def ruby_testdownstream_hello(args):
     """Run a minimal Hello World test"""
@@ -273,17 +273,13 @@ def ruby_testdownstream_aot(args):
     debug_build = args[2] == 'debug' if len(args) >= 3 else False
 
     fast = ['--excl-tag', 'slow']
-    mspec_args = [
-        '--excl-tag', 'ci',
-        '--excl-tag', 'graalvm',
-        '--excl-tag', 'aot',
-        '-t', aot_bin
-    ]
+    mspec_args = ['--native', '--format', format, '--excl-tag', 'ci']
 
-    ruby_run_specs(aot_bin, format, mspec_args)
+    os.environ['AOT_BIN'] = aot_bin
+    ruby_run_specs(mspec_args)
 
     # Run "jt test fast --native :truffle" to catch slow specs in Truffle which only apply to native
-    ruby_run_specs(aot_bin, format, fast + mspec_args + [':truffle'])
+    ruby_run_specs(fast + mspec_args + [':truffle'])
 
 def ruby_testdownstream_sulong(args):
     """Run C extension tests"""
