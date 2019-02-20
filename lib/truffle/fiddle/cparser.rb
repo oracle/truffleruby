109c13f0384e1879f6f409ac843b7545f2c22e8b
Chris Seaton <chris.seaton@oracle.com>
2019-02-20 01:32:26 +0000

    Implement just enough of fiddle so that we can run (and fail) the specs

--- lib/truffle/fiddle/cparser.rb
+++ lib/truffle/fiddle/cparser.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved. This
+# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
 #
@@ -6,16 +6,7 @@
 # GNU General Public License version 2, or
 # GNU Lesser General Public License version 2.1.
 
-require 'ffi'
-
 module Fiddle
-
-  TYPE_DOUBLE = 'DOUBLE'
-
-  SIZEOF_DOUBLE = Truffle::FFI.type_size(Truffle::FFI::TYPE_DOUBLE)
-
-  def self.dlopen(name=nil)
-    Handle.new(name)
+  module CParser
   end
-
 end
