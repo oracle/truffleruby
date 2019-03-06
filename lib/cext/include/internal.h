5784b2e3f7bc0867c423246fae1c39708b52c40c
Chris Seaton <chris.seaton@oracle.com>
2019-03-06 21:35:46 +0000

    STR_EMBED_P and STR_SHARED_P

--- lib/cext/include/internal.h
+++ lib/cext/include/internal.h
@@ -7,4 +7,7 @@
 #define RHASH_ST_TABLE(hash)  (hash, false)
 #define RHASH_TBL_RAW(hash)   (hash, false)
 
+#define STR_EMBED_P(str)      (str, false)
+#define STR_SHARED_P(str)     (str, false)
+
 VALUE rb_hash_key_str(VALUE);
