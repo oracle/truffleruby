3d9646c6378a974be3514c4d9e63f08646190e87
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-03-08 13:11:18 +0100

    Pass in the Truffle runtime implementation name
    * Creating a nested Engine is not well supported, and can lead to errors
      if experimental options are set in system properties.

--- src/shared/java/org/truffleruby/shared/TruffleRuby.java
+++ src/shared/java/org/truffleruby/shared/TruffleRuby.java
@@ -27,12 +27,7 @@ public class TruffleRuby {
     public static final String RUBY_COPYRIGHT = "truffleruby - Copyright (c) 2013-2019 Oracle and/or its affiliates";
     public static final boolean PRE_INITIALIZE_CONTEXTS = System.getProperty("polyglot.engine.PreinitializeContexts") != null;
 
-    public static String getVersionString(boolean isAOT) {
-        final String implementationName;
-        try (Engine engine = Engine.create()) {
-            implementationName = engine.getImplementationName();
-        }
-
+    public static String getVersionString(String implementationName, boolean isAOT) {
         final String vm;
         if (isAOT) {
             vm = implementationName + " Native";
