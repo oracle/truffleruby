addeb79fa9ee5b37a024b05786fa51947952d5fd
Chris Seaton <chris.seaton@oracle.com>
2019-03-09 22:01:58 +0000

    Un-exclude RubyGems test

---
+++ test/mri/excludes/TestGemExtRakeBuilder.rb
@@ -0,0 +1,3 @@
+exclude :test_class_build_with_args, "needs investigation"
+exclude :test_class_build, "needs investigation"
+exclude :test_class_build_no_mkrf_passes_args, "needs investigation"
--- test/mri/excludes/TestGemRequestSetGemDependencyAPI.rb
+++ test/mri/excludes/TestGemRequestSetGemDependencyAPI.rb
@@ -1,1 +1,2 @@
 exclude :test_platform_multiple, "needs investigation"
+exclude :test_ruby_engine_mismatch_version, "needs investigation"
