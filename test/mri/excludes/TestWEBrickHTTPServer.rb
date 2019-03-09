2195c8cc6f45f4d606bc3589793520ef21adc9a1
Chris Seaton <chris.seaton@oracle.com>
2019-03-09 17:40:24 +0000

    MRI tests don’t need to be excluded

---
+++ test/mri/excludes/TestWEBrickHTTPServer.rb
@@ -0,0 +1,1 @@
+exclude :test_callbacks, "needs investigation"
---
+++ test/mri/excludes/TestWEBrickSSLServer.rb
@@ -0,0 +1,3 @@
+exclude :test_self_signed_cert_server, "needs investigation"
+exclude :test_self_signed_cert_server_with_string, "needs investigation"
+exclude :test_slow_connect, "needs investigation"
