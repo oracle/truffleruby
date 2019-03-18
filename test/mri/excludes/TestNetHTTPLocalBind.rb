e1d0791568f2af5d6151b03c712232bec3b02c92
Kevin Menard <kevin.j.menard@oracle.com>
2019-03-18 17:28:48 -0400

    Untag passing 'net' MRI tests.

---
+++ test/mri/excludes/TestNetHTTPLocalBind.rb
@@ -0,0 +1,2 @@
+exclude :test_bind_to_local_host, "needs investigation"
+exclude :test_bind_to_local_port, "needs investigation"
---
+++ test/mri/excludes/TestProtocol.rb
@@ -0,0 +1,2 @@
+exclude :test_each_crlf_line, "needs investigation"
+exclude :test_should_properly_dot_stuff_period_with_no_endline, "needs investigation"
