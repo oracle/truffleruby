5f125727b810f6d39b4fda8b8de55adf8b74d8d8
Chris Seaton <chris.seaton@oracle.com>
2019-03-19 01:33:23 +0000

    Import MRI bootstraptest and basictest

--- tool/import-mri-files.sh
+++ tool/import-mri-files.sh
@@ -76,6 +76,12 @@ rm -rf test/mri/excludes
 git checkout -- test/mri/excludes
 git checkout -- test/mri/tests/runner.rb
 
+# basictest/ and bootstraptest/
+rm -rf test/basictest
+cp -r ../ruby/basictest test/basictest
+rm -rf test/bootstraptest
+cp -r ../ruby/bootstraptest test/bootstraptest
+
 # Licences
 cp ../ruby/BSDL doc/legal/ruby-bsdl.txt
 cp ../ruby/COPYING doc/legal/ruby-licence.txt
