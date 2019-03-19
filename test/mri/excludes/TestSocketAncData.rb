4d6d269883c866d14c2c9b2037e1e173e0572c7c
Kevin Menard <kevin.j.menard@oracle.com>
2019-03-19 12:59:32 -0400

    Tag MRI tests that fail on Darwin.

---
+++ test/mri/excludes/TestSocketAncData.rb
@@ -0,0 +1,1 @@
+exclude :test_ipv6_pktinfo, "fails on Darwin in CI"
--- test/mri/excludes/TestWEBrickHTTPProxy.rb
+++ test/mri/excludes/TestWEBrickHTTPProxy.rb
@@ -1,2 +1,3 @@
 exclude :test_upstream_proxy, "needs investigation"
 exclude :test_connect, "needs investigation"
+exclude :test_big_bodies, "fails on Darwin in CI"
