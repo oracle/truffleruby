c43ae3ae49738f07171402c55f6d0a69f75df5e2
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-03-28 23:13:19 +0100

    Change internal macros so they don't warn about unused value

--- lib/cext/include/internal.h
+++ lib/cext/include/internal.h
@@ -4,8 +4,8 @@
 #define LIKELY(x)             RB_LIKELY(x)
 #define UNLIKELY(x)           RB_UNLIKELY(x)
 
-#define RHASH_ST_TABLE(hash)  (hash, false)
-#define RHASH_TBL_RAW(hash)   (hash, false)
+#define RHASH_ST_TABLE(hash)  (hash)
+#define RHASH_TBL_RAW(hash)   (hash)
 
 #define STR_EMBED_P(str)      (str, false)
 #define STR_SHARED_P(str)     (str, false)
