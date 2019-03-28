ea08b7249fb62e584f5ad7e3b65fc8270de4953b
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-03-28 23:17:53 +0100

    Remove macros and functions we do not currently support for st

--- lib/cext/include/internal.h
+++ lib/cext/include/internal.h
@@ -4,9 +4,6 @@
 #define LIKELY(x)             RB_LIKELY(x)
 #define UNLIKELY(x)           RB_UNLIKELY(x)
 
-#define RHASH_ST_TABLE(hash)  (hash)
-#define RHASH_TBL_RAW(hash)   (hash)
-
 #define STR_EMBED_P(str)      (str, false)
 #define STR_SHARED_P(str)     (str, false)
 
