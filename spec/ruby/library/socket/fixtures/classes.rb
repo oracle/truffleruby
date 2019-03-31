3de4d2a25716e65d1eb41ef49fc0eb979bbabbf6
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-03-31 13:48:41 +0200

    Use clock_gettime(CLOCK_MONOTONIC) insted of Time.now

--- spec/ruby/library/socket/fixtures/classes.rb
+++ spec/ruby/library/socket/fixtures/classes.rb
@@ -73,10 +73,10 @@ module SocketSpecs
 
   def self.loop_with_timeout(timeout = 5)
     require 'timeout'
-    time = Time.now
+    time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
     loop do
-      if Time.now - time >= timeout
+      if Process.clock_gettime(Process::CLOCK_MONOTONIC) - time >= timeout
         raise TimeoutError, "Did not succeed within #{timeout} seconds"
       end
 
