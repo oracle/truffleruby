30bb8d5d39c0c9d5a6a8e605939f875401002f99
Duncan MacGregor <duncan.macgregor@oracle.com>
2019-05-13 11:43:17 +0100

    Add some vertical spacing.

--- src/main/java/org/truffleruby/core/string/FrozenStringLiterals.java
+++ src/main/java/org/truffleruby/core/string/FrozenStringLiterals.java
@@ -37,8 +37,10 @@ public class FrozenStringLiterals {
 
     public DynamicObject getFrozenStringLiteral(DynamicObject string) {
         assert Layouts.STRING.getFrozen(string) == true;
+
         final Rope rope = Layouts.STRING.getRope(string);
         final RopeKey key = new RopeKey(rope, context.getHashing(values));
+
         return values.addInCacheIfAbsent(key, string);
     }
 
