32cba83fa794db222cc3ed030b4782ad0114b9b9
Kevin Menard <kevin.j.menard@oracle.com>
2019-06-06 09:37:15 -0400

    Check the set of public instance methods available in `Enumerable::Lazy`.

--- spec/truffle/methods_spec.rb
+++ spec/truffle/methods_spec.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved. This
+# Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
 #
@@ -19,7 +19,7 @@ require_relative '../ruby/spec_helper'
 modules = [
   BasicObject, Kernel, Object,
   Module, Class,
-  Enumerable, Enumerator, Range,
+  Enumerable, Enumerator, Enumerator::Lazy, Range,
   Numeric, Integer, Float,
   Rational, Complex,
   Array, Hash, String,
