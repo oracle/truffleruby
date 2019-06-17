5581539fd3fc4bda115c1b52f327d5a223b3d4c9
Benoit Daloze <benoit.daloze@oracle.com>
2019-06-17 13:12:40 +0200

    Fix xopenssl test for the toolchain
    * Native linkers do not accept "-l ABSOLUTE_PATH".

--- test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
+++ test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
@@ -4,9 +4,9 @@ require 'truffle/openssl-prefix'
 
 if ENV['OPENSSL_PREFIX']
   $CFLAGS += " -I #{ENV['OPENSSL_PREFIX']}/include"
-  $LIBS += " -l #{ENV['OPENSSL_PREFIX']}/lib/libssl.#{RbConfig::CONFIG['SOEXT']}"
+  $LIBS += " #{ENV['OPENSSL_PREFIX']}/lib/libssl.#{RbConfig::CONFIG['SOEXT']}"
 else
-  $LIBS += " -l libssl.#{RbConfig::CONFIG['SOEXT']}"
+  $LIBS += " -lssl"
 end
 
 create_makefile('xopenssl')
