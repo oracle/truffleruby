57692c58627058628afeb5ebeba771471d92e381
Benoit Daloze <benoit.daloze@oracle.com>
2019-06-21 12:26:27 +0200

    MD5() is defined in libcrypto
    * Directly linking to libcrypto is needed for successful linking on macOS.

--- test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
+++ test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
@@ -4,9 +4,9 @@ require 'truffle/openssl-prefix'
 
 if ENV['OPENSSL_PREFIX']
   $CFLAGS += " -I #{ENV['OPENSSL_PREFIX']}/include"
-  $LIBS += " #{ENV['OPENSSL_PREFIX']}/lib/libssl.#{RbConfig::CONFIG['SOEXT']}"
+  $LIBS += " #{ENV['OPENSSL_PREFIX']}/lib/libssl.#{RbConfig::CONFIG['SOEXT']} #{ENV['OPENSSL_PREFIX']}/lib/libcrypto.#{RbConfig::CONFIG['SOEXT']}"
 else
-  $LIBS += " -lssl"
+  $LIBS += " -lssl -lcrypto"
 end
 
 create_makefile('xopenssl')
