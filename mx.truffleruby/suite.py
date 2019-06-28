fab393372b7db75c16b68104c347b5d6d7565797
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-06-28 12:57:52 +0200

    Update Truffle to use the new property for pre-initialization

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "0527e987ef67a1a93d14754becd2b3877d961b35",
+                "version": "ce416312365c2756c02afe034173eee431003175",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "0527e987ef67a1a93d14754becd2b3877d961b35",
+                "version": "ce416312365c2756c02afe034173eee431003175",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
