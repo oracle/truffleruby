d59ba9555a862b70647fc33cdb91bc7e3f54fc9f
Benoit Daloze <benoit.daloze@oracle.com>
2019-07-17 12:28:20 +0200

    [GR-16285] Update to latest GraalVM compiler to fix compilation issue

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "e76bd0761fdb05dc528c7529166c3bedc11572ce",
+                "version": "181709243d88668bd83b590fbeca87eb0777feae",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "e76bd0761fdb05dc528c7529166c3bedc11572ce",
+                "version": "181709243d88668bd83b590fbeca87eb0777feae",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
