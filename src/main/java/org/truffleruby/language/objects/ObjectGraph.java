61fe2d7a14b4ded89d050805e3fe6856ff9c5b81
Benoit Daloze <benoit.daloze@oracle.com>
2019-07-22 13:23:42 +0200

    [GR-17181] Do not visit marked objects in ObjectGraph#getAdjacentObjects
    * At least, they should not be considered when asserting fields are shared,
      because the purpose of storing marked objects there is just to keep them alive.
      The Ruby program should not be able to access those objects via this object,
      so there is no need to share them.

--- src/main/java/org/truffleruby/language/objects/ObjectGraph.java
+++ src/main/java/org/truffleruby/language/objects/ObjectGraph.java
@@ -19,7 +19,6 @@ import com.oracle.truffle.api.object.DynamicObject;
 import com.oracle.truffle.api.object.Property;
 import org.truffleruby.Layouts;
 import org.truffleruby.RubyContext;
-import org.truffleruby.cext.ValueWrapper;
 import org.truffleruby.core.hash.Entry;
 import org.truffleruby.core.queue.SizedQueue;
 import org.truffleruby.core.queue.UnsizedQueue;
@@ -131,10 +130,6 @@ public abstract class ObjectGraph {
                 }
             } else if (value instanceof Object[]) {
                 for (Object element : (Object[]) value) {
-                    // Needed to get wrappers set by Truffle::CExt.set_mark_list_on_object.
-                    if (element instanceof ValueWrapper) {
-                        element = ((ValueWrapper) element).getObject();
-                    }
                     if (element instanceof DynamicObject) {
                         reachable.add((DynamicObject) element);
                     }
