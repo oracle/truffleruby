9198c98240c91bb8665011c39aab0ab353e9a1b3
Chris Jester-Young <cky@cky.nz>
2019-08-01 21:57:26 -0400

    Make rb_undef_method work for private methods.

--- lib/truffle/truffle/cext.rb
+++ lib/truffle/truffle/cext.rb
@@ -1298,7 +1298,7 @@ module Truffle::CExt
   end
 
   def rb_undef(mod, name)
-    if mod.frozen? or mod.method_defined?(name)
+    if mod.frozen? or mod.method_defined?(name) or mod.private_method_defined?(name)
       mod.send(:undef_method, name)
     end
   end
