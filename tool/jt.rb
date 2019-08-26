1e93d7f16d9aab0d00a7a310c09380e5e98d2764
Petr Chalupa <petr.chalupa@oracle.com>
2019-08-26 16:19:18 +0200

    Apply otherwise ignored --jvm and --vm options coming from TRUFFLERUBYOPT and RUBYOPT

--- tool/jt.rb
+++ tool/jt.rb
@@ -802,6 +802,17 @@ module Commands
 
     options[:no_print_cmd] = true if @silent
 
+    if truffleruby? && !truffleruby_native?
+      %w[TRUFFLERUBYOPT RUBYOPT].each do |env_name|
+        if ENV[env_name] && !ENV[env_name].empty?
+          vm_options = ENV[env_name].split(" ").select { |v| v =~ /^--(jvm|vm)/ }
+          $stderr.print "Not a native build, applying --jvm and --vm options #{vm_options.inspect} from #{env_name} \"#{ENV[env_name]}\" directly as options. "
+          $stderr.puts "Otherwise they would be ignored since jvm process cannot apply them to itself."
+          vm_args += vm_options
+        end
+      end
+    end
+
     raw_sh env_vars, ruby_launcher, *(vm_args if truffleruby?), *ruby_args, options
   end
 
