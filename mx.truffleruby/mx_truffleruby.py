e295da37815dd8e2c4cfcdb350d25943cf7df6c8
Benoit Daloze <benoit.daloze@oracle.com>
2019-08-26 18:53:51 +0200

    [GR-15451] Support standalone dependencies

--- mx.truffleruby/mx_truffleruby.py
+++ mx.truffleruby/mx_truffleruby.py
@@ -119,6 +119,10 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
     license_files=[],
     third_party_license_files=[],
     dependencies=['rbyl', 'Truffle', 'Truffle NFI', 'Sulong', 'GraalVM Chrome Inspector', 'GraalVM Profiler', 'VisualVM'],
+    standalone_dependencies={
+        'Sulong': ('lib/sulong', ['bin/<exe:lli>']),
+        'LLVM.org toolchain': ('lib/llvm-toolchain', []),
+    },
     truffle_jars=[
         'truffleruby:TRUFFLERUBY',
         'truffleruby:TRUFFLERUBY-SHARED',
