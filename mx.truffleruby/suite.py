0abff3c3d84d3ec7c6db0245449446b4f94e0ea7
Benoit Daloze <benoit.daloze@oracle.com>
2019-09-10 19:40:50 +0200

    Update Truffle to get JDK 11 fixes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "432df38e6864205e10e52aa4058ce868aedab929",
+                "version": "7980245f92d00f4b6c38688d46f0a586ec397fde",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "432df38e6864205e10e52aa4058ce868aedab929",
+                "version": "7980245f92d00f4b6c38688d46f0a586ec397fde",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
