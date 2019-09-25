01f4886f39ab9efa538e700f05dd40f3d7524cf6
Chris Seaton <chris.seaton@shopify.com>
2019-09-25 23:22:08 +0100

    New spec for Struct#dup

--- spec/ruby/core/struct/dup_spec.rb
+++ spec/ruby/core/struct/dup_spec.rb
@@ -3,6 +3,14 @@ require_relative 'fixtures/classes'
 
 describe "Struct-based class#dup" do
 
+  it "duplicates members" do
+    klass = Struct.new(:foo, :bar)
+    instance = klass.new(14, 2)
+    duped = instance.dup
+    duped.foo.should == 14
+    duped.bar.should == 2
+  end
+
   # From https://github.com/jruby/jruby/issues/3686
   it "retains an included module in the ancestor chain for the struct's singleton class" do
     klass = Struct.new(:foo)
