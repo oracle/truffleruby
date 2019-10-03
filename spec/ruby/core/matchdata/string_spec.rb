3f1f0387193192ce1035c3db293608ab18d2ae87
Benoit Daloze <benoit.daloze@oracle.com>
2019-10-03 17:51:30 +0200

    Add a spec that MatchData#string always returns the same String

--- spec/ruby/core/matchdata/string_spec.rb
+++ spec/ruby/core/matchdata/string_spec.rb
@@ -12,6 +12,11 @@ describe "MatchData#string" do
     str.frozen?.should == true
   end
 
+  it "returns the same frozen string for every call" do
+    md = /(.)(.)(\d+)(\d)/.match("THX1138.")
+    md.string.should equal(md.string)
+  end
+
   it "returns a frozen copy of the matched string for gsub(String)" do
     'he[[o'.gsub!('[', ']')
     $~.string.should == 'he[[o'
