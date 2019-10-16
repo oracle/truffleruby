62af3cfd83d3f49a9c4a7bb2c9ee8ce5dab49920
Benoit Daloze <benoit.daloze@oracle.com>
2019-10-16 17:00:30 +0200

    Improve warning message for gem update --system

--- lib/patches/rubygems/commands/update_command.rb
+++ lib/patches/rubygems/commands/update_command.rb
@@ -23,8 +23,11 @@ class Gem::Commands::UpdateCommand
       original_update_rubygems
     else
       # gem update --system is skipped on TruffleRuby as it might break RubyGems
-      ui.alert_warning 'This command does nothing on TruffleRuby, since it is not fully supported yet. ' +
-                           '"gem update --system --force-update" can by tried. However, it might break RubyGems.'
+      ui.alert_warning <<~EOS
+        "gem update --system" does nothing on TruffleRuby, since it is not fully supported yet.
+        Usually it is not needed to update RubyGems (e.g., for Bundler), since TruffleRuby already ships with a recent RubyGems.
+        "gem update --system --force-update" can be used to force the update. However, it might break RubyGems.
+      EOS
     end
   end
 
