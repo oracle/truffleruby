de6e2f71f6f181940c22dc8ff4798a4cd051fe2b
Benoit Daloze <benoit.daloze@oracle.com>
2019-11-05 11:12:08 +0100

    [GR-18925] Update Sulong to fix a race condition while defining functions.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "a83f3ffd0d919dcb7140112f87e3c29ee7218661",
+                "version": "aee967f1d90e1f032b266c62d10fc8c32a805fb8",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "a83f3ffd0d919dcb7140112f87e3c29ee7218661",
+                "version": "aee967f1d90e1f032b266c62d10fc8c32a805fb8",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
