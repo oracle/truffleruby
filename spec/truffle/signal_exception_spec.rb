84bb2e1234965606834c74358c0ca2c73c94d276
Benoit Daloze <benoit.daloze@oracle.com>
2019-11-07 15:54:27 +0100

    Move signals specs that work on MRI to ruby/spec

--- spec/truffle/signal_exception_spec.rb
+++ spec/truffle/signal_exception_spec.rb
@@ -9,42 +9,6 @@
 require_relative '../ruby/spec_helper'
 
 describe "SignalException" do
-  it "can be rescued" do
-    ruby_exe(<<-RUBY)
-      begin
-        raise SignalException, 'SIGKILL'
-      rescue SignalException
-        exit(0)
-      end
-      exit(1)
-    RUBY
-
-    $?.exitstatus.should == 0
-  end
-
-  it "runs after at_exit" do
-    output = ruby_exe(<<-RUBY)
-      at_exit do
-        puts "hello"
-        $stdout.flush
-      end
-
-      raise SignalException, 'SIGKILL'
-    RUBY
-
-    $?.termsig.should == Signal.list.fetch("KILL")
-    output.should == "hello\n"
-  end
-
-  it "cannot be trapped with Signal.trap" do
-    ruby_exe(<<-RUBY)
-      Signal.trap("PROF") {}
-      raise(SignalException, "PROF")
-    RUBY
-
-    $?.termsig.should == Signal.list.fetch("PROF")
-  end
-
   it "does not self-signal for VTALRM" do
     IO.popen(ruby_cmd("raise(SignalException, 'VTALRM')"), err: [:child, :out]) do |out|
       out.read.should include("for SIGVTALRM as it is VM reserved")
