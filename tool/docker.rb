b85ad3a0e84e0e43f9ccb3c48378e8d7b96298ac
Benoit Daloze <benoit.d.daloze@oracle.com>
2019-11-08 21:23:42 +0100

    jt docker: use << instead of push

--- tool/docker.rb
+++ tool/docker.rb
@@ -103,7 +103,7 @@ class JT
       lines = []
       packages = []
 
-      lines.push "FROM #{distro.fetch('base')}"
+      lines << "FROM #{distro.fetch('base')}"
       lines.push(*distro.fetch('setup'))
       lines.push(*distro.fetch('locale'))
 
@@ -120,12 +120,12 @@ class JT
 
       lines << [distro.fetch('install'), *packages.compact].join(' ')
 
-      lines.push 'WORKDIR /test'
+      lines << 'WORKDIR /test'
 
       unless root
-        lines.push 'RUN useradd -ms /bin/bash test'
-        lines.push 'RUN chown test /test'
-        lines.push 'USER test'
+        lines << 'RUN useradd -ms /bin/bash test'
+        lines << 'RUN chown test /test'
+        lines << 'USER test'
       end
 
       docker_dir = File.join(TRUFFLERUBY_DIR, 'tool', 'docker')
@@ -138,59 +138,59 @@ class JT
       case install_method
       when :public
         graalvm_tarball = "graalvm-ce-#{public_version}-linux-amd64.tar.gz"
-        lines.push "RUN curl -OL https://github.com/oracle/graal/releases/download/vm-#{public_version}/#{graalvm_tarball}"
+        lines << "RUN curl -OL https://github.com/oracle/graal/releases/download/vm-#{public_version}/#{graalvm_tarball}"
         graalvm_base = '/test/graalvm'
-        lines.push "RUN mkdir #{graalvm_base}"
-        lines.push "RUN tar -zxf #{graalvm_tarball} -C #{graalvm_base} --strip-components=1"
-        lines.push "RUN #{graalvm_base}/bin/gu install org.graalvm.ruby | tee install.log"
+        lines << "RUN mkdir #{graalvm_base}"
+        lines << "RUN tar -zxf #{graalvm_tarball} -C #{graalvm_base} --strip-components=1"
+        lines << "RUN #{graalvm_base}/bin/gu install org.graalvm.ruby | tee install.log"
         lines.push(*check_post_install_message)
         ruby_base = "#{graalvm_base}/#{language_dir}/ruby"
         graalvm_bin = "#{graalvm_base}/bin"
         ruby_bin = graalvm_bin
-        lines.push "RUN #{ruby_base}/lib/truffle/post_install_hook.sh" if run_post_install_hook
+        lines << "RUN #{ruby_base}/lib/truffle/post_install_hook.sh" if run_post_install_hook
       when :graalvm
         FileUtils.copy graalvm_tarball, docker_dir
         FileUtils.copy graalvm_component, docker_dir
         graalvm_tarball = File.basename(graalvm_tarball)
         graalvm_component = File.basename(graalvm_component)
-        lines.push "COPY #{graalvm_tarball} /test/"
-        lines.push "COPY #{graalvm_component} /test/"
+        lines << "COPY #{graalvm_tarball} /test/"
+        lines << "COPY #{graalvm_component} /test/"
         graalvm_base = '/test/graalvm'
-        lines.push "RUN mkdir #{graalvm_base}"
-        lines.push "RUN tar -zxf #{graalvm_tarball} -C #{graalvm_base} --strip-components=1"
+        lines << "RUN mkdir #{graalvm_base}"
+        lines << "RUN tar -zxf #{graalvm_tarball} -C #{graalvm_base} --strip-components=1"
         ruby_base = "#{graalvm_base}/#{language_dir}/ruby"
         graalvm_bin = "#{graalvm_base}/bin"
         ruby_bin = graalvm_bin
-        lines.push "RUN #{graalvm_bin}/gu install --file /test/#{graalvm_component} | tee install.log"
+        lines << "RUN #{graalvm_bin}/gu install --file /test/#{graalvm_component} | tee install.log"
         lines.push(*check_post_install_message)
-        lines.push "RUN #{ruby_base}/lib/truffle/post_install_hook.sh" if run_post_install_hook
+        lines << "RUN #{ruby_base}/lib/truffle/post_install_hook.sh" if run_post_install_hook
       when :standalone
         FileUtils.copy standalone_tarball, docker_dir
         standalone_tarball = File.basename(standalone_tarball)
-        lines.push "COPY #{standalone_tarball} /test/"
+        lines << "COPY #{standalone_tarball} /test/"
         ruby_base = '/test/truffleruby-standalone'
-        lines.push "RUN mkdir #{ruby_base}"
-        lines.push "RUN tar -zxf #{standalone_tarball} -C #{ruby_base} --strip-components=1"
+        lines << "RUN mkdir #{ruby_base}"
+        lines << "RUN tar -zxf #{standalone_tarball} -C #{ruby_base} --strip-components=1"
         ruby_bin = "#{ruby_base}/bin"
-        lines.push "RUN #{ruby_base}/lib/truffle/post_install_hook.sh" if run_post_install_hook
+        lines << "RUN #{ruby_base}/lib/truffle/post_install_hook.sh" if run_post_install_hook
       when :source
-        lines.push 'RUN git clone --depth 1 https://github.com/graalvm/mx.git'
-        lines.push 'ENV PATH=$PATH:/test/mx'
-        lines.push 'RUN git clone --depth 1 https://github.com/graalvm/graal-jvmci-8.git'
+        lines << 'RUN git clone --depth 1 https://github.com/graalvm/mx.git'
+        lines << 'ENV PATH=$PATH:/test/mx'
+        lines << 'RUN git clone --depth 1 https://github.com/graalvm/graal-jvmci-8.git'
 
         # Disable compiler warnings as errors, as we may be using a more recent compiler
-        lines.push "RUN sed -i 's/WARNINGS_ARE_ERRORS = -Werror/WARNINGS_ARE_ERRORS = /g' graal-jvmci-8/make/linux/makefiles/gcc.make"
+        lines << "RUN sed -i 's/WARNINGS_ARE_ERRORS = -Werror/WARNINGS_ARE_ERRORS = /g' graal-jvmci-8/make/linux/makefiles/gcc.make"
 
-        lines.push 'RUN cd graal-jvmci-8 && JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac)))) mx build'
-        lines.push "ENV JAVA_HOME=/test/graal-jvmci-8/#{distro.fetch('jdk')}/linux-amd64/product"
-        lines.push 'ENV PATH=$JAVA_HOME/bin:$PATH'
-        lines.push 'ENV JVMCI_VERSION_CHECK=ignore'
-        lines.push 'RUN java -version'
-        lines.push 'RUN git clone https://github.com/oracle/graal.git'
-        lines.push "RUN git clone --depth 1 --branch #{source_branch} #{truffleruby_repo}"
-        lines.push 'RUN cd truffleruby && mx build'
-        lines.push 'RUN cd graal/compiler && mx build'
-        lines.push "ENV JAVA_OPTS='-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -Djvmci.class.path.append=/test/graal/compiler/mxbuild/dists/jdk1.8/graal.jar'"
+        lines << 'RUN cd graal-jvmci-8 && JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac)))) mx build'
+        lines << "ENV JAVA_HOME=/test/graal-jvmci-8/#{distro.fetch('jdk')}/linux-amd64/product"
+        lines << 'ENV PATH=$JAVA_HOME/bin:$PATH'
+        lines << 'ENV JVMCI_VERSION_CHECK=ignore'
+        lines << 'RUN java -version'
+        lines << 'RUN git clone https://github.com/oracle/graal.git'
+        lines << "RUN git clone --depth 1 --branch #{source_branch} #{truffleruby_repo}"
+        lines << 'RUN cd truffleruby && mx build'
+        lines << 'RUN cd graal/compiler && mx build'
+        lines << "ENV JAVA_OPTS='-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -Djvmci.class.path.append=/test/graal/compiler/mxbuild/dists/jdk1.8/graal.jar'"
         ruby_base = '/test/truffleruby'
         ruby_bin = "#{ruby_base}/bin"
       end
@@ -199,47 +199,47 @@ class JT
         if [:public, :graalvm].include?(install_method)
           FileUtils.copy native_component, docker_dir
           native_component = File.basename(native_component)
-          lines.push "COPY #{native_component} /test/"
-          lines.push "RUN #{graalvm_bin}/gu install --file /test/#{native_component} | tee install.log"
-          lines.push "RUN #{graalvm_base}/bin/gu rebuild-images polyglot libpolyglot"
+          lines << "COPY #{native_component} /test/"
+          lines << "RUN #{graalvm_bin}/gu install --file /test/#{native_component} | tee install.log"
+          lines << "RUN #{graalvm_base}/bin/gu rebuild-images polyglot libpolyglot"
         else
           abort "can't rebuild images for a build not from public or from local GraalVM components"
         end
       end
 
-      lines.push "ENV PATH=#{ruby_bin}:$PATH"
+      lines << "ENV PATH=#{ruby_bin}:$PATH"
 
       configs = [:public, :graalvm].include?(install_method) ? %w[--native --jvm] : ['']
 
       configs.each do |c|
-        lines.push "RUN ruby #{c} --version"
+        lines << "RUN ruby #{c} --version"
       end
 
       if basic_test || full_test
         configs.each do |c|
-          lines.push "RUN cp -r #{ruby_base}/lib/gems /test/clean-gems"
+          lines << "RUN cp -r #{ruby_base}/lib/gems /test/clean-gems"
 
           gem_install = "ruby #{c} -S gem install --no-document"
-          lines.push "RUN #{gem_install} color"
-          lines.push "RUN ruby #{c} -rcolor -e 'raise unless defined?(Color)'"
+          lines << "RUN #{gem_install} color"
+          lines << "RUN ruby #{c} -rcolor -e 'raise unless defined?(Color)'"
 
-          lines.push "RUN #{gem_install} oily_png"
-          lines.push "RUN ruby #{c} -roily_png -e 'raise unless defined?(OilyPNG::Color)'"
+          lines << "RUN #{gem_install} oily_png"
+          lines << "RUN ruby #{c} -roily_png -e 'raise unless defined?(OilyPNG::Color)'"
 
-          lines.push "RUN #{gem_install} unf"
-          lines.push "RUN ruby #{c} -runf -e 'raise unless defined?(UNF)'"
+          lines << "RUN #{gem_install} unf"
+          lines << "RUN ruby #{c} -runf -e 'raise unless defined?(UNF)'"
 
-          lines.push "RUN rm -rf #{ruby_base}/lib/gems"
-          lines.push "RUN mv /test/clean-gems #{ruby_base}/lib/gems"
+          lines << "RUN rm -rf #{ruby_base}/lib/gems"
+          lines << "RUN mv /test/clean-gems #{ruby_base}/lib/gems"
         end
       end
 
       if full_test
-        lines.push "RUN git clone #{truffleruby_repo} truffleruby-tests"
-        lines.push "RUN cd truffleruby-tests && git checkout #{test_branch}"
-        lines.push 'RUN cp -r truffleruby-tests/spec .'
-        lines.push 'RUN cp -r truffleruby-tests/test/truffle/compiler/pe .'
-        lines.push 'RUN rm -rf truffleruby-tests'
+        lines << "RUN git clone #{truffleruby_repo} truffleruby-tests"
+        lines << "RUN cd truffleruby-tests && git checkout #{test_branch}"
+        lines << 'RUN cp -r truffleruby-tests/spec .'
+        lines << 'RUN cp -r truffleruby-tests/test/truffle/compiler/pe .'
+        lines << 'RUN rm -rf truffleruby-tests'
 
         configs.each do |c|
           excludes = ['fails', 'slow']
@@ -247,16 +247,16 @@ class JT
           [':command_line', ':security', ':language', ':core', ':library', ':capi', ':library_cext', ':truffle', ':truffle_capi'].each do |set|
             t_config = c.empty? ? '' : '-T' + c
             t_excludes = excludes.map { |e| '--excl-tag ' + e }.join(' ')
-            lines.push "RUN ruby spec/mspec/bin/mspec --config spec/truffle.mspec -t #{ruby_bin}/ruby #{t_config} #{t_excludes} #{set}"
+            lines << "RUN ruby spec/mspec/bin/mspec --config spec/truffle.mspec -t #{ruby_bin}/ruby #{t_config} #{t_excludes} #{set}"
           end
         end
 
         configs.each do |c|
-          lines.push "RUN ruby #{c} --vm.Dgraal.TruffleCompilationExceptionsAreThrown=true --vm.Dgraal.TruffleIterativePartialEscape=true pe/pe.rb"
+          lines << "RUN ruby #{c} --vm.Dgraal.TruffleCompilationExceptionsAreThrown=true --vm.Dgraal.TruffleIterativePartialEscape=true pe/pe.rb"
         end
       end
 
-      lines.push 'CMD bash'
+      lines << 'CMD bash'
 
       lines.join("\n") + "\n"
     end
