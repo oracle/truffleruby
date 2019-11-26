896db5a205eaee81d3a80429a0705f301b0d132c
Chris Seaton <chris.seaton@shopify.com>
2019-11-26 05:58:03 +0100

    Fix undef_method error message

--- src/main/java/org/truffleruby/core/exception/CoreExceptions.java
+++ src/main/java/org/truffleruby/core/exception/CoreExceptions.java
@@ -642,7 +642,10 @@ public class CoreExceptions {
     public DynamicObject nameErrorUndefinedMethod(String name, DynamicObject module, Node currentNode) {
         assert RubyGuards.isRubyModule(module);
         return nameError(
-                StringUtils.format("undefined method `%s' for class `%s'", name, Layouts.MODULE.getFields(module).getName()),
+                StringUtils.format("undefined method `%s' for %s `%s'",
+                        name,
+                        Layouts.CLASS.isClass(module) ? "class" : "module",
+                        Layouts.MODULE.getFields(module).getName()),
                 module,
                 name,
                 currentNode);
