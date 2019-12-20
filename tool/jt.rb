e623d8da0869431773ba3435a70d39c1cd3d1b6c
Brandon Fish <brandon.j.fish@oracle.com>
2019-12-20 13:48:19 -0600

    Update ecosystem test rack dependency version and gem test pack version

--- tool/jt.rb
+++ tool/jt.rb
@@ -30,7 +30,7 @@ TRUFFLERUBY_DIR = File.expand_path('../..', File.realpath(__FILE__))
 GRAAL_DIR = File.expand_path('../graal', TRUFFLERUBY_DIR)
 PROFILES_DIR = "#{TRUFFLERUBY_DIR}/profiles"
 
-TRUFFLERUBY_GEM_TEST_PACK_VERSION = 'fa8e5817c1496e136caa1f994864712b2faf6bc7'
+TRUFFLERUBY_GEM_TEST_PACK_VERSION = '74d5e0101bb143a4f9a2a60664328ad936b05594'
 
 JDEBUG = '--vm.agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y'
 METRICS_REPS = Integer(ENV['TRUFFLERUBY_METRICS_REPS'] || 10)
