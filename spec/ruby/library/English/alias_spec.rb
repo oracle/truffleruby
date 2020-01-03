c700bcb4e8a6173566de0fd941c775ae97a5f0e8
Benoit Daloze <benoit.daloze@oracle.com>
2020-01-03 16:23:53 +0100

    Require 'English' instead of manually aliasing

--- spec/ruby/library/English/alias_spec.rb
+++ spec/ruby/library/English/alias_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../../spec_helper'
+require 'English'
 
 describe "alias keyword" do
   it "aliases $ERROR_INFO to $! in English and $ERROR_INFO still returns a backtrace" do
-    alias $ERROR_INFO $!
     (1 / 0 rescue $ERROR_INFO).should_not == nil
   end
 end
