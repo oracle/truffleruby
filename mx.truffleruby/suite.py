8fbc25dd3bb53e074a1927daf66ffd0213f13a40
Benoit Daloze <benoit.daloze@oracle.com>
2020-01-15 14:59:09 +0100

    Update Truffle

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "70c5f95afa7f3e5f66952310bcfe8bea50b78eb9",
+                "version": "e5cf1f7daf6381e3ea516c55962ec2f1c0dd430e",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "70c5f95afa7f3e5f66952310bcfe8bea50b78eb9",
+                "version": "e5cf1f7daf6381e3ea516c55962ec2f1c0dd430e",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
