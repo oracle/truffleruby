3cf1ba59aa2b8e2ccb2e01fbdca4caebc509b2c3
Benoit Daloze <benoit.d.daloze@oracle.com>
2020-01-25 20:37:01 +0100

    Show the total time with `jt profile`

--- tool/jt.rb
+++ tool/jt.rb
@@ -1783,9 +1783,12 @@ EOS
     if stdin
       File.write(profile_data_file, STDIN.read)
     else
+      require 'benchmark'
       FileUtils.rm_f(profile_data_file)
       run_args = DEFAULT_PROFILE_OPTIONS + ["--cpusampler.OutputFile=#{profile_data_file}"] + args
-      run_ruby(env, *run_args)
+      puts Benchmark.measure {
+        run_ruby(env, *run_args)
+      }
     end
     raw_sh "#{repo}/stackcollapse-graalvm.rb", profile_data_file, out: flamegraph_data_file
     raw_sh "#{repo}/flamegraph.pl", flamegraph_data_file, out: svg_filename
