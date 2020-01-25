26cc873bf941b168a5d136e670d5e1e05a500f7d
Benoit Daloze <benoit.d.daloze@oracle.com>
2020-01-25 20:40:37 +0100

    Directly return a native pointer for RSTRING_PTR() if the String is already native
    * 110s => 64s for a small `bundle install`.
    
    (cherry picked from commit 190cf34fa58e18996473b9e725b694eaf73965ea)

--- src/main/c/cext/ruby.c
+++ src/main/c/cext/ruby.c
@@ -812,7 +812,11 @@ char *rb_string_value_cstr(VALUE *value_pointer) {
 }
 
 char *RSTRING_PTR_IMPL(VALUE string) {
-  return (char *)polyglot_as_i8_array(RUBY_CEXT_INVOKE_NO_WRAP("RSTRING_PTR", string));
+  if (polyglot_as_boolean(RUBY_CEXT_INVOKE_NO_WRAP("native_string?", string))) {
+    return NATIVE_RSTRING_PTR(string);
+  } else {
+    return (char *)polyglot_as_i8_array(RUBY_CEXT_INVOKE_NO_WRAP("RSTRING_PTR", string));
+  }
 }
 
 char *RSTRING_END(VALUE string) {
