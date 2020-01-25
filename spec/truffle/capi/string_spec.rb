329cbd6a88e51ba8f630c4671d6a3acce919ab73
Benoit Daloze <benoit.d.daloze@oracle.com>
2020-01-25 19:49:23 +0100

    Use primitives for string_pointer* calls in cext.rb
    * Removes the overhead of an extra call.
    
    (cherry picked from commit 82178d9f44e2e1e13c085deb7c8c8c4e28538911)

--- spec/truffle/capi/string_spec.rb
+++ spec/truffle/capi/string_spec.rb
@@ -18,13 +18,13 @@ describe "TruffleRuby RSTRING_PTR" do
   it "does not store the String to native memory if not needed" do
     str = "foobar"
     @s.string_ptr(str).should == "f"
-    Truffle::CExt.string_pointer_is_native?(str).should == false
+    Truffle::CExt.native_string?(str).should == false
   end
 
   it "stores the String to native memory if the address is returned" do
     str = "foobar"
     @s.string_ptr_return_address(str).should be_kind_of(Integer)
-    Truffle::CExt.string_pointer_is_native?(str).should == true
+    Truffle::CExt.native_string?(str).should == true
   end
 end
 
@@ -35,8 +35,8 @@ describe "TruffleRuby NATIVE_RSTRING_PTR" do
 
   it "ensures the String is stored in native memory" do
     str = "foobar"
-    Truffle::CExt.string_pointer_is_native?(str).should == false
+    Truffle::CExt.native_string?(str).should == false
     @s.NATIVE_RSTRING_PTR(str)
-    Truffle::CExt.string_pointer_is_native?(str).should == true
+    Truffle::CExt.native_string?(str).should == true
   end
 end
