c73bc059920b424f43f6379eea513fa1cc4e42c7
Petr Chalupa <petr.chalupa@oracle.com>
2020-01-29 21:51:18 +0100

    Fix formatting after a closing tag

--- src/main/java/org/truffleruby/core/array/ArrayBuilderNode.java
+++ src/main/java/org/truffleruby/core/array/ArrayBuilderNode.java
@@ -38,11 +38,10 @@ import com.oracle.truffle.api.object.DynamicObject;
  * is generalized as needed and the expected length is increased until all elements fit.
  * <p>
  * Append nodes handle only one strategy, but must still return a valid storage when:
- * <ul>
- * <li>The element(s) added do not match the strategy.</li>
+ *
+ * <li>The element(s) added do not match the strategy.
  * <li>The being-built storage no longer matches the strategy, due to the node having been replaced
- * by another thread or by another usage (e.g. recursive) of this ArrayBuilderNode.</li>
- * </ul>
+ * by another thread or by another usage (e.g. recursive) of this ArrayBuilderNode.
  */
 public abstract class ArrayBuilderNode extends RubyContextNode {
 
