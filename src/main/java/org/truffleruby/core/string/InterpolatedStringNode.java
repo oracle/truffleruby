5a8d6877dbb03b1e45645b3372194d90596fef22
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-03-07 13:51:53 +0100

    ensure proper encoding for interpolated string

--- src/main/java/org/truffleruby/core/string/InterpolatedStringNode.java
+++ src/main/java/org/truffleruby/core/string/InterpolatedStringNode.java
@@ -10,14 +10,10 @@
 package org.truffleruby.core.string;
 
 import org.jcodings.Encoding;
-import org.jcodings.specific.ASCIIEncoding;
-import org.jcodings.specific.USASCIIEncoding;
-import org.truffleruby.Layouts;
 import org.truffleruby.core.cast.ToSNode;
 import org.truffleruby.core.rope.RopeOperations;
 import org.truffleruby.language.RubyContextSourceNode;
 import org.truffleruby.language.RubyGuards;
-import org.truffleruby.language.dispatch.CallDispatchHeadNode;
 import org.truffleruby.language.objects.IsTaintedNode;
 import org.truffleruby.language.objects.TaintNode;
 
@@ -33,32 +29,31 @@ public final class InterpolatedStringNode extends RubyContextSourceNode {
     @Children private final ToSNode[] children;
 
     @Child private StringNodes.StringAppendPrimitiveNode appendNode;
-    @Child private CallDispatchHeadNode dupNode;
     @Child private IsTaintedNode isTaintedNode;
     @Child private TaintNode taintNode;
 
+    private final Encoding encoding;
+
     private final ConditionProfile taintProfile = ConditionProfile.createCountingProfile();
 
-    public InterpolatedStringNode(ToSNode[] children) {
+    public InterpolatedStringNode(ToSNode[] children, Encoding encoding) {
         this.children = children;
+        this.encoding = encoding;
     }
 
     @ExplodeLoop
     @Override
     public Object execute(VirtualFrame frame) {
         assert children.length > 0;
-        DynamicObject builder = null;
+
+        // Start with an empty string to ensure the result has class String and the proper encoding.
+        DynamicObject builder = StringOperations.createString(getContext(), RopeOperations.emptyRope(encoding));
         boolean tainted = false;
 
         // TODO (nirvdrum 11-Jan-16) Rewrite to avoid massively unbalanced trees.
-        for (int n = 0; n < children.length; n++) {
-            final Object toInterpolate = children[n].execute(frame);
+        for (ToSNode child : children) {
+            final Object toInterpolate = child.execute(frame);
             assert RubyGuards.isRubyString(toInterpolate);
-            if (n == 0) {
-                // Start with an empty string, in the case the initial string is of a subclass of String.
-                Encoding encoding = Layouts.STRING.getRope((DynamicObject) toInterpolate).getEncoding();
-                builder = StringOperations.createString(getContext(), RopeOperations.emptyRope(encoding));
-            }
             builder = executeStringAppend(builder, (DynamicObject) toInterpolate);
             tainted |= executeIsTainted(toInterpolate);
         }
@@ -84,14 +79,6 @@ public final class InterpolatedStringNode extends RubyContextSourceNode {
             appendNode = insert(StringNodesFactory.StringAppendPrimitiveNodeFactory.create(null));
         }
         return appendNode.executeStringAppend(builder, string);
-    }
-
-    private Object callDup(Object string) {
-        if (dupNode == null) {
-            CompilerDirectives.transferToInterpreterAndInvalidate();
-            dupNode = insert(CallDispatchHeadNode.createPrivate());
-        }
-        return dupNode.call(string, "dup");
     }
 
     private boolean executeIsTainted(Object object) {
