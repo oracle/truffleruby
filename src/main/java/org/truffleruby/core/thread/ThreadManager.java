a7b41c77163b7a5741f60562052f8258d9edefd5
Duncan MacGregor <duncan.macgregor@oracle.com>
2020-03-30 12:37:39 +0100

    Fix linter error.

--- src/main/java/org/truffleruby/core/thread/ThreadManager.java
+++ src/main/java/org/truffleruby/core/thread/ThreadManager.java
@@ -19,7 +19,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.concurrent.locks.Lock;
 import java.util.function.Supplier;
 
