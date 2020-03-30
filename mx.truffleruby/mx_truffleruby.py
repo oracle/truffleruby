4bbd20d784317b0672638a8f63f686e4509fb01e
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-03-30 15:38:34 +0200

    Revert "add blacklisted method check flag"
    This reverts commit cb5374e4c2e22607e913bd22b0e066f4ba13fcd9.

--- mx.truffleruby/mx_truffleruby.py
+++ mx.truffleruby/mx_truffleruby.py
@@ -223,7 +223,6 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
             main_class='org.truffleruby.launcher.RubyLauncher',
             build_args=[
                 '-H:+DetectUserDirectoriesInImageHeap',
-                '-H:+TruffleCheckBlackListedMethods',
             ],
             language='ruby',
             links=['bin/<exe:ruby>'],
