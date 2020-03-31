aa548e80dac96f49f1adcfe3479a4f80c6136b7d
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-03-31 14:51:27 +0200

    Revert "add TreatPerformanceWarningsAsErrors flag for all benchmarks	add PerformanceWarningsAreFatal flag for all benchmarks"
    This reverts commit 9b46367cbbf91e4bce42cb9e38408fdc82fd635a.

--- tool/jt.rb
+++ tool/jt.rb
@@ -1799,9 +1799,7 @@ EOS
   def benchmark(*args)
     vm_args = []
     if truffleruby_compiler?
-      vm_args << '--experimental-options'
-      vm_args << '--engine.CompilationFailureAction=ExitVM'
-      vm_args << '--engine.TreatPerformanceWarningsAsErrors=all'
+      vm_args << '--experimental-options' << '--engine.CompilationFailureAction=ExitVM'
     end
     run_ruby(*vm_args, "#{TRUFFLERUBY_DIR}/bench/benchmark", *args, use_exec: true)
   end
