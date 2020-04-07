3751aeafce9376f31832627e0fb59caf27bbc576
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-04-07 13:02:11 +0200

    add blacklisted method check flag

--- mx.truffleruby/mx_truffleruby.py
+++ mx.truffleruby/mx_truffleruby.py
@@ -223,6 +223,7 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
             main_class='org.truffleruby.launcher.RubyLauncher',
             build_args=[
                 '-H:+DetectUserDirectoriesInImageHeap',
+                '-H:+TruffleCheckBlackListedMethods',
             ],
             language='ruby',
             links=['bin/<exe:ruby>'],
