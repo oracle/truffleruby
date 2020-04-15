e64af8ebb8ec9444c32eeedff60cc3df40a83e0c
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-04-15 14:12:20 +0200

    eliminate code outside of it-blocks in rbasic spec

--- spec/ruby/optional/capi/shared/rbasic.rb
+++ spec/ruby/optional/capi/shared/rbasic.rb
@@ -1,38 +1,42 @@
 describe :rbasic, shared: true do
 
-  specs = CApiRBasicSpecs.new
-  RUBY_FL_TAINT     = specs.taint_flag
-  RUBY_FL_FREEZE    = specs.freeze_flag
+  before :all do
+    class << self
+      specs = CApiRBasicSpecs.new
+      define_method(:_TAINT) { specs.taint_flag }
+      define_method(:_FREEZE) { specs.freeze_flag }
+    end
+  end
 
   it "reports the appropriate FREEZE and TAINT flags for the object when reading" do
     obj, _ = @object.data
     @object.get_flags(obj).should == 0
     obj.taint
-    @object.get_flags(obj).should == RUBY_FL_TAINT
+    @object.get_flags(obj).should == _TAINT
     obj.untaint
     @object.get_flags(obj).should == 0
     obj.freeze
-    @object.get_flags(obj).should == RUBY_FL_FREEZE
+    @object.get_flags(obj).should == _FREEZE
 
     obj, _ = @object.data
     obj.taint
     obj.freeze
-    @object.get_flags(obj).should == RUBY_FL_FREEZE | RUBY_FL_TAINT
+    @object.get_flags(obj).should == _FREEZE | _TAINT
   end
 
   it "supports setting the FREEZE and TAINT flags" do
     obj, _ = @object.data
-    @object.set_flags(obj, RUBY_FL_TAINT).should == RUBY_FL_TAINT
+    @object.set_flags(obj, _TAINT).should == _TAINT
     obj.tainted?.should == true
     @object.set_flags(obj, 0).should == 0
     obj.tainted?.should == false
-    @object.set_flags(obj, RUBY_FL_FREEZE).should == RUBY_FL_FREEZE
+    @object.set_flags(obj, _FREEZE).should == _FREEZE
     obj.frozen?.should == true
 
-    @object.get_flags(obj).should == RUBY_FL_FREEZE
+    @object.get_flags(obj).should == _FREEZE
 
     obj, _ = @object.data
-    @object.set_flags(obj, RUBY_FL_FREEZE | RUBY_FL_TAINT).should == RUBY_FL_FREEZE | RUBY_FL_TAINT
+    @object.set_flags(obj, _FREEZE | _TAINT).should == _FREEZE | _TAINT
     obj.tainted?.should == true
     obj.frozen?.should == true
   end
@@ -42,7 +46,7 @@ describe :rbasic, shared: true do
     @object.get_flags(obj) == 0
     @object.set_flags(obj, 1 << 14 | 1 << 16).should == 1 << 14 | 1 << 16
     obj.taint
-    @object.get_flags(obj).should == RUBY_FL_TAINT | 1 << 14 | 1 << 16
+    @object.get_flags(obj).should == _TAINT | 1 << 14 | 1 << 16
     obj.untaint
     @object.get_flags(obj).should == 1 << 14 | 1 << 16
     @object.set_flags(obj, 0).should == 0
@@ -50,9 +54,9 @@ describe :rbasic, shared: true do
 
   it "supports copying the flags from one object over to the other" do
     obj1, obj2 = @object.data
-    @object.set_flags(obj1, RUBY_FL_TAINT | 1 << 14 | 1 << 16)
+    @object.set_flags(obj1, _TAINT | 1 << 14 | 1 << 16)
     @object.copy_flags(obj2, obj1)
-    @object.get_flags(obj2).should == RUBY_FL_TAINT | 1 << 14 | 1 << 16
+    @object.get_flags(obj2).should == _TAINT | 1 << 14 | 1 << 16
     @object.set_flags(obj1, 0)
     @object.copy_flags(obj2, obj1)
     @object.get_flags(obj2).should == 0
