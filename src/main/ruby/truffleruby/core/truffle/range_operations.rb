0a37b4b727cbe3714c45affd74a1a4e986cd9b14
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-05-07 18:00:12 +0200

    add normalizing range operations

--- src/main/ruby/truffleruby/core/truffle/range_operations.rb
+++ src/main/ruby/truffleruby/core/truffle/range_operations.rb
@@ -72,5 +72,63 @@ module Truffle
       [first, last, step_size]
     end
 
+    # Returns an array containing normalized long range bounds `[start, ending]`,
+    # such that both are longs (if conversion is impossible, an error is raised),
+    # and `ending` is exluded from the range.
+    #
+    # Alternatively, `ending` can also be nil for unbounded ranges.
+    def self.normalized(range)
+      start = Truffle::Type.rb_num2long(range.first)
+      if Primitive.object_equal(range.end, nil)
+        ending = nil
+      else
+        ending = Truffle::Type.rb_num2long(range.end)
+        ending += 1 unless range.exclude_end?
+      end
+      [start, ending]
+    end
+
+    # Returns an array containing normalized long range bounds `[start, ending]`,
+    # such that both are longs (if conversion is impossible, an error is raised),
+    # positive (negative values are converted by adding `size`)
+    # and `ending` is exluded from the range.
+    #
+    # Alternatively, `ending` can also be nil for unbounded ranges.
+    #
+    # `size` is assumed to be normalized: fitting in a long, and positive.
+    def self.normalized_with_size(range, size)
+      start = Truffle::Type.rb_num2long(range.first)
+      start += size if start < 0
+      if Primitive.object_equal(range.end, nil)
+        ending = nil
+      else
+        ending = Truffle::Type.rb_num2long(range.end)
+        ending += size if ending < 0
+        ending += 1 unless range.exclude_end?
+      end
+      [start, ending]
+    end
+
+    # Returns an array containing normalized long range parameters `[start, length]`,
+    # such that both are longs (if conversion is impossible, an error is raised),
+    # positive (negative values are converted by adding `size`).
+    #
+    # The length will *not* be clamped to size!
+    #
+    # `size` is assumed to be normalized: fitting in a long, and positive.
+    def self.normalized_start_length(range, size)
+      start = Truffle::Type.rb_num2long(range.first)
+      start += size if start < 0
+      if Primitive.object_equal(range.end, nil)
+        length = size
+      else
+        length = Truffle::Type.rb_num2long(range.end)
+        length += size if length < 0
+        length += 1 unless range.exclude_end?
+        length -= start
+      end
+      [start, length]
+    end
+
   end
 end
