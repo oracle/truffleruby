c0d1d352dec9d5412aba55e7276893ab162a209e
Benoit Daloze <benoit.daloze@oracle.com>
2020-05-12 14:12:35 +0200

    Improve TracePoint specs

--- spec/ruby/core/tracepoint/lineno_spec.rb
+++ spec/ruby/core/tracepoint/lineno_spec.rb
@@ -3,8 +3,11 @@ require_relative '../../spec_helper'
 describe 'TracePoint#lineno' do
   it 'returns the line number of the event' do
     lineno = nil
-    TracePoint.new(:line) { |tp| lineno = tp.lineno }.enable do
-      lineno.should == 7
+    TracePoint.new(:line) { |tp|
+      lineno = tp.lineno
+    }.enable do
+      line_event = true
     end
+    lineno.should == __LINE__ - 2
   end
 end
--- spec/ruby/core/tracepoint/path_spec.rb
+++ spec/ruby/core/tracepoint/path_spec.rb
@@ -3,16 +3,21 @@ require_relative '../../spec_helper'
 describe 'TracePoint#path' do
   it 'returns the path of the file being run' do
     path = nil
-    TracePoint.new(:line) { |tp| path = tp.path }.enable do
-      path.should == "#{__FILE__}"
+    TracePoint.new(:line) { |tp|
+      path = tp.path
+    }.enable do
+      line_event = true
     end
+    path.should == "#{__FILE__}"
   end
 
   it 'equals (eval) inside an eval for :end event' do
     path = nil
-    TracePoint.new(:end) { |tp| path = tp.path }.enable do
+    TracePoint.new(:end) { |tp|
+      path = tp.path
+    }.enable do
       eval("module TracePointSpec; end")
-      path.should == '(eval)'
     end
+    path.should == '(eval)'
   end
 end
