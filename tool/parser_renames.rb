7e1c9e9979c4eb84a6a476e8b4ecaa24aedc9995
Benoit Daloze <benoit.daloze@oracle.com>
2020-05-25 19:05:56 +0200

    Add a tool to automatically do parser renames and other small changes

---
+++ tool/parser_renames.rb
@@ -0,0 +1,36 @@
+contents = STDIN.read
+
+header_regexp = /\A%{.+?%}/m
+trailer_regexp = /^%%[^%]+\z/m
+original = `git show master:src/main/java/org/truffleruby/parser/parser/RubyParser.y`
+
+original_header = original[header_regexp, 0] or raise
+original_trailer = original[trailer_regexp, 0] or raise
+
+contents = contents.lines.map do |line|
+  line = line.gsub(/\t/, ' '*8)
+
+  if line.include?('//')
+    line, comment = line.split('//', 2)
+  end
+
+  # import org.jruby.ast.IfNode;
+  # import org.truffleruby.parser.ast.IfParseNode;
+  line = line.gsub(/\borg\.jruby\.ast\./, 'org.truffleruby.parser.ast.')
+  line = line.gsub(/\b([A-Z]\w+)Node\b/, '\1ParseNode')
+  line = line.gsub(/\b(ILiteral|IName)ParseNode\b/, '\1Node')
+
+  line = line.gsub(/\bNode\b/, 'ParseNode')
+  line = line.gsub(/\bISourcePosition\b/, 'SourceIndexLength')
+
+  line = line.gsub(/\bsupport.warn\(ID[^,]+, /, 'support.warn(')
+  line = line.gsub(/<KeyValuePair>/, '<ParseNodeTuple>')
+
+  line = "#{line}//#{comment}" if comment
+  line
+end.join
+
+contents = contents.sub(header_regexp, original_header)
+contents = contents.sub(trailer_regexp, original_trailer)
+
+puts contents
