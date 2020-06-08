a18cbf3775b13e4238f1916464016a3a8825782d
Benoit Daloze <benoit.daloze@oracle.com>
2020-06-08 15:21:15 +0200

    Update versions

--- src/main/c/cext/tracepoint.c
+++ src/main/c/cext/tracepoint.c
@@ -4,7 +4,7 @@
 // TracePoint, rb_tracepoint_*
 
 VALUE rb_tracepoint_new(VALUE target_thval, rb_event_flag_t events, void (*func)(VALUE, void *), void *data) {
-  // target_thval parameter is unused as of 2.6.5
+  // target_thval parameter is unused as of MRI 2.6
   return rb_tr_wrap(polyglot_invoke(RUBY_CEXT, "rb_tracepoint_new", events, func, data));
 }
 
