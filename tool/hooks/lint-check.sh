f9cf1712e1807d2d5196d3bf73cc530a2d500084
Benoit Daloze <benoit.daloze@oracle.com>
2020-06-15 18:33:57 +0200

    Also document using "jt lint fast" as a post-commit hook

--- tool/hooks/lint-check.sh
+++ tool/hooks/lint-check.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Set this up by before each commit with
+# Set this up before each commit with
 #   cp tool/hooks/lint-check.sh .git/hooks/pre-commit
+# or after each commit with
+#   cp tool/hooks/lint-check.sh .git/hooks/post-commit
 # or before each push with
 #   cp tool/hooks/lint-check.sh .git/hooks/pre-push
 # The choice is yours.
