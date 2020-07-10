ce915905010bacafebb9589b70f68b70e55ff9f6
Benoit Daloze <benoit.daloze@oracle.com>
2020-07-10 21:39:42 +0200

    Ignore internal frames in RubyGems' Kernel#warn
    * See https://github.com/oracle/truffleruby/issues/2046

--- lib/mri/rubygems/core_ext/kernel_warn.rb
+++ lib/mri/rubygems/core_ext/kernel_warn.rb
@@ -4,7 +4,7 @@
 if RUBY_VERSION >= "2.5"
 
   module Kernel
-    path = "#{__dir__}/" # Frames to be skipped start with this path.
+    rubygems_path = "#{__dir__}/" # Frames to be skipped start with this path.
 
     # Suppress "method redefined" warning
     original_warn = instance_method(:warn)
@@ -32,7 +32,8 @@ if RUBY_VERSION >= "2.5"
 
           start += 1
 
-          unless loc.path.start_with?(path)
+          path = loc.path
+          unless path.start_with?(rubygems_path) or path.start_with?('<internal:')
             # Non-rubygems frames
             uplevel -= 1
           end
