40d859f41a517b7010fd7ba6cd26238677860a31
Benoit Daloze <benoit.daloze@oracle.com>
2020-07-27 16:21:47 +0200

    Share more logic between ReadLocalVariableNode and ReadDeclarationVariableNode

--- src/main/java/org/truffleruby/parser/ReadLocalNode.java
+++ src/main/java/org/truffleruby/parser/ReadLocalNode.java
@@ -9,11 +9,48 @@
  */
 package org.truffleruby.parser;
 
+import org.truffleruby.RubyContext;
 import org.truffleruby.language.RubyContextSourceNode;
 import org.truffleruby.language.RubyNode;
+import org.truffleruby.language.locals.LocalVariableType;
+import org.truffleruby.language.locals.ReadFrameSlotNode;
+import org.truffleruby.utils.Utils;
+
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.VirtualFrame;
 
 public abstract class ReadLocalNode extends RubyContextSourceNode {
 
+    protected final FrameSlot frameSlot;
+    protected final LocalVariableType type;
+
+    @Child protected ReadFrameSlotNode readFrameSlotNode;
+
+    public ReadLocalNode(FrameSlot frameSlot, LocalVariableType type) {
+        this.frameSlot = frameSlot;
+        this.type = type;
+    }
+
+    protected abstract Object readFrameSlot(VirtualFrame frame);
+
     public abstract RubyNode makeWriteNode(RubyNode rhs);
+
+    @Override
+    public Object isDefined(VirtualFrame frame, RubyContext context) {
+        switch (type) {
+            case FRAME_LOCAL:
+                return coreStrings().LOCAL_VARIABLE.createInstance();
+
+            case FRAME_LOCAL_GLOBAL:
+                if (readFrameSlot(frame) != nil) {
+                    return coreStrings().GLOBAL_VARIABLE.createInstance();
+                } else {
+                    return nil;
+                }
+
+            default:
+                throw Utils.unsupportedOperation("didn't expect local type ", type);
+        }
+    }
 
 }
