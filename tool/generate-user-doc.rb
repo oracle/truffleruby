d3d5d416994752af7413be6e32ad1249e4631530
Benoit Daloze <benoit.daloze@oracle.com>
2020-08-14 12:48:56 +0200

    Remove the Documentation section for the website
    * Redundant with the sidebar menu.

--- tool/generate-user-doc.rb
+++ tool/generate-user-doc.rb
@@ -21,6 +21,12 @@ contents = contents.gsub(/\]\(([^)]+)\)/) {
   "](#{link})"
 }
 
+# Remove the Documentation section for the website (redundant with the sidebar menu)
+contents = contents.sub(/^## Documentation\n.+?\n##/m, '##')
+
+# Remove 'built by Oracle Labs' for the website (redundant)
+contents = contents.sub(" built by\n[Oracle Labs](https://labs.oracle.com)", '')
+
 # Add top-level title as expected by the website
 contents = "# TruffleRuby\n\n#{contents}"
 
