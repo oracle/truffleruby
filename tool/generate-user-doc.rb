0c5ad7e29f432aad11f50438263991f278ecb8e8
Benoit Daloze <benoit.daloze@oracle.com>
2020-08-14 12:07:04 +0200

    Use absolute links for links outside of doc/user for the website

--- tool/generate-user-doc.rb
+++ tool/generate-user-doc.rb
@@ -2,10 +2,26 @@ root = File.expand_path('../..', __FILE__)
 
 contents = File.read("#{root}/README.md")
 
-# Fix relative links
-contents = contents.gsub('(doc/user/', '(')
+# Fix links
+contents = contents.gsub(/\]\(([^)]+)\)/) {
+  link = $1
+  if link.start_with?('#')
+    # link to same document
+  elsif link.start_with?('http://') or link.start_with?('https://')
+    # absolute link
+  elsif link.start_with?("doc/user/")
+    link = link.sub("doc/user/", "")
+  else
+    if link.end_with?('.png')
+      link = "https://raw.githubusercontent.com/oracle/truffleruby/master/#{link}"
+    else
+      link = "https://github.com/oracle/truffleruby/blob/master/#{link}"
+    end
+  end
+  "](#{link})"
+}
 
 # Add top-level title as expected by the website
-contents = "# For Ruby User\n\n#{contents}"
+contents = "# TruffleRuby\n\n#{contents}"
 
 File.write("#{root}/doc/user/README.md", contents)
