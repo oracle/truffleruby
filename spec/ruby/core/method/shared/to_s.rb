6dca58c16295cf5c1aeca64afad0e1b2ba5ca8a4
Benoit Daloze <benoit.daloze@oracle.com>
2020-10-06 12:19:33 +0200

    Add more specs for Method and UnboundMethod#inspect

--- spec/ruby/core/method/shared/to_s.rb
+++ spec/ruby/core/method/shared/to_s.rb
@@ -4,7 +4,7 @@ require_relative '../fixtures/classes'
 describe :method_to_s, shared: true do
   before :each do
     @m = MethodSpecs::MySub.new.method :bar
-    @string = @m.send(@method).sub(/0x\w+/, '0xXXXXXX')
+    @string = @m.send(@method)
   end
 
   it "returns a String" do
@@ -32,13 +32,21 @@ describe :method_to_s, shared: true do
     @string.should =~ /MethodSpecs::MySub/
   end
 
+  it "returns a String including all details" do
+    @string.should.start_with? "#<Method: MethodSpecs::MySub(MethodSpecs::MyMod)#bar"
+  end
+
+  it "does not show the defining module if it is the same as the receiver class" do
+    MethodSpecs::A.new.method(:baz).send(@method).should.start_with? "#<Method: MethodSpecs::A#baz"
+  end
+
   ruby_version_is '3.0' do
     it "returns a String containing the Module containing the method if object has a singleton class but method is not defined in the singleton class" do
       obj = MethodSpecs::MySub.new
       obj.singleton_class
       @m = obj.method(:bar)
-      @string = @m.send(@method).sub(/0x\w+/, '0xXXXXXX')
-      @string.should =~ /\A#<Method: MethodSpecs::MySub\(MethodSpecs::MyMod\)#bar\(\) /
+      @string = @m.send(@method)
+      @string.should.start_with? "#<Method: MethodSpecs::MySub(MethodSpecs::MyMod)#bar"
     end
   end
 
@@ -46,7 +54,7 @@ describe :method_to_s, shared: true do
     obj = MethodSpecs::MySub.new
     def obj.bar; end
     @m = obj.method(:bar)
-    @string = @m.send(@method).sub(/0x\w+/, '0xXXXXXX')
-    @string.should =~ /\A#<Method: #<MethodSpecs::MySub:0xXXXXXX>\.bar/
+    @string = @m.send(@method).sub(/0x\h+/, '0xXXXXXX')
+    @string.should.start_with? "#<Method: #<MethodSpecs::MySub:0xXXXXXX>.bar"
   end
 end
