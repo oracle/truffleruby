b5756b053d82ea02e36b4c244e14acd8d6564040
Duncan MacGregor <duncan.macgregor@oracle.com>
2020-10-09 18:13:02 +0100

    Add tests for modifying rest args before post args.

--- spec/ruby/language/super_spec.rb
+++ spec/ruby/language/super_spec.rb
@@ -303,6 +303,13 @@ describe "The super keyword" do
     SuperSpecs::ZSuperWithRestOthersAndPost::B.new.m(1, 2).should == []
   end
 
+  it "without explicit arguments passes arguments, rest arguments including modifications, and post arguments" do
+    SuperSpecs::ZSuperWithRestAndPost::B.new.m_modified(1, 2, 3, 4, 5).should == [1, 14, 3]
+    SuperSpecs::ZSuperWithRestOthersAndPost::B.new.m_modified(1, 2, 3, 4, 5).should == [2, 14, 4]
+    SuperSpecs::ZSuperWithRestAndPost::B.new.m_modified(1, 2).should == [nil, 14]
+    SuperSpecs::ZSuperWithRestOthersAndPost::B.new.m_modified(1, 2).should == [nil, 14]
+  end
+
   it "without explicit arguments passes arguments and rest arguments including any modifications" do
     SuperSpecs::ZSuperWithRestAndOthers::B.new.m_modified(1, 2, 3, 4, 5).should == [3, 14, 5]
   end
