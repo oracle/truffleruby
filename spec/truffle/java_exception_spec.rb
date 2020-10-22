2595f7fa4f9b85dfd20f5dae911af374ec40f765
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-10-22 16:09:31 +0200

    update java_exception_spec.rb

--- spec/truffle/java_exception_spec.rb
+++ spec/truffle/java_exception_spec.rb
@@ -11,54 +11,25 @@ require_relative '../ruby/spec_helper'
 describe "Java exceptions" do
 
   it "formats to include the source information" do
-    -> { Truffle::Debug.throw_java_exception 'message' }.should raise_error { |e|
-      message = e.message.gsub(/:\d+/, ':LINE')
-      message.lines[0].should == "message (RuntimeException)\n"
-      message.lines[1].should == "\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwingMethod(TruffleDebugNodes.java:LINE)\n"
-    }
-  end
-
-  it "includes the first lines of the Java stacktrace for uncaught Java exceptions" do
-    -> { Truffle::Debug.throw_java_exception 'message' }.should raise_error { |e|
-      message = e.message.gsub(/:\d+/, ':LINE')
-      message.lines[0...4].join.should == <<EOS
-message (RuntimeException)
-\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwingMethod(TruffleDebugNodes.java:LINE)
-\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.callingMethod(TruffleDebugNodes.java:LINE)
-\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwJavaException(TruffleDebugNodes.java:LINE)
-EOS
-    }
-  end
-
-  it "stops the Java stacktrace at the CallTarget boundary" do
-    -> { Truffle::Debug.throw_java_exception 'message' }.should raise_error { |e|
-      message = e.message
-      message.should.include?('RubyRootNode.execute')
-      message.should_not.include?('.RubyCallNode.')
-    }
+    out = ruby_exe("Truffle::Debug.throw_java_exception 'message'", args: "2>&1")
+    lines = out.gsub(/:\d+/, ':LINE').lines
+    lines[0].should == "truffleruby: an exception escaped out of the interpreter - this is an implementation bug\n"
+    lines[1].should == "org.graalvm.polyglot.PolyglotException: java.lang.RuntimeException: message\n"
+    lines[2].should == "\tat org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwingMethod(TruffleDebugNodes.java:LINE)\n"
+    lines[3].should == "\tat org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.callingMethod(TruffleDebugNodes.java:LINE)\n"
+    lines[4].should == "\tat org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionNode.throwJavaException(TruffleDebugNodes.java:LINE)\n"
+    lines.should include "\tat org.truffleruby.launcher.RubyLauncher.main(RubyLauncher.java:LINE)\n"
   end
 
   it "formats to include the source information including cause" do
-    -> { Truffle::Debug.throw_java_exception_with_cause 'message' }.should raise_error { |e|
-      message = e.message.gsub(/:\d+/, ':LINE')
-
-      message.should include <<EOS
-message (RuntimeException)
-\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:LINE)
+    out = ruby_exe("Truffle::Debug.throw_java_exception_with_cause 'message'", args: "2>&1")
+    message = out.gsub(/:\d+/, ':LINE')
+    message.should include <<EOS
+org.graalvm.polyglot.PolyglotException: java.lang.RuntimeException: message
+\tat org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:LINE)
 EOS
-
-      message.should include <<EOS
-Caused by:
-cause 1 (RuntimeException)
-\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:LINE)
-EOS
-
-      message.should include <<EOS
-Caused by:
-cause 2 (RuntimeException)
-\tfrom org.truffleruby.debug.TruffleDebugNodes$ThrowJavaExceptionWithCauseNode.throwJavaExceptionWithCause(TruffleDebugNodes.java:LINE)
-EOS
-    }
+    message.should include "Caused by: java.lang.RuntimeException: cause 1"
+    message.should include "Caused by: java.lang.RuntimeException: cause 2"
   end
 
   it "AssertionError in another Thread is rethrown on the main Ruby Thread" do
@@ -68,11 +39,10 @@ EOS
     end
     sleep
     RUBY
-
     out = ruby_exe(code, args: "2>&1")
     $?.exitstatus.should == 1
-    out.should.include?('terminated with internal error')
-    out.should.include?('Caused by:')
-    out.should.include?('custom_assertion_error_message (AssertionError)')
+    out.should include "org.graalvm.polyglot.PolyglotException: java.lang.RuntimeException: Ruby Thread"
+    out.should include "terminated with internal error:"
+    out.should include "Caused by: java.lang.AssertionError: custom_assertion_error_message"
   end
 end
