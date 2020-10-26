00793325e71acc33172ece793eb5d6ea0080226d
Benoit Daloze <benoit.daloze@oracle.com>
2020-10-26 13:42:51 +0100

    Turn ALLOW_TO_STRING into a debug flag as in most cases it's OK to store the byte[]
    * And preventing {RubyString,Rope}#toString() calls is really challenging
      and does not seem worth it.
    
    (cherry picked from commit b4fcc2cfff36ace34ccb062432fa23a5e8231487)

--- src/main/java/org/truffleruby/core/rope/ManagedRope.java
+++ src/main/java/org/truffleruby/core/rope/ManagedRope.java
@@ -54,12 +54,14 @@ public abstract class ManagedRope extends Rope {
 
     @Override
     public final String toString() {
-        assert ALLOW_TO_STRING;
-
-        final byte[] bytesBefore = bytes;
-        final String string = RopeOperations.decodeOrEscapeBinaryRope(this, RopeOperations.flattenBytes(this));
-        assert bytes == bytesBefore : "bytes should not be modified by Rope#toString() as otherwise inspecting a Rope would have a side effect";
-        return string;
+        if (DEBUG_ROPE_BYTES) {
+            final byte[] bytesBefore = bytes;
+            final String string = RopeOperations.decodeOrEscapeBinaryRope(this, RopeOperations.flattenBytes(this));
+            assert bytes == bytesBefore : "bytes should not be modified by Rope#toString() as otherwise inspecting a Rope would have a side effect";
+            return string;
+        } else {
+            return RopeOperations.decodeRope(this);
+        }
     }
 
 }
