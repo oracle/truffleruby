cbab00a4ed85ffc2e6c5a09613e0b08f7ccc854d
Benoit Daloze <benoit.daloze@oracle.com>
2020-11-05 17:27:50 +0100

    Skip tracepointchecker and iseq_loader_checker for MRI tests

--- test/mri/tests/runner.rb
+++ test/mri/tests/runner.rb
@@ -6,9 +6,9 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require 'test/unit'
 
 require "profile_test_all" if ENV.key?('RUBY_TEST_ALL_PROFILE')
-require "tracepointchecker"
+require "tracepointchecker" unless defined?(::TruffleRuby)
 require "zombie_hunter"
-require "iseq_loader_checker"
+require "iseq_loader_checker" unless defined?(::TruffleRuby)
 require "gc_compact_checker"
 require_relative "../test-coverage.rb" if ENV.key?('COVERAGE')
 
