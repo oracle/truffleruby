7be4bd93d91d82e1ccee7abca146ad45e45f82eb
Benoit Daloze <benoit.daloze@oracle.com>
2020-11-16 13:24:42 +0100

    JT: jvmci_update_and_version => jvmci_version

--- tool/jt.rb
+++ tool/jt.rb
@@ -143,19 +143,14 @@ module Utilities
     end
   end
 
-  def jvmci_update_and_version
-    if env = ENV['JVMCI_VERSION']
-      unless /8u(\d+(?:\+\d+)?)-(jvmci-\d+\.\d+-b\d+)/ =~ env
-        raise 'Could not parse JDK update and JVMCI version from $JVMCI_VERSION'
-      end
-    else
+  def jvmci_version
+    @jvmci_version ||= begin
       ci = File.read("#{TRUFFLERUBY_DIR}/common.json")
-      unless /{\s*"name"\s*:\s*"openjdk"\s*,\s*"version"\s*:\s*"8u(\d+(?:\+\d+)?)-(jvmci-[^"]+)"\s*,/ =~ ci
+      unless /{\s*"name"\s*:\s*"openjdk"\s*,\s*"version"\s*:\s*"8u(?:\d+(?:\+\d+)?)-(jvmci-[^"]+)"\s*,/ =~ ci
         raise 'JVMCI version not found in common.json'
       end
+      $1
     end
-    update, jvmci = $1, $2
-    [update, jvmci]
   end
 
   def send_signal(signal, pid)
@@ -518,7 +513,6 @@ module Utilities
       end
       java_home ||= ENV['JAVA_HOME']
 
-      _, jvmci_version = jvmci_update_and_version
       if java_home
         if java_home.include?(jvmci_version)
           :use_env_java_home
@@ -1939,7 +1933,6 @@ module Commands
   private def install_jvmci(download_message, ee)
     jdk_name = ee ? 'oraclejdk8' : 'openjdk8'
 
-    _, jvmci_version = jvmci_update_and_version
     java_home = "#{CACHE_EXTRA_DIR}/#{jdk_name}-#{jvmci_version}"
     unless File.directory?(java_home)
       STDERR.puts "#{download_message} (#{jdk_name})"
