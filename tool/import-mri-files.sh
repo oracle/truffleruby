6b220fef0796736c1bc42e5a4ca403fcf32cd9cc
Duncan MacGregor <duncan.macgregor@oracle.com>
2020-12-18 11:56:37 +0000

    Update ruby import script.

--- tool/import-mri-files.sh
+++ tool/import-mri-files.sh
@@ -58,8 +58,8 @@ cp -r ../ruby/ext/ripper/lib/ripper lib/mri
 cp -r ../ruby/ext/syslog/lib/syslog lib/mri
 
 # Copy C extensions in ext/, sorted alphabetically
-rm -r src/main/c/{bigdecimal,etc,nkf,openssl,psych,rbconfig-sizeof,syslog,ripper,zlib}
-mkdir src/main/c/{bigdecimal,etc,nkf,openssl,psych,rbconfig-sizeof,syslog,ripper,zlib}
+rm -r src/main/c/{bigdecimal,etc,nkf,openssl,psych,rbconfig-sizeof,syslog,ripper,zlib,io/console}
+mkdir src/main/c/{bigdecimal,etc,nkf,openssl,psych,rbconfig-sizeof,syslog,ripper,zlib,io/console}
 cp ../ruby/ext/bigdecimal/*.{c,gemspec,h,rb} src/main/c/bigdecimal
 cp ../ruby/ext/etc/*.{c,rb} src/main/c/etc
 cp ../ruby/ext/nkf/*.{c,rb} src/main/c/nkf
@@ -70,6 +70,7 @@ cp -r ../ruby/ext/psych/yaml src/main/c/psych
 cp ../ruby/ext/rbconfig/sizeof/*.{c,rb} src/main/c/rbconfig-sizeof
 cp ../ruby/ext/syslog/*.{c,rb} src/main/c/syslog
 cp ../ruby/ext/zlib/*.{c,rb} src/main/c/zlib
+cp ../ruby/ext/io/console/*.{c,rb} src/main/c/io/console
 
 # Ripper
 cp "$RUBY_BUILD_DIR"/{id.h,symbol.h} lib/cext/include/truffleruby/internal
