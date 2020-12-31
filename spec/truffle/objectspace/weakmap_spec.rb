723d9c89ac89ed7b4bd45e67e74ec5106d378cef
Benoit Daloze <benoit.daloze@oracle.com>
2020-12-31 12:00:00 +0000

    Update copyright years for 2020

--- spec/truffle/objectspace/weakmap_spec.rb
+++ spec/truffle/objectspace/weakmap_spec.rb
@@ -2,6 +2,14 @@
 # rubocop:disable Lint/UselessAssignment
 #   (to nil out references to make unreachable)
 
+# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 2.0, or
+# GNU General Public License version 2, or
+# GNU Lesser General Public License version 2.1.
+
 require_relative '../../ruby/spec_helper'
 
 describe "ObjectSpace::WeakMap" do
