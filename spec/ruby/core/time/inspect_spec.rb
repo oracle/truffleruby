1ea2fa0959a90ab7ca371310c5f140f3d1f9bb92
Tom Stuart <tom.stuart@shopify.com>
2021-01-08 11:18:56 +0000

    Test for removal of trailing zeros when inspecting a microsecond-precision time

--- spec/ruby/core/time/inspect_spec.rb
+++ spec/ruby/core/time/inspect_spec.rb
@@ -10,6 +10,11 @@ describe "Time#inspect" do
       t.inspect.should == "2007-11-01 15:25:00.123456 UTC"
     end
 
+    it "omits trailing zeros from microseconds" do
+      t = Time.utc(2007, 11, 1, 15, 25, 0, 100000)
+      t.inspect.should == "2007-11-01 15:25:00.1 UTC"
+    end
+
     it "preserves nanoseconds" do
       t = Time.utc(2007, 11, 1, 15, 25, 0, 123456.789r)
       t.inspect.should == "2007-11-01 15:25:00.123456789 UTC"
