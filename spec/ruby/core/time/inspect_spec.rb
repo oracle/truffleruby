942e27b99efb25603e128765512b6d33e9c17081
Tom Stuart <tom.stuart@shopify.com>
2021-01-08 11:50:26 +0000

    Test for correct time zone when inspecting a non-UTC time

--- spec/ruby/core/time/inspect_spec.rb
+++ spec/ruby/core/time/inspect_spec.rb
@@ -15,6 +15,18 @@ describe "Time#inspect" do
       t.inspect.should == "2007-11-01 15:25:00.1 UTC"
     end
 
+    it "uses the correct time zone without microseconds" do
+      t = Time.utc(2000, 1, 1)
+      t = t.localtime(9*3600)
+      t.inspect.should == "2000-01-01 09:00:00 +0900"
+    end
+
+    it "uses the correct time zone with microseconds" do
+      t = Time.utc(2000, 1, 1, 0, 0, 0, 123456)
+      t = t.localtime(9*3600)
+      t.inspect.should == "2000-01-01 09:00:00.123456 +0900"
+    end
+
     it "preserves nanoseconds" do
       t = Time.utc(2007, 11, 1, 15, 25, 0, 123456.789r)
       t.inspect.should == "2007-11-01 15:25:00.123456789 UTC"
