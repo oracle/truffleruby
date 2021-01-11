60ed713bd209540d7aa29669c2f62f2a94e1b226
Benoit Daloze <benoit.daloze@oracle.com>
2021-01-11 14:29:09 +0100

    Update the graal import to the commit which enables MultiTier by default

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "63b9e6469704bd66178b196adab0eb372fbecbcf",
+                "version": "d7cc62ad964d16258dc5cd5db4daede81d661076",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "63b9e6469704bd66178b196adab0eb372fbecbcf",
+                "version": "d7cc62ad964d16258dc5cd5db4daede81d661076",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
