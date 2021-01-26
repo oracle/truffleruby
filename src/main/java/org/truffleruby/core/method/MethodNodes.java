35b56e7dacbb170f09e042af4cd1b016f16edafa
Benoit Daloze <benoit.daloze@oracle.com>
2021-01-26 13:56:45 +0100

    Ensure the block passed to CallBoundMethodNode is always nil or a RubyProc

--- src/main/java/org/truffleruby/core/method/MethodNodes.java
+++ src/main/java/org/truffleruby/core/method/MethodNodes.java
@@ -31,6 +31,7 @@ import org.truffleruby.core.rope.CodeRange;
 import org.truffleruby.core.string.RubyString;
 import org.truffleruby.core.string.StringNodes;
 import org.truffleruby.core.symbol.RubySymbol;
+import org.truffleruby.language.NotProvided;
 import org.truffleruby.language.RubyContextSourceNode;
 import org.truffleruby.language.RubyRootNode;
 import org.truffleruby.language.Visibility;
@@ -132,7 +133,8 @@ public abstract class MethodNodes {
 
         @Specialization
         protected Object call(RubyMethod method, Object[] arguments, Object maybeBlock) {
-            return callBoundMethodNode.executeCallBoundMethod(method, arguments, maybeBlock);
+            return callBoundMethodNode
+                    .executeCallBoundMethod(method, arguments, maybeBlock == NotProvided.INSTANCE ? nil : maybeBlock);
         }
 
     }
