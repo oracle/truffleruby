59d1be0a88d50bb34ef22db638858364e171a8cc
Benoit Daloze <benoit.daloze@oracle.com>
2021-02-12 18:06:31 +0100

    Update to graal just before compilation threshold changes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "71f5012eafd5b62809432c5006d61913b02f5d64",
+                "version": "ca1b62faf656d71e4bbb39660880ebf71d48e41a",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "71f5012eafd5b62809432c5006d61913b02f5d64",
+                "version": "ca1b62faf656d71e4bbb39660880ebf71d48e41a",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
