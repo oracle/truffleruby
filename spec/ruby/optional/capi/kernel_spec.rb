07f1939391b08e81de872fbb4f49fcb14a415083
Duncan MacGregor <duncan.macgregor@oracle.com>
2021-02-26 10:25:55 +0000

    Fix typo.

--- spec/ruby/optional/capi/kernel_spec.rb
+++ spec/ruby/optional/capi/kernel_spec.rb
@@ -313,7 +313,7 @@ describe "C-API Kernel function" do
       @s.rb_protect_null_status(42) { |x| raise }.should == nil
     end
 
-    it "poulates errinfo with the captured exception" do
+    it "populates errinfo with the captured exception" do
       proof = []
       @s.rb_protect_errinfo(77, proof) { |x| raise NameError }.class.should == NameError
       proof[0].should == 23
