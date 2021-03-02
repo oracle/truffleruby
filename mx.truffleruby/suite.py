2cd8a7ef5ca14a2ff7cd39e76a2dd02018f5d3ab
Jirka Marsik <jiri.marsik@oracle.com>
2021-03-02 12:54:19 +0100

    Bump graal import and introduce ruby spec for groups in line comments

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "43a4c1b16e2baaa16ed0598fa6fb59408ee71ef2",
+                "version": "bf2c742da013f65f715dad08f58cb6a8eec1eee4",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "43a4c1b16e2baaa16ed0598fa6fb59408ee71ef2",
+                "version": "bf2c742da013f65f715dad08f58cb6a8eec1eee4",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
