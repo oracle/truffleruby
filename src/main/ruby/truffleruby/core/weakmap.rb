b14eb610fb424f2e8dfc640122c439a07dc0e658
Nicolas Laurent <nicolas.laurent@oracle.com>
2021-03-02 17:38:57 +0100

    allow immediate and frozen key/values in WeakMap

--- src/main/ruby/truffleruby/core/weakmap.rb
+++ src/main/ruby/truffleruby/core/weakmap.rb
@@ -10,28 +10,15 @@
 
 module ObjectSpace
   # WeakMap uses identity comparison semantics. The implementation assumes that the Java representation of objects
-  # do compare (equals() and hashCode()) using object identity. This is the case for instances of RubyDynamicObject.
+  # do compare (equals() and hashCode()) using object identity. This is the case for instances of RubyDynamicObject,
+  # boxed primitives, and RubySymbol.
   #
   # However, results are unspecified if used with instances of TruffleObject that override equals() and
   # hashCode().
-  #
-  # Note that, following MRI, we disallow immediate (primitive) values and frozen objects as keys or value of
-  # WeakMap. We could easily transcend this limitation as we do not modify objects like MRI does (it sets a finalizer).
-  # However, keeping the limitation enables the above assumption of identity comparison.
   class WeakMap
     include Enumerable
 
-    private def check_key_or_value(kv, type)
-      if Primitive.immediate_value?(kv)
-        raise ArgumentError, "WeakMap #{type} can't be an instance of #{kv.class}"
-      elsif Truffle::KernelOperations.value_frozen?(kv)
-        raise FrozenError, "WeakMap #{type} can't be a frozen object"
-      end
-    end
-
     def []=(key, value)
-      check_key_or_value(key, 'key')
-      check_key_or_value(value, 'value')
       Primitive.weakmap_aset(self, key, value)
     end
 
--- test/mri/excludes/TestWeakMap.rb
+++
@@ -1,2 +0,0 @@
-exclude :test_frozen_object, "needs investigation"
-exclude :test_aset_const, "needs investigation"
