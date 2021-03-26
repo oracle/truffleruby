03eb8d8454522ac15e82d797426c041c359fc8bc
Benoit Daloze <benoit.daloze@oracle.com>
2021-03-26 17:24:43 +0100

    [GR-30240] Default to --disable-static when installing nokogiri
    * nokogiri 1.11.2 defaults to --disable-static, so this is noop on recent nokogiri.
    * Fixes a compilation issue on older macOS for older nokogiri releases.
    * About 2x faster to build than without --disable-static for older nokogiri releases on macOS.
    
    (cherry picked from commit cf1e33e31886a7aa0ad93c85da1c5e2267a24ff7)

---
+++ lib/patches/rubygems/ext/builder.rb
@@ -0,0 +1,19 @@
+require 'rubygems/ext/builder'
+
+# nokogiri 1.11.2 defaults to --disable-static, so this is noop on recent nokogiri.
+# This fixes a compilation issue on older macOS for older nokogiri releases (GR-30240).
+# About 2x faster to build than without --disable-static for older nokogiri releases on macOS.
+# Also gives more flexibility to control what is run natively and on Sulong.
+module Truffle::NokogiriDefaultBuildsArgs
+  def initialize(*args)
+    super
+
+    if @spec.name == 'nokogiri' and @build_args.empty?
+      @build_args = ['--disable-static']
+    end
+  end
+end
+
+class Gem::Ext::Builder
+  prepend Truffle::NokogiriDefaultBuildsArgs
+end
