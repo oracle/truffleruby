9a2037123bdd9daba5f143c8e7f389d514cc7ede
ol-automation_ww <ol-automation_ww@oracle.com>
2021-04-10 12:16:38 +0000

    [GR-30713] Update to latest graal
    * Fixes https://github.com/oracle/truffleruby/issues/2319

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "cbd151589bf6047ca39761891892dfe883410bcc",
+                "version": "d866b4ab387bd461e8681a462956839def26c752",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "cbd151589bf6047ca39761891892dfe883410bcc",
+                "version": "d866b4ab387bd461e8681a462956839def26c752",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
