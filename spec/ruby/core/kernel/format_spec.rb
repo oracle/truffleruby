cbf989126d3f3d1960c2d6f530e7c0e96cb367f1
Benoit Daloze <benoit.daloze@oracle.com>
2021-04-14 15:40:05 +0200

    Improve sprintf specs and move them to shared specs

--- spec/ruby/core/kernel/format_spec.rb
+++ spec/ruby/core/kernel/format_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 
+# NOTE: most specs are in sprintf_spec.rb, this is just an alias
 describe "Kernel#format" do
   it "is a private method" do
     Kernel.should have_private_instance_method(:format)
@@ -10,21 +11,5 @@ end
 describe "Kernel.format" do
   it "is accessible as a module function" do
     Kernel.format("%s", "hello").should == "hello"
-  end
-
-  it "formats string with precision" do
-    Kernel.format("%.3s", "hello").should == "hel"
-    Kernel.format("%-3.3s", "hello").should == "hel"
-  end
-
-  describe "on multibyte strings" do
-    it "formats string with precision" do
-      Kernel.format("%.3s", "hello".encode('UTF-16LE')).should == "hel".encode('UTF-16LE')
-    end
-
-    it "preserves encoding" do
-      str = format('%s', 'foobar'.encode('UTF-16LE'))
-      str.encoding.to_s.should == "UTF-16LE"
-    end
   end
 end
