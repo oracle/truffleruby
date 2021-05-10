a59c9d2eeb930db323d55bc119e1fd1c0a18bf9c
Benoit Daloze <benoit.daloze@oracle.com>
2021-05-10 13:35:43 +0200

    [GR-30679] Update graal to get the traversing compilation queue

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "fff1a8a5c7a9898b19e7e7014bfaa03cc2a698ab",
+                "version": "c7c061b3230852e9582badf788b3dab74a809ca9",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "fff1a8a5c7a9898b19e7e7014bfaa03cc2a698ab",
+                "version": "c7c061b3230852e9582badf788b3dab74a809ca9",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
