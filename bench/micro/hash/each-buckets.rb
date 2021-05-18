82e07eec75a392b43b61ee83bdd0de44d244f049
Nicolas Laurent <nicolas.laurent@oracle.com>
2021-05-18 16:55:43 +0200

    add micro benchmarks for each & map on packed/bucket hashes

---
+++ bench/micro/hash/each-buckets.rb
@@ -0,0 +1,16 @@
+# truffleruby_primitives: true
+
+# Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 2.0, or
+# GNU General Public License version 2, or
+# GNU Lesser General Public License version 2.1.
+
+# Benchmarks Hash#each for bucket hashes.
+
+hash = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10}
+benchmark 'core-hash-each-buckets' do
+  hash.each { |k, v| Primitive.blackhole(v) }
+end
