7bda81cef7f1bc44dab10ef5999a1ae4750bd735
Chris Seaton <chris.seaton@shopify.com>
2021-05-28 19:09:03 +0100

    Specialise digit length for nanoseconds

--- src/main/java/org/truffleruby/core/time/RubyDateFormatter.java
+++ src/main/java/org/truffleruby/core/time/RubyDateFormatter.java
@@ -666,8 +666,35 @@ public abstract class RubyDateFormatter {
                         break;
 
                     case FORMAT_NANOSEC: {
-                        final LazyIntRope nanoRope = new LazyIntRope(dt.getNano());
-                        final int padding = 6 - nanoRope.characterLength();
+                        final int nano = dt.getNano();
+                        assert nano >= 0;
+                        assert nano < 1000000000;
+
+                        final int digits;
+
+                        if (nano < 10) {
+                            digits = 1;
+                        } else if (nano < 100) {
+                            digits = 2;
+                        } else if (nano < 1000) {
+                            digits = 3;
+                        } else if (nano < 10000) {
+                            digits = 4;
+                        } else if (nano < 100000) {
+                            digits = 5;
+                        } else if (nano < 1000000) {
+                            digits = 6;
+                        } else if (nano < 10000000) {
+                            digits = 7;
+                        } else if (nano < 100000000) {
+                            digits = 8;
+                        } else {
+                            digits = 9;
+                        }
+
+                        final LazyIntRope nanoRope = new LazyIntRope(nano, UTF8Encoding.INSTANCE, digits);
+
+                        final int padding = 6 - digits;
 
                         if (padding == 0) {
                             appendRope = nanoRope;
