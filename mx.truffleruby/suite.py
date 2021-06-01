4739950f705c2ebfaba2cecc396304932949bf5b
Benoit Daloze <benoit.daloze@oracle.com>
2021-06-01 13:25:26 +0200

    [GR-31457] Update to latest Sulong to get the performance warning fix for globals

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "9ae4f87534cec3a6a858724d53fd26856c829cee",
+                "version": "f99631396f42d8f1b8850cf5efc34f136cdef8ff",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "9ae4f87534cec3a6a858724d53fd26856c829cee",
+                "version": "f99631396f42d8f1b8850cf5efc34f136cdef8ff",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
