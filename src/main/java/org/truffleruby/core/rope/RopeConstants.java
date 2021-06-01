3ced42d56dff3f62be01875e267294bb74fccd15
Benoit Daloze <benoit.daloze@oracle.com>
2021-06-01 15:05:43 +0200

    Mark RopeConstants arrays as CompilationFinal

--- src/main/java/org/truffleruby/core/rope/RopeConstants.java
+++ src/main/java/org/truffleruby/core/rope/RopeConstants.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import org.jcodings.specific.ASCIIEncoding;
 import org.jcodings.specific.USASCIIEncoding;
 import org.jcodings.specific.UTF8Encoding;
@@ -30,9 +31,9 @@ public class RopeConstants {
     public static final LeafRope EMPTY_UTF8_ROPE = withHashCode(
             new AsciiOnlyLeafRope(EMPTY_BYTES, UTF8Encoding.INSTANCE));
 
-    public static final LeafRope[] UTF8_SINGLE_BYTE_ROPES = new LeafRope[256];
-    public static final LeafRope[] US_ASCII_SINGLE_BYTE_ROPES = new LeafRope[256];
-    public static final LeafRope[] ASCII_8BIT_SINGLE_BYTE_ROPES = new LeafRope[256];
+    @CompilationFinal(dimensions = 1) public static final LeafRope[] UTF8_SINGLE_BYTE_ROPES = new LeafRope[256];
+    @CompilationFinal(dimensions = 1) public static final LeafRope[] US_ASCII_SINGLE_BYTE_ROPES = new LeafRope[256];
+    @CompilationFinal(dimensions = 1) public static final LeafRope[] ASCII_8BIT_SINGLE_BYTE_ROPES = new LeafRope[256];
 
     static {
         for (int i = 0; i < 128; i++) {
@@ -128,7 +129,7 @@ public class RopeConstants {
         }
     }
 
-    private static final LeafRope[] PADDED_NUMBERS = createPaddedNumbersTable();
+    @CompilationFinal(dimensions = 1) private static final LeafRope[] PADDED_NUMBERS = createPaddedNumbersTable();
 
     private static LeafRope[] createPaddedNumbersTable() {
         final LeafRope[] table = new LeafRope[100];
@@ -147,7 +148,7 @@ public class RopeConstants {
         return PADDED_NUMBERS[n];
     }
 
-    private static final LeafRope[] PADDING_ZEROS = createPaddingZeroTable();
+    @CompilationFinal(dimensions = 1) private static final LeafRope[] PADDING_ZEROS = createPaddingZeroTable();
 
     private static LeafRope[] createPaddingZeroTable() {
         final LeafRope[] table = new LeafRope[6];
