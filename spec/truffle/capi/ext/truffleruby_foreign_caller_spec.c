9c0f7ce2a2b2d83d04919bca240f05de4b0624f5
Benoit Daloze <benoit.daloze@oracle.com>
2021-06-10 15:50:42 +0200

    [GR-26849] Raise a RuntimeError when calling a method needing caller data directly from a foreign language
    * Instead of an internal error later on.

---
+++ spec/truffle/capi/ext/truffleruby_foreign_caller_spec.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 2.0, or
+ * GNU General Public License version 2, or
+ * GNU Lesser General Public License version 2.1.
+ */
+#include "ruby.h"
+#include "ruby/thread.h"
+#include "rubyspec.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static VALUE call_binding(VALUE self) {
+  return rb_tr_wrap(polyglot_invoke(rb_tr_unwrap(self), "binding"));
+}
+
+static VALUE call_binding_rb_funcall(VALUE self) {
+  return rb_funcall(self, rb_intern("binding"), 0);
+}
+
+void Init_truffleruby_foreign_caller_spec(void) {
+  VALUE cls = rb_define_class("CApiTruffleRubyForeignCallerSpecs", rb_cObject);
+  rb_define_method(cls, "call_binding", call_binding, 0);
+  rb_define_method(cls, "call_binding_rb_funcall", call_binding_rb_funcall, 0);
+}
+
+#ifdef __cplusplus
+}
+#endif
