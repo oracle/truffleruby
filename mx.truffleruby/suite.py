ee84f8241cda252db1bdf997d785e88a9702e87f
Benoit Daloze <benoit.daloze@oracle.com>
2021-06-10 11:39:21 +0200

    [GR-31320] Update Truffle to get the fix for CompilationStatistics invalidation reasons

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "ee260eb1e4ef12d1cba81de13bab566e7e09b4cc",
+                "version": "2f811ae4f61c971bd5259b3e336b4cb1f493ca3f",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "ee260eb1e4ef12d1cba81de13bab566e7e09b4cc",
+                "version": "2f811ae4f61c971bd5259b3e336b4cb1f493ca3f",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
