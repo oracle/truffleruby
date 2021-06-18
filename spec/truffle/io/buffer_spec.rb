e34127e66dae577435fd37985e1e219263cc093d
Duncan MacGregor <duncan.macgregor@oracle.com>
2021-06-18 10:54:23 +0100

    Add tests for allocating zero byte buffers.

--- spec/truffle/io/buffer_spec.rb
+++ spec/truffle/io/buffer_spec.rb
@@ -12,7 +12,7 @@
 
 require_relative '../../ruby/spec_helper'
 
-describe "IO bufffers" do
+describe "IO buffers" do
 
   before do
     @mem_check = Proc.new do |*ptrs|
@@ -74,7 +74,7 @@ describe "IO bufffers" do
     end
   end
 
-  it "allocate multiple buffers and returns non-null pointers" do
+  it "allocate multiple buffers on a new thread and returns non-null pointers" do
     Thread.new do
       ptr1 = Primitive.io_thread_buffer_allocate(1)
       ptr2 = Primitive.io_thread_buffer_allocate(1)
@@ -89,7 +89,7 @@ describe "IO bufffers" do
     end.join
   end
 
-  it "allocate multiple buffers and returns non-null pointers for large buffer sizes" do
+  it "allocate multiple buffers on a new thread and returns non-null pointers for large buffer sizes" do
     Thread.new do
       ptr1 = Primitive.io_thread_buffer_allocate(4096 * 4)
       ptr2 = Primitive.io_thread_buffer_allocate(4096 * 4)
@@ -100,6 +100,28 @@ describe "IO bufffers" do
       ensure
         Primitive.io_thread_buffer_free(ptr2)
         Primitive.io_thread_buffer_free(ptr1)
+      end
+    end.join
+  end
+
+  it "return a non-null pointer when 0 bytes of buffer are requested" do
+    ptr = Primitive.io_thread_buffer_allocate(1)
+    begin
+      ptr.null?.should == false
+
+    ensure
+      Primitive.io_thread_buffer_free(ptr)
+    end
+  end
+
+  it "return a non-null pointer on a new thread when 0 bytes of buffer are requested" do
+    Thread.new do
+      ptr = Primitive.io_thread_buffer_allocate(1)
+      begin
+        ptr.null?.should == false
+
+      ensure
+        Primitive.io_thread_buffer_free(ptr)
       end
     end.join
   end
