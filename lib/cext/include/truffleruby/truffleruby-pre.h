d4a0721dc2eb96c4ad57d5ec5678facfff33387a
Benoit Daloze <benoit.daloze@oracle.com>
2021-06-22 10:37:08 +0200

    Prefer RUBY_SYMBOL_EXPORT_{BEGIN,END} for consistency with other ruby headers

--- lib/cext/include/truffleruby/truffleruby-pre.h
+++ lib/cext/include/truffleruby/truffleruby-pre.h
@@ -18,6 +18,8 @@
 extern "C" {
 #endif
 
+RUBY_SYMBOL_EXPORT_BEGIN
+
 #define TRUFFLERUBY
 
 #include <graalvm/llvm/polyglot.h>
@@ -53,7 +55,7 @@ POLYGLOT_DECLARE_TYPE(VALUE)
 extern void* rb_tr_cext;
 #define RUBY_CEXT rb_tr_cext
 
-void* rb_tr_abi_version(void) __attribute__((weak)) __attribute__((visibility("default")));
+void* rb_tr_abi_version(void) __attribute__((weak));
 void* rb_tr_abi_version(void) {
   char* abi_version = STRINGIZE(TRUFFLERUBY_ABI_VERSION);
   return polyglot_from_string(abi_version, "US-ASCII");
@@ -74,6 +76,8 @@ extern ID (*rb_tr_sym2id)(VALUE sym);
 #ifndef offsetof
 #define offsetof(p_type,field) ((size_t)&(((p_type *)0)->field))
 #endif
+
+RUBY_SYMBOL_EXPORT_END
 
 #if defined(__cplusplus)
 }
