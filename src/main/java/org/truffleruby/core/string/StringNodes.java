4d549f05a33894dd286c1a7177deac67cedec465
Lillian Zhang <lillian.zhang@shopify.com>
2021-07-07 10:16:00 -0400

    stylistic edits

--- src/main/java/org/truffleruby/core/string/StringNodes.java
+++ src/main/java/org/truffleruby/core/string/StringNodes.java
@@ -594,7 +594,7 @@ public abstract class StringNodes {
 
         @Specialization
         protected Object getIndex(Object string, long index, NotProvided length) {
-            assert (int) index != index; // verified via lowerFixnum
+            assert (int) index != index : "verified via lowerFixnum";
             return outOfBoundsNil();
         }
 
@@ -4418,7 +4418,7 @@ public abstract class StringNodes {
         }
 
         @Specialization(
-                guards = { "singleByteOptimizableNode.execute(stringRope)" })
+                guards = "singleByteOptimizableNode.execute(stringRope)")
         protected Object singleByteOptimizable(Rope stringRope, Rope patternRope, int offset,
                 @Cached RopeNodes.BytesNode stringBytesNode,
                 @Cached RopeNodes.BytesNode patternBytesNode,
@@ -4426,7 +4426,8 @@ public abstract class StringNodes {
                 @Cached("createCountingProfile()") ConditionProfile matchProfile) {
 
             assert offset >= 0;
-            assert offset + patternRope.byteLength() <= stringRope.byteLength(); // otherwise returns in ruby fastpath
+            assert offset + patternRope.byteLength() <= stringRope
+                    .byteLength() : "already checked in the caller, String#index";
 
             int p = offset;
             final int e = stringRope.byteLength();
@@ -4451,14 +4452,15 @@ public abstract class StringNodes {
 
         @TruffleBoundary
         @Specialization(
-                guards = { "!singleByteOptimizableNode.execute(stringRope)" })
+                guards = "!singleByteOptimizableNode.execute(stringRope)")
         protected Object multiByte(Rope stringRope, Rope patternRope, int offset,
                 @Cached RopeNodes.CalculateCharacterLengthNode calculateCharacterLengthNode,
                 @Cached RopeNodes.BytesNode stringBytesNode,
                 @Cached RopeNodes.BytesNode patternBytesNode) {
 
             assert offset >= 0;
-            assert offset + patternRope.byteLength() <= stringRope.byteLength(); // otherwise returns in ruby fastpath
+            assert offset + patternRope.byteLength() <= stringRope
+                    .byteLength() : "already checked in the caller, String#index";
 
             int p = 0;
             final int e = stringRope.byteLength();
@@ -4516,7 +4518,7 @@ public abstract class StringNodes {
             return ToRopeNodeGen.create(pattern);
         }
 
-        @Specialization(guards = { "!patternFits(stringRope, patternRope, offset)" })
+        @Specialization(guards = "!patternFits(stringRope, patternRope, offset)")
         protected Object patternTooLarge(Rope stringRope, Rope patternRope, int offset) {
             assert offset >= 0;
             return nil;
