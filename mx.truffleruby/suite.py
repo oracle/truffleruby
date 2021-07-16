4fff15fea2312245787832ac1260ef0dccae4b3d
Benoit Daloze <benoit.daloze@oracle.com>
2021-07-16 13:12:17 +0200

    Update to latest tregex to get fixes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "42c83e13dc5ef41c5ca63e76175864c3d94626e2",
+                "version": "d1b1b1f6355682ccbe7976617417397925fcf207",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "42c83e13dc5ef41c5ca63e76175864c3d94626e2",
+                "version": "d1b1b1f6355682ccbe7976617417397925fcf207",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
