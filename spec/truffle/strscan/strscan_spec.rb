a080edbea1f430829af445087c7bbbbbe83e7afe
Duncan MacGregor <duncan.macgregor@oracle.com>
2021-08-03 11:27:15 +0100

    Update Truffle `strscan` test to use `ruby_exe`.

--- spec/truffle/strscan/strscan_spec.rb
+++ spec/truffle/strscan/strscan_spec.rb
@@ -4,6 +4,6 @@ require 'strscan'
 
 describe 'StringScanner' do
   it 'has a `scan_internal` method of under 100 AST nodes' do
-    Truffle::Debug.ast_size(StringScanner.instance_method(:scan_internal)).should < 100
+    ruby_exe('require "strscan"; print Truffle::Debug.ast_size(StringScanner.instance_method(:scan_internal))').to_i.should < 100
   end
 end
