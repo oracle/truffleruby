fa9f7a744dc28803c3c2c87220d4e8f9acbeb063
Brandon Fish <brandon.j.fish@oracle.com>
2021-08-09 12:50:21 -0500

    Update to Graal import to fix GC regression with auxiliary images

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "ba02a33c5109792e5fd05cc44649cea4218e5233",
+                "version": "c7604b7fd48cc8c3509cf215cd5e268748a81d85",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "ba02a33c5109792e5fd05cc44649cea4218e5233",
+                "version": "c7604b7fd48cc8c3509cf215cd5e268748a81d85",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
