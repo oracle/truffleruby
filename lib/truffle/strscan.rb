15bb22a8f64775f06f9306c0dabb8ac2ca578038
Duncan MacGregor <duncan.macgregor@oracle.com>
2021-08-20 13:40:32 +0100

    Accept string pattern for `StringScanner` headonly searches.

--- lib/truffle/strscan.rb
+++ lib/truffle/strscan.rb
@@ -309,7 +309,11 @@ class StringScanner
   end
 
   private def scan_check_args(pattern, headonly)
-    unless pattern.kind_of? Regexp
+    case pattern
+    when String
+      raise TypeError, "bad pattern argument: #{pattern.inspect}" unless headonly
+    when Regexp
+    else
       raise TypeError, "bad pattern argument: #{pattern.inspect}"
     end
     raise ArgumentError, 'uninitialized StringScanner object' unless @string
@@ -321,7 +325,11 @@ class StringScanner
   private def scan_internal(pattern, advance_pos, getstr, headonly)
     scan_check_args(pattern, headonly)
 
-    md = Truffle::RegexpOperations.match_in_region pattern, @string, pos, @string.bytesize, headonly, pos
+    if String === pattern
+      md = scan_internal_string_pattern(pattern, headonly)
+    else
+      md = Truffle::RegexpOperations.match_in_region pattern, @string, pos, @string.bytesize, headonly, pos
+    end
     if md
       Primitive.matchdata_fixup_positions(md, pos)
       @match = md
@@ -329,6 +337,10 @@ class StringScanner
     else
       @match = nil
     end
+  end
+
+  private def scan_internal_string_pattern(pattern, headonly)
+    @string[pos..].start_with?(pattern) ? Primitive.matchdata_create_single_group(pattern, @string.dup, pos, pos + pattern.bytesize) : nil
   end
 
   private def scan_internal_set_pos_and_str(advance_pos, getstr, md)
