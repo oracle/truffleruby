d39b0992c59323778c3330725ad2970e483bf7d1
Duncan MacGregor <duncan.macgregor@oracle.com>
2021-08-23 14:14:15 +0100

    Refactor string splitting to avoid creating new Regexps.

--- src/main/ruby/truffleruby/core/splitter.rb
+++ src/main/ruby/truffleruby/core/splitter.rb
@@ -77,13 +77,10 @@ module Truffle
           valid_encoding?(string)
           valid_encoding?(pattern)
 
-          if limit == 0 && pattern.empty?      # split('', 0)
+          if pattern.empty?
             split_type_chars(string, limit, block)
-          elsif limit <= 0 && !pattern.empty?  # split(':', -1)
+          else
             split_type_string(string, pattern, limit, block)
-          else                                 # transfer to regexp
-            pattern = Regexp.new(Regexp.quote(pattern))
-            split_type_regexp(string, pattern, limit, block)
           end
         end
 
@@ -116,6 +113,8 @@ module Truffle
       def split_type_string(string, pattern, limit, block)
         pos = 0
         empty_count = 0
+        limited = limit > 0
+        count = 0;
 
         ret = []
 
@@ -123,6 +122,7 @@ module Truffle
         str_size = string.bytesize
 
         while pos < str_size
+          break if limited && limit - count <= 1
           nxt = Primitive.find_string(string, pattern, pos)
           break unless nxt
 
@@ -130,6 +130,7 @@ module Truffle
           empty_count = add_substring(string, ret, string.byteslice(pos, match_size), empty_count, block)
 
           pos = nxt + pat_size
+          count += 1
         end
 
         # No more separators, but we need to grab the last part still.
