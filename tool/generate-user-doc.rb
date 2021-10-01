3d3b7910fa90af9d172b334237552475762c696f
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-01 19:01:51 +0200

    Fix link to logo in user README

--- tool/generate-user-doc.rb
+++ tool/generate-user-doc.rb
@@ -5,6 +5,10 @@ contents = File.read("#{root}/README.md")
 # Update links to user docs which are sibling files on the website
 contents = contents.gsub(%r{\]\(doc/user/([^)]+)\)}, '](\1)')
 
+# Fix link to logo
+contents = contents.gsub(%r{\]\(logo/([^)]+)\)},
+  '](https://raw.githubusercontent.com/oracle/truffleruby/master/logo/\1)')
+
 # Remove the Documentation section for the website (redundant with the sidebar menu)
 contents = contents.sub(/^## Documentation\n.+?\n##/m, '##')
 
