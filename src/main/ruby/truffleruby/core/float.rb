870acb83cc874b90ed73744c9f73d89206bed7d8
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-05 20:22:23 +0200

    Fix remaining RuboCop issues

--- src/main/ruby/truffleruby/core/float.rb
+++ src/main/ruby/truffleruby/core/float.rb
@@ -62,7 +62,8 @@
 
 class Float < Numeric
 
-  NAN        = -(0.0 / 0.0) # to match MRI binary representation
+  # to match MRI binary representation
+  NAN        = -(0.0 / 0.0) # rubocop:disable Lint/BinaryOperatorWithIdenticalOperands
   INFINITY   = 1.0 / 0.0
   EPSILON    = 2.2204460492503131e-16
   RADIX      = 2
