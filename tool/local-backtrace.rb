3b42288ad4733a421a6e5eaf588ff58c27a1ead6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-11 16:39:25 +0800

    Delete tool directory

--- tool/local-backtrace.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-root = File.dirname(__dir__)
-contents = STDIN.read
-contents = contents.gsub("/b/b/e/main", ".")
-contents = contents.gsub("/mxbuild/truffleruby-jvm/jre/languages/ruby/", "/")
-STDOUT.puts contents
