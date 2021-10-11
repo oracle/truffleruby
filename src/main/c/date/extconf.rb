b6eed13b7336eaea6c27a8d8d8bc2a53bdea0570
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-11 16:36:38 +0800

    Delete src directory

--- src/main/c/date/extconf.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'mkmf'
-
-config_string("strict_warnflags") {|w| $warnflags += " #{w}"}
-
-have_var("timezone", "time.h")
-have_var("altzone", "time.h")
-
-create_makefile('date_core')
