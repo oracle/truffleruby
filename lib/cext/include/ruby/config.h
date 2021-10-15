adea2f64015a6e6d92882881168bbb6ac05c29ba
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-15 15:11:23 +0200

    Use separate config.h per OS and arch

--- lib/cext/include/ruby/config.h
+++ lib/cext/include/ruby/config.h
@@ -1,6 +1,20 @@
 // See updating-ruby.md for instructions on regenerating config headers.
 #ifdef __APPLE__
-#include <truffleruby/config_darwin.h>
+
+#if defined(__x86_64__)
+#include <truffleruby/config_darwin_amd64.h>
 #else
-#include <truffleruby/config_linux.h>
+#error Unsupported platform
+#endif
+
+#else
+
+#if defined(__x86_64__)
+#include <truffleruby/config_linux_amd64.h>
+#elif defined(__aarch64__)
+#include <truffleruby/config_linux_aarch64.h>
+#else
+#error Unsupported platform
+#endif
+
 #endif
