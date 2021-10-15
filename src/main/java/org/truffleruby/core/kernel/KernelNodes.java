88c70bc510f0d4fbbf3d306b734f94287f8f34ac
Andrew Konchin <andry.konchin@gmail.com>
2021-10-15 20:55:59 +0300

    Add deprecation warning when Kernel#lambda is called without block literal

--- src/main/java/org/truffleruby/core/kernel/KernelNodes.java
+++ src/main/java/org/truffleruby/core/kernel/KernelNodes.java
@@ -1305,7 +1305,20 @@ public abstract class KernelNodes {
             return ProcOperations.createLambdaFromBlock(getContext(), getLanguage(), block);
         }
 
-        @Specialization(guards = "!isLiteralBlock(block)")
+        @Specialization(guards = { "!isLiteralBlock(block)", "block.isProc()" })
+        protected RubyProc lambdaFromExistingProc(RubyProc block,
+                @Cached("new()") WarningNode warningNode) {
+            if (warningNode.shouldWarnForDeprecation()) {
+                warningNode.warningMessage(
+                        getContext().getCallStack().getTopMostUserSourceSection(),
+                        "lambda without a literal block is deprecated; use the proc without lambda instead");
+            }
+
+            // If the argument isn't a literal, its original behaviour (proc or lambda) is preserved.
+            return block;
+        }
+
+        @Specialization(guards = { "!isLiteralBlock(block)", "block.isLambda()" })
         protected RubyProc lambdaFromExistingProc(RubyProc block) {
             // If the argument isn't a literal, its original behaviour (proc or lambda) is preserved.
             return block;
