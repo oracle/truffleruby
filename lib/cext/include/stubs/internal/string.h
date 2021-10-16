9c5588a7d71ea8f6d86fd06bd85df28edee9da53
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-16 14:48:57 +0200

    Simplify STR_EMBED_P/STR_SHARED_P

--- lib/cext/include/stubs/internal/string.h
+++ lib/cext/include/stubs/internal/string.h
@@ -1,8 +1,8 @@
 #ifndef INTERNAL_STRING_H                                /*-*-C-*-vi:se ft=c:*/
 #define INTERNAL_STRING_H
 
-#define STR_EMBED_P(str)      (str, false)
-#define STR_SHARED_P(str)     (str, false)
+#define STR_EMBED_P(str) (false)
+#define STR_SHARED_P(str) (false)
 
 VALUE rb_fstring(VALUE);
 VALUE rb_fstring_cstr(const char *str);
