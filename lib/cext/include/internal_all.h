59e9c87b767e672c6f32cc9086db87731deabb1f
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-16 14:54:25 +0200

    test/mri/tests/cext-c/time/leap_second.c needs internal/time.h

--- lib/cext/include/internal_all.h
+++ lib/cext/include/internal_all.h
@@ -28,6 +28,7 @@
 #include <internal/sanitizers.h>
 #include <internal/string.h>
 #include <internal/symbol.h>
+#include <internal/time.h>
 #include <internal/thread.h>
 #include <internal/variable.h>
 #include <internal/vm.h>
