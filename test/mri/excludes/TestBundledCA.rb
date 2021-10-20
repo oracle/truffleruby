7fec96b6869af430ad3f42482d2409bbb4112e6c
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-20 19:49:22 +0200

    Exclude tests which need access to rubygems.org

--- test/mri/excludes/TestBundledCA.rb
+++ test/mri/excludes/TestBundledCA.rb
@@ -1,3 +1,5 @@
 exclude :test_accessing_fastly, "accesses a public web site at runtime"
 exclude :test_accessing_new_index, "accesses a public web site at runtime"
 exclude :test_accessing_rubygems, "accesses a public web site at runtime"
+exclude :test_accessing_staging, "needs investigation"
+exclude :test_accessing_www_rubygems, "needs investigation"
