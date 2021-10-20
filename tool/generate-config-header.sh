e813f9d8cde745201308ddeb3debee997484c596
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-20 17:43:23 +0200

    Use the toolchain for running ./configure on Linux but not on macOS
    * For some reason it does not work on macOS currently, so there we use
      the system compiler to run ./configure.

--- tool/generate-config-header.sh
+++ tool/generate-config-header.sh
@@ -8,6 +8,23 @@ if [ -z "$url" ]; then
     url=$(mx urlrewrite https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/ruby-3.0.2.tar.gz)
 fi
 
+os=$(uname -s)
+os=${os/Linux/linux}
+os=${os/Darwin/darwin}
+
+arch=$(uname -m)
+arch=${arch/x86_64/amd64}
+
+mx_platform="${os}_${arch}"
+
+if [ "$os" != "darwin" ]; then
+    if [ -z "$TOOLCHAIN_PATH" ]; then
+        echo "TOOLCHAIN_PATH must be set"
+        exit 1
+    fi
+    export PATH="$TOOLCHAIN_PATH:$PATH"
+fi
+
 archive=$(basename "$url")
 
 if [ ! -e "$archive" ]; then
@@ -17,14 +34,5 @@ fi
 
 cd ruby-3.0.2 || exit 1
 ./configure || (cat config.log; exit 1)
-
-os=$(uname -s)
-os=${os/Linux/linux}
-os=${os/Darwin/darwin}
-
-arch=$(uname -m)
-arch=${arch/x86_64/amd64}
-
-mx_platform="${os}_${arch}"
 
 cp .ext/include/*/ruby/config.h "../lib/cext/include/truffleruby/config_${mx_platform}.h"
