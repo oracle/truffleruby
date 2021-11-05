b3db32d327f35f5e783798c205254ef8c371a623
ol-automation_ww <ol-automation_ww@oracle.com>
2021-11-05 21:06:39 +0000

    Update graal import just before [GR-34785] Type system code generation improvements.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "6178bf1efa51876102fa3873f24ed965344a7208",
+                "version": "90eb322c605aa7571827d066d9ffba14dc185757",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "6178bf1efa51876102fa3873f24ed965344a7208",
+                "version": "90eb322c605aa7571827d066d9ffba14dc185757",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
