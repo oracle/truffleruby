68d333e3480a84d7eff6c2955c7f71ef657a4c4a
Benoit Daloze <benoit.daloze@oracle.com>
2021-11-29 16:19:22 +0100

    [GR-35332] [GR-35303] [GR-35172] Update to latest graal
    * Fixes regression for compilation of frame types.
    * Fixes regression in NFI.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "995f87f2e4716ce9f48ea67f15cd9181d0671bd1",
+                "version": "9340ad3351db79723da3af1e941f023fefb591af",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "995f87f2e4716ce9f48ea67f15cd9181d0671bd1",
+                "version": "9340ad3351db79723da3af1e941f023fefb591af",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
