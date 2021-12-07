8e112f6227c239d54a5bbba66fcac12976b54ed7
Benoit Daloze <benoit.daloze@oracle.com>
2021-12-07 15:54:35 +0100

    [GR-35354] Update to latest graal
    * Fixes building Sulong on CMake 3.22+

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "4a13bc3e2a3c4b6a2c2b94db22e45a00a8497fc4",
+                "version": "cf1887f14079502f59a787b13b194e123986f62a",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "4a13bc3e2a3c4b6a2c2b94db22e45a00a8497fc4",
+                "version": "cf1887f14079502f59a787b13b194e123986f62a",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
