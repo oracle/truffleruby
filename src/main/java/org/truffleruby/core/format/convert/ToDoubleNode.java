c094ad880ca18850c0041266d9b65d3f968f2f15
Duncan MacGregor <duncan.macgregor@oracle.com>
2021-12-10 15:21:21 +0000

    Put `BigINteger#doubleValue` behind a boundary.

--- src/main/java/org/truffleruby/core/format/convert/ToDoubleNode.java
+++ src/main/java/org/truffleruby/core/format/convert/ToDoubleNode.java
@@ -14,6 +14,7 @@ import org.truffleruby.core.numeric.RubyBignum;
 import org.truffleruby.language.RubyDynamicObject;
 import org.truffleruby.language.dispatch.DispatchNode;
 
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -44,6 +45,7 @@ public abstract class ToDoubleNode extends FormatNode {
     }
 
     @Specialization
+    @TruffleBoundary
     protected double toDouble(RubyBignum bignum) {
         return bignum.value.doubleValue();
     }
