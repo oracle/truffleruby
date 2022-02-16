d5e4a9d60e93c01e76b915c6c6aa84055feab7a6
Chris Seaton <chris.seaton@shopify.com>
2022-02-16 22:53:36 +0000

    Use WeakValueCache for ArgumentsDescriptorManager

--- src/main/java/org/truffleruby/language/arguments/ArgumentsDescriptorManager.java
+++ src/main/java/org/truffleruby/language/arguments/ArgumentsDescriptorManager.java
@@ -9,15 +9,14 @@
  */
 package org.truffleruby.language.arguments;
 
-import java.lang.ref.WeakReference;
-import java.util.Collections;
-import java.util.Map;
-import java.util.WeakHashMap;
+import org.truffleruby.collections.WeakValueCache;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ArgumentsDescriptorManager {
 
-    private final Map<NonEmptyArgumentsDescriptor, WeakReference<NonEmptyArgumentsDescriptor>> CANONICAL_NON_EMPTY_DESCRIPTORS = Collections
-            .synchronizedMap(new WeakHashMap<>());
+    private final WeakValueCache<Key, NonEmptyArgumentsDescriptor> CANONICAL_NON_EMPTY_DESCRIPTORS = new WeakValueCache<>();
 
     public ArgumentsDescriptor getArgumentsDescriptor(String[] keywordArgumentsNames, int keywordHashIndex,
             boolean keywordHashElidable) {
@@ -27,17 +26,49 @@ public class ArgumentsDescriptorManager {
             return EmptyArgumentsDescriptor.INSTANCE;
         }
 
-        final NonEmptyArgumentsDescriptor reference = new NonEmptyArgumentsDescriptor(keywordArgumentsNames,
+        final Key key = new Key(keywordArgumentsNames, keywordHashIndex, keywordHashElidable);
+
+        final NonEmptyArgumentsDescriptor descriptor = new NonEmptyArgumentsDescriptor(keywordArgumentsNames,
                 keywordHashIndex, keywordHashElidable);
 
-        final WeakReference<NonEmptyArgumentsDescriptor> canonical = CANONICAL_NON_EMPTY_DESCRIPTORS
-                .putIfAbsent(reference, new WeakReference<>(reference));
+        return CANONICAL_NON_EMPTY_DESCRIPTORS.addInCacheIfAbsent(key, descriptor);
+    }
 
-        if (canonical == null) {
-            return reference;
-        } else {
-            return canonical.get();
+    public static class Key {
+
+        private final String[] keywordArgumentNames;
+        private final int keywordHashIndex;
+        private final boolean keywordHashElidable;
+
+        public Key(
+                String[] keywordArgumentNames,
+                int keywordHashIndex,
+                boolean keywordHashElidable) {
+            this.keywordArgumentNames = keywordArgumentNames;
+            this.keywordHashIndex = keywordHashIndex;
+            this.keywordHashElidable = keywordHashElidable;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Key that = (Key) o;
+            return keywordHashIndex == that.keywordHashIndex && keywordHashElidable == that.keywordHashElidable &&
+                    Arrays.equals(keywordArgumentNames, that.keywordArgumentNames);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(keywordHashIndex, keywordHashElidable);
+            result = 31 * result + Arrays.hashCode(keywordArgumentNames);
+            return result;
+        }
+
     }
 
 }
