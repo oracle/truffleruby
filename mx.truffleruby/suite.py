aa452219a05b0c09bfd4e9c1897691d9f207ca31
ol-automation_ww <ol-automation_ww@oracle.com>
2022-03-11 21:10:55 +0000

    [GR-35093] [GR-36522] [GR-37336] Update graal import
    * [GR-35093] Avoid polymorphism for assumptions and profiles.
    * [GR-36522] [GR-37336] Better --help output.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "85c258963b605f95061b37a912a633b1bf1a1c0f",
+                "version": "8a42ff96c5bee396affbeeab8251e7dd195b6554",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "85c258963b605f95061b37a912a633b1bf1a1c0f",
+                "version": "8a42ff96c5bee396affbeeab8251e7dd195b6554",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
