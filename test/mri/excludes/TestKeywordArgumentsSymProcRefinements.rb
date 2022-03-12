84377dbed7099c5a50521080bf0858903954a85d
Benoit Daloze <benoit.daloze@oracle.com>
2022-03-12 17:53:37 +0100

    Untag passing TestKeywordArguments tests

--- test/mri/excludes/TestKeywordArgumentsSymProcRefinements.rb
+++ test/mri/excludes/TestKeywordArgumentsSymProcRefinements.rb
@@ -1,3 +1,1 @@
-exclude :test_sym_proc_refine_kwsplat, "needs investigation"
-exclude :test_sym_proc_refine_method_missing_kwsplat, "needs investigation"
 exclude :test_sym_proc_refine_super_method_missing_kwsplat, "needs investigation"
