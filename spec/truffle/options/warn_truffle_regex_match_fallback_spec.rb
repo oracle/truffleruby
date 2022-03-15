af96e64cc300c3c3e0da9fbc50c2c617e854244b
Jirka Marsik <jiri.marsik@oracle.com>
2022-03-15 11:21:14 +0100

    Use regexps less likely to be supported by TRegex in fallback specs

--- spec/truffle/options/warn_truffle_regex_match_fallback_spec.rb
+++ spec/truffle/options/warn_truffle_regex_match_fallback_spec.rb
@@ -10,11 +10,11 @@ require_relative '../../ruby/spec_helper'
 
 describe "The --warn-truffle-regex-match-fallback option" do
   it "works" do
-    out = ruby_exe('2.times { /a*+/ =~ "a" }', options: "--experimental-options --warn-truffle-regex-match-fallback", args: "2>&1")
+    out = ruby_exe('2.times { /(?<loop>a\g<loop>|)/ =~ "aaa" }', options: "--experimental-options --warn-truffle-regex-match-fallback", args: "2>&1")
     out = out.gsub(/^.+: warning/, 'warning')
     out.should == <<~OUT
-      warning: match_in_region(/a*+/, "a"@UTF-8, 0, 1, false, 0) enc=US-ASCII cannot be run as a Truffle regexp and fell back to Joni
-      warning: match_in_region(/a*+/, "a"@UTF-8, 0, 1, false, 0) enc=US-ASCII cannot be run as a Truffle regexp and fell back to Joni
+      warning: match_in_region(/(?<loop>a\\g<loop>|)/, "aaa"@UTF-8, 0, 3, false, 0) enc=US-ASCII cannot be run as a Truffle regexp and fell back to Joni
+      warning: match_in_region(/(?<loop>a\\g<loop>|)/, "aaa"@UTF-8, 0, 3, false, 0) enc=US-ASCII cannot be run as a Truffle regexp and fell back to Joni
     OUT
   end
 end
