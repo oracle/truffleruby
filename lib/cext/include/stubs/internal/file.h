0649b0dd9f15fbbaffcd0e745e63b5356ebf37e4
Benoit Daloze <benoit.daloze@oracle.com>
2022-03-21 14:01:19 +0100

    Declare internal functions which MRI C API tests need

--- lib/cext/include/stubs/internal/file.h
+++ lib/cext/include/stubs/internal/file.h
@@ -1,1 +1,10 @@
+#ifndef INTERNAL_FILE_H                                  /*-*-C-*-vi:se ft=c:*/
+#define INTERNAL_FILE_H
+
 #include "ruby.h"
+
+#ifdef __APPLE__
+VALUE rb_str_normalize_ospath(const char *ptr, long len);
+#endif
+
+#endif /* INTERNAL_FILE_H */
--- lib/cext/include/stubs/internal/time.h
+++ lib/cext/include/stubs/internal/time.h
@@ -1,1 +1,8 @@
+#ifndef INTERNAL_TIME_H                                  /*-*-C-*-vi:se ft=c:*/
+#define INTERNAL_TIME_H
+
 #include "ruby.h"
+
+void ruby_reset_leap_second_info(void);
+
+#endif /* INTERNAL_TIME_H */
