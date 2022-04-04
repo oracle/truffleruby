3656075a326f36a0d5ed5af9f0ff9b2adc5efe87
Benoit Daloze <benoit.daloze@oracle.com>
2022-04-04 16:32:02 +0200

    [GR-37755] Implement Zlib.crc_table by using Java code and not using libz
    * In older zlib (< 1.2.7 it seems), get_crc_table() is stored as u64[],
      but in newer zlib, get_crc_table() is stored as u32[]. This causes
      invalid values when building with an old zlib and running with a new zlib.
    * Avoid these zlib binary compatibility issues entirely by defining the
      table in Java, and not needing to know about z_crc_t.

--- src/main/java/org/truffleruby/cext/CExtNodes.java
+++ src/main/java/org/truffleruby/cext/CExtNodes.java
@@ -1954,4 +1954,12 @@ public class CExtNodes {
                     threadManager.isRubyManagedThread(Thread.currentThread());
         }
     }
+
+    @CoreMethod(names = "zlib_get_crc_table", onSingleton = true)
+    public abstract static class ZLibGetCRCTable extends CoreMethodArrayArgumentsNode {
+        @Specialization
+        protected RubyArray zlibGetCRCTable() {
+            return createArray(ZLibCRCTable.TABLE.clone());
+        }
+    }
 }
