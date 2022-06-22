51031620e07fdb506058fdede70a9c338a4f22af
Benoit Daloze <benoit.daloze@oracle.com>
2022-06-22 11:48:36 +0200

    Replace Truffle::Ropes.debug_print_rope and remove unused TruffleRopesNodes

--- src/main/java/org/truffleruby/core/rope/TruffleRopesNodes.java
+++ src/main/java/org/truffleruby/core/rope/TruffleRopesNodes.java
@@ -11,19 +11,15 @@ package org.truffleruby.core.rope;
 
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.strings.TruffleString;
-import org.jcodings.specific.UTF8Encoding;
 import org.truffleruby.builtins.CoreMethod;
 import org.truffleruby.builtins.CoreMethodArrayArgumentsNode;
 import org.truffleruby.builtins.CoreModule;
-import org.truffleruby.cext.CExtNodes;
 import org.truffleruby.core.encoding.Encodings;
 import org.truffleruby.core.encoding.RubyEncoding;
 import org.truffleruby.core.encoding.TStringUtils;
 import org.truffleruby.core.string.RubyString;
 import org.truffleruby.core.string.StringNodes;
-import org.truffleruby.core.string.StringOperations;
 import org.truffleruby.core.string.StringUtils;
-import org.truffleruby.language.NotProvided;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
@@ -55,73 +51,6 @@ public abstract class TruffleRopesNodes {
 
     }
 
-    @CoreMethod(names = "debug_print_rope", onSingleton = true, required = 1, optional = 1)
-    public abstract static class DebugPrintRopeNode extends CoreMethodArrayArgumentsNode {
-
-        @Child private RopeNodes.DebugPrintRopeNode debugPrintRopeNode = RopeNodesFactory.DebugPrintRopeNodeGen
-                .create();
-
-        @TruffleBoundary
-        @Specialization(guards = "strings.isRubyString(string)")
-        protected Object debugPrintDefault(Object string, NotProvided printString,
-                @CachedLibrary(limit = "LIBSTRING_CACHE") RubyStringLibrary strings) {
-            return debugPrint(string, true, strings);
-        }
-
-        @TruffleBoundary
-        @Specialization(guards = "strings.isRubyString(string)")
-        protected Object debugPrint(Object string, boolean printString,
-                @CachedLibrary(limit = "LIBSTRING_CACHE") RubyStringLibrary strings) {
-            System.err.println("Legend: ");
-            System.err.println("BN = Bytes Null? (byte[] not yet populated)");
-            System.err.println("BL = Byte Length");
-            System.err.println("CL = Character Length");
-            System.err.println("CR = Code Range");
-            System.err.println("O = Byte Offset (SubstringRope only)");
-            System.err.println("T = Times (RepeatingRope only)");
-            System.err.println("V = Value (LazyIntRope only)");
-            System.err.println("E = Encoding");
-            System.err.println("P = Native Pointer (NativeRope only)");
-            System.err.println("S = Native Size (NativeRope only)");
-
-            return debugPrintRopeNode.executeDebugPrint(strings.getRope(string), 0, printString);
-        }
-    }
-
-    /** The returned string (when evaluated) will create a string with the same Rope structure as the string which is
-     * passed as argument. */
-    @CoreMethod(names = "debug_get_structure_creation", onSingleton = true, required = 1)
-    public abstract static class DebugGetStructureCreationNode extends CoreMethodArrayArgumentsNode {
-
-        @Child private StringNodes.MakeStringNode makeStringNode = StringNodes.MakeStringNode.create();
-
-        @TruffleBoundary
-        @Specialization(guards = "strings.isRubyString(string)")
-        protected RubyString getStructure(Object string,
-                @CachedLibrary(limit = "LIBSTRING_CACHE") RubyStringLibrary strings) {
-            Rope rope = strings.getRope(string);
-            String result = getStructure(rope);
-            byte[] bytes = StringOperations.encodeBytes(result, UTF8Encoding.INSTANCE);
-            return makeStringNode.executeMake(
-                    bytes,
-                    strings.getEncoding(string),
-                    CodeRange.CR_7BIT);
-        }
-
-        protected static String getStructure(Rope rope) {
-            if (rope instanceof LeafRope) {
-                return getStructure((LeafRope) rope);
-            } else {
-                return "(unknown rope class: " + rope.getClass() + ")";
-            }
-        }
-
-        private static String getStructure(LeafRope rope) {
-            return RopeOperations.escape(rope);
-        }
-
-    }
-
     @CoreMethod(names = "flatten_rope", onSingleton = true, required = 1)
     public abstract static class FlattenRopeNode extends CoreMethodArrayArgumentsNode {
 
@@ -137,19 +66,6 @@ public abstract class TruffleRopesNodes {
             // Ensure the result has offset = 0 and length = byte[].length for image build time checks
             byte[] byteArray = TStringUtils.getBytesOrCopy(tstring, rubyEncoding);
             return createString(fromByteArrayNode, byteArray, rubyEncoding);
-        }
-
-    }
-
-    @CoreMethod(names = "convert_to_native", onSingleton = true, required = 1)
-    public abstract static class NativeRopeNode extends CoreMethodArrayArgumentsNode {
-
-        @Specialization(guards = "strings.isRubyString(string)")
-        protected Object nativeRope(Object string,
-                @Cached CExtNodes.StringToNativeNode toNativeNode,
-                @CachedLibrary(limit = "LIBSTRING_CACHE") RubyStringLibrary strings) {
-            toNativeNode.executeToNative(string);
-            return string;
         }
 
     }
