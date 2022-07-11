5ed6c0acf1c316c4844ef4fb8efe3648eae348c8
Benoit Daloze <benoit.daloze@oracle.com>
2022-07-11 17:40:18 +0200

    Improve start_with? specs

--- spec/ruby/core/string/start_with_spec.rb
+++ spec/ruby/core/string/start_with_spec.rb
@@ -5,4 +5,9 @@ require_relative '../../shared/string/start_with'
 
 describe "String#start_with?" do
   it_behaves_like :start_with, :to_s
+
+  # Here and not in the shared examples because this is invalid as a Symbol
+  it "does not check that we are not starting to match at the head of a character" do
+    "\xA9".should.start_with?("\xA9") # A9 is not a character head for UTF-8
+  end
 end
