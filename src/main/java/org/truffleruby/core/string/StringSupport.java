6a800ea3d9bed52f4d090acaf2d3d480911ce62d
Benoit Daloze <benoit.daloze@oracle.com>
2022-07-12 13:25:36 +0200

    Respect recoverIfBroken for characterLength()

--- src/main/java/org/truffleruby/core/string/StringSupport.java
+++ src/main/java/org/truffleruby/core/string/StringSupport.java
@@ -37,6 +37,7 @@ import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.strings.AbstractTruffleString;
 import com.oracle.truffle.api.strings.InternalByteArray;
 import com.oracle.truffle.api.strings.TruffleString;
+import com.oracle.truffle.api.strings.TruffleString.ErrorHandling;
 import org.graalvm.collections.Pair;
 import org.jcodings.Config;
 import org.jcodings.Encoding;
@@ -77,8 +78,13 @@ public final class StringSupport {
             var substring = TruffleString.FromByteArrayNode.getUncached().execute(bytes, byteOffset,
                     byteEnd - byteOffset, encoding.tencoding, false);
 
-            return TruffleString.ByteLengthOfCodePointNode.getUncached().execute(substring, 0, encoding.tencoding,
-                    TruffleString.ErrorHandling.RETURN_NEGATIVE);
+            if (recoverIfBroken) {
+                return TruffleString.ByteLengthOfCodePointNode.getUncached().execute(substring, 0, encoding.tencoding,
+                        ErrorHandling.BEST_EFFORT);
+            } else {
+                return TruffleString.ByteLengthOfCodePointNode.getUncached().execute(substring, 0, encoding.tencoding,
+                        ErrorHandling.RETURN_NEGATIVE);
+            }
         }
 
         return characterLength(encoding.jcoding, codeRange, bytes, byteOffset, byteEnd, recoverIfBroken);
