6b1fb2ac686f262bd51392291a7708bcd0fc7205
Benoit Daloze <benoit.daloze@oracle.com>
2022-07-21 14:06:45 +0200

    [GR-39354] [GR-38179] Update to latest graal for TruffleString and thin launcher fixes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "b3f4a16ca2942a1b9ba135f740a5a4795492251f",
+                "version": "5bb740508beb175c209804192c87d6f941a2b112",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "b3f4a16ca2942a1b9ba135f740a5a4795492251f",
+                "version": "5bb740508beb175c209804192c87d6f941a2b112",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
