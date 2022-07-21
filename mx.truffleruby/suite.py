bc44a5081d17ae68bd8ffa830af14452bfd132e9
Benoit Daloze <benoit.daloze@oracle.com>
2022-07-21 14:03:19 +0200

    Update graal to get host inlining changes

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "d24695e267dbff610ec262c5c990560af88bd804",
+                "version": "b3f4a16ca2942a1b9ba135f740a5a4795492251f",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "d24695e267dbff610ec262c5c990560af88bd804",
+                "version": "b3f4a16ca2942a1b9ba135f740a5a4795492251f",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
