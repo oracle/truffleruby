58fe5ec6c368f553c1e91de0dd8b8cfba92fd1c6
Kevin Menard <kevin.menard@shopify.com>
2022-08-17 14:50:59 -0400

    Undefine allocator for Polyglot::ForeignException
    * The same way as for Integer, Symbol, etc.
    * Polyglot::ForeignObject.new would otherwise return a RubyBasicObject
      which is not actually a foreign object, and that would lead to confusion.
    * #new is overridden by InstantiableTrait and that works fine for
      InteropLibrary#isInstantiable() foreign objects.

--- src/main/ruby/truffleruby/core/truffle/polyglot.rb
+++ src/main/ruby/truffleruby/core/truffle/polyglot.rb
@@ -546,6 +546,14 @@ module Polyglot
   class ForeignException < Exception # rubocop:disable Lint/InheritException
     include ObjectTrait
     include ExceptionTrait
+
+    class << self
+      undef_method :new
+    end
+
+    def self.__allocate__
+      raise TypeError, "allocator undefined for #{self}"
+    end
   end
   # endregion
 end
