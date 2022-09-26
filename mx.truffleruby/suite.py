07e3c91288ea2fd65d16870ee8655a0f004e1e1f
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2022-09-26 15:39:40 +0200

    Update graal import

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "2abdd963206587b09dd7e5849baabc9aaee6fd84",
+                "version": "46235c96a132a7bb776a396fc26e31f5b5852c8c",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "2abdd963206587b09dd7e5849baabc9aaee6fd84",
+                "version": "46235c96a132a7bb776a396fc26e31f5b5852c8c",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
