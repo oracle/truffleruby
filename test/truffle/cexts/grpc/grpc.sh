fc90527178d03e58b59caf92a44b55dccd271f4a
Benoit Daloze <benoit.daloze@oracle.com>
2022-10-07 21:32:38 +0200

    [GR-40810] Skip grpc test on linux-aarch64 as it fails

--- test/truffle/cexts/grpc/grpc.sh
+++ test/truffle/cexts/grpc/grpc.sh
@@ -2,6 +2,13 @@
 
 source test/truffle/common.sh.inc
 
+platform="$(uname -s)-$(uname -m)"
+
+if [[ "$platform" == "Linux-aarch64" ]]; then
+  echo '[GR-40810] Skipping test on linux-aarch64 as it fails'
+  exit 0
+fi
+
 gem_test_pack=$(jt gem-test-pack)
 
 ruby_home="$(jt ruby -e 'print RbConfig::CONFIG["prefix"]')"
