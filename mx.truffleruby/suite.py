4bec82c59dd11a31c17e9fe2950b68a3ba53c93c
Benoit Daloze <benoit.daloze@oracle.com>
2022-10-10 12:46:56 +0200

    [GR-41618] Use a fixed mx version in CI, from common.json

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "f1003e9fe528a97502ed38b1dccdd52d6391b936",
+                "version": "71b88590c92f2d77d1033d76687703e925f5dfff",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "f1003e9fe528a97502ed38b1dccdd52d6391b936",
+                "version": "71b88590c92f2d77d1033d76687703e925f5dfff",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
