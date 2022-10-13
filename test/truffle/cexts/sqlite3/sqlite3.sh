bfd722a68a922a704679e85e9b2fa56be482b1e6
Benoit Daloze <benoit.daloze@oracle.com>
2022-10-13 15:09:23 +0200

    Ensure pkg-config is available

--- test/truffle/cexts/sqlite3/sqlite3.sh
+++ test/truffle/cexts/sqlite3/sqlite3.sh
@@ -4,6 +4,10 @@ source test/truffle/common.sh.inc
 
 gem_test_pack=$(jt gem-test-pack)
 
+# The sqlite3 extconf.rb needs pkg-config
+which pkg-config
+pkg-config --version
+
 jt gem install --local "$gem_test_pack/gem-cache/mini_portile2-2.8.0.gem" -V -N --backtrace
 jt gem install --local "$gem_test_pack/gem-cache/sqlite3-1.5.3.gem" -V -N --backtrace
 
