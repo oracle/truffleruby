a05b31efbdf04d975f49ffb64f0436e17217ac2a
Andrew Konchin <andrii.konchyn@oracle.com>
2022-11-16 18:49:13 +0200

    Don't use explicit `return`

--- src/main/ruby/truffleruby/core/time.rb
+++ src/main/ruby/truffleruby/core/time.rb
@@ -490,12 +490,10 @@ class Time
     end
 
     def valid_utc_offset_string?(utc_offset)
-      return true if utc_offset == 'UTC'
-      return true if utc_offset.size == 1 && ('A'..'Z') === utc_offset && utc_offset != 'J'
-      return true if utc_offset =~ /\A[+-](\d{2})(?::(\d{2})(?::(\d{2}))?)?\z/ && $1.to_i < 24 && $2.to_i < 60 && $3.to_i < 60 # with ":" separators
-      return true if utc_offset =~ /\A[+-](\d{2})(?:(\d{2})(?:(\d{2}))?)?\z/ && $1.to_i < 24 && $2.to_i < 60 && $3.to_i < 60 # without ":" separators
-
-      false
+      utc_offset == 'UTC' \
+        || (utc_offset.size == 1 && ('A'..'Z') === utc_offset && utc_offset != 'J') \
+        || (utc_offset =~ /\A[+-](\d{2})(?::(\d{2})(?::(\d{2}))?)?\z/ && $1.to_i < 24 && $2.to_i < 60 && $3.to_i < 60) \
+        || (utc_offset =~ /\A[+-](\d{2})(?:(\d{2})(?:(\d{2}))?)?\z/ && $1.to_i < 24 && $2.to_i < 60 && $3.to_i < 60) # without ":" separators
     end
     private :valid_utc_offset_string?
 
