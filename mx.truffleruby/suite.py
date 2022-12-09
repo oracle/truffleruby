520866a6973fecae0f88953cf68dd13fb484dc93
ol-automation_ww <ol-automation_ww@oracle.com>
2022-12-09 21:10:42 +0000

    Update graal import.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "d25a1d9e1b68daf9aa1b2103d8a8e92ef8fda9b9",
+                "version": "4ce1c067556eb63ed8d0b25ac4cfe584b6245f97",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "d25a1d9e1b68daf9aa1b2103d8a8e92ef8fda9b9",
+                "version": "4ce1c067556eb63ed8d0b25ac4cfe584b6245f97",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
