e5c1352957ef02fa1f9932b933e0f747938799c1
Andrew Konchin <andrii.konchyn@oracle.com>
2022-12-28 17:54:34 +0200

    Fix Array#pack and accept Numeric arguments when expected Double

--- src/main/java/org/truffleruby/core/format/convert/ToDoubleNode.java
+++ src/main/java/org/truffleruby/core/format/convert/ToDoubleNode.java
@@ -9,16 +9,18 @@
  */
 package org.truffleruby.core.format.convert;
 
+import org.truffleruby.core.cast.ToFNode;
 import org.truffleruby.core.format.FormatNode;
 import org.truffleruby.core.numeric.RubyBignum;
 import org.truffleruby.language.RubyDynamicObject;
-import org.truffleruby.language.dispatch.DispatchNode;
+import org.truffleruby.language.control.RaiseException;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import org.truffleruby.language.objects.IsANode;
 
 @NodeChild("value")
 public abstract class ToDoubleNode extends FormatNode {
@@ -50,13 +52,22 @@ public abstract class ToDoubleNode extends FormatNode {
         return bignum.value.doubleValue();
     }
 
-    @Specialization(guards = "isRational(rational)")
-    protected double toDoubleFromRational(RubyDynamicObject rational,
-            @Cached DispatchNode toFloatNode) {
-        return (Double) (toFloatNode.call(rational, "to_f", EMPTY_ARGUMENTS));
+    @Specialization(guards = { "!isRubyNumber(object)", "isNumeric(object, isANode)" })
+    protected double toDouble(RubyDynamicObject object,
+            @Cached IsANode isANode,
+            @Cached ToFNode toFNode) {
+        return toFNode.executeToDouble(object);
     }
 
-    protected boolean isRational(RubyDynamicObject object) {
-        return coreLibrary().rationalClass == object.getLogicalClass();
+    @Specialization(guards = { "!isRubyNumber(object)", "!isNumeric(object, isANode)" })
+    protected double toDouble(Object object,
+            @Cached IsANode isANode) {
+        throw new RaiseException(
+                getContext(),
+                coreExceptions().typeErrorCantConvertInto(object, "Float", this));
+    }
+
+    protected boolean isNumeric(Object object, IsANode isANode) {
+        return isANode.executeIsA(object, coreLibrary().numericClass);
     }
 }
