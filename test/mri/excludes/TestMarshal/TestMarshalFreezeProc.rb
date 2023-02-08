7b6f50a400918aba17abe7304d3b7900690933b8
Andrew Konchin <andrii.konchyn@oracle.com>
2023-02-08 17:47:55 +0200

    Updated excluded tests list for test_marshal.rb

---
+++ test/mri/excludes/TestMarshal/TestMarshalFreezeProc.rb
@@ -0,0 +1,7 @@
+exclude :test_object_prepend, "[ruby-core:53202] [Bug #8041](/Users/andrykonchin/projects/truffleruby-ws/truffleruby/test/mri/tests/ruby/marshaltestlib.rb:45:in `marshal_equal_with_ancestry')."
+exclude :test_singleton, "TypeError expected but nothing was raised."
+exclude :test_time_subclass, "ArgumentError: year too big to marshal: 10"
+exclude :test_regexp, "RegexpError: incompatible character encoding"
+exclude :test_range_cyclic, "ArgumentError: dump format error (unlinked)"
+exclude :test_regexp_subclass, "TypeError: no implicit conversion of Integer into String"
+exclude :test_range_subclass, "ArgumentError: wrong number of arguments (given 4, expected 2..3)"
