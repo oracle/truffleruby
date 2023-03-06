d9ad36e2226b8b8149bdf09da3e1891a894a28e2
moste00 <ubermenchun@gmail.com>
2023-03-06 21:56:07 +0200

    Add StructClass#keyword_init?

--- src/main/ruby/truffleruby/core/struct.rb
+++ src/main/ruby/truffleruby/core/struct.rb
@@ -33,7 +33,7 @@ class Struct
     alias_method :subclass_new, :new
   end
 
-  def self.new(klass_name, *attrs, keyword_init: false, &block)
+  def self.new(klass_name, *attrs, keyword_init: nil, &block)
     if klass_name
       if Primitive.object_kind_of?(klass_name, Symbol) # Truffle: added to avoid exception and match MRI
         attrs.unshift klass_name
@@ -92,7 +92,15 @@ class Struct
 
       const_set :STRUCT_ATTRS, attrs
       const_set :KEYWORD_INIT, keyword_init
-    end
+
+      def self.keyword_init?
+        kw_init = self.const_get(:KEYWORD_INIT)
+        return nil if Primitive.nil?(kw_init)
+
+        Primitive.as_boolean(kw_init)
+      end
+
+      end
 
     const_set klass_name, klass if klass_name
 
