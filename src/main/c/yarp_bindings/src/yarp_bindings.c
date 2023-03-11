cfad84097fb9fa76823a2ff1f889683de8b7ced5
Benoit Daloze <benoit.daloze@oracle.com>
2023-03-11 15:54:06 +0100

    Null-terminate until https://github.com/Shopify/yarp/issues/463 is fixed

--- src/main/c/yarp_bindings/src/yarp_bindings.c
+++ src/main/c/yarp_bindings/src/yarp_bindings.c
@@ -3,14 +3,17 @@
 
 JNIEXPORT jbyteArray JNICALL Java_org_yarp_Parser_parseAndSerialize(JNIEnv *env, jclass clazz, jbyteArray source) {
   jsize size = (*env)->GetArrayLength(env, source);
-  jbyte* bytes = (*env)->GetByteArrayElements(env, source, NULL);
+  // Null-terminate for safety, as parsers are prone to read further than the end
+  jbyte* bytes = malloc(size + 4);
+  (*env)->GetByteArrayRegion(env, source, 0, size, bytes);
+  memset(bytes + size, 0, 4);
 
   yp_buffer_t buffer;
   yp_buffer_init(&buffer);
 
   yp_parse_serialize((char*) bytes, size, &buffer);
 
-  (*env)->ReleaseByteArrayElements(env, source, bytes, JNI_ABORT);
+  free(bytes);
 
   jbyteArray serialized = (*env)->NewByteArray(env, buffer.length);
   (*env)->SetByteArrayRegion(env, serialized, 0, buffer.length, (jbyte*) buffer.value);
