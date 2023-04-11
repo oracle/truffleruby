aa5399030d8a5d9858ed2d6774654e676f50c97c
Benoit Daloze <benoit.daloze@oracle.com>
2023-04-11 19:25:52 +0200

    Import Shopify/yarp@8ce6ee101e6a873ff229067d02e198fcb60a2581

--- tool/import-yarp.sh
+++ tool/import-yarp.sh
@@ -9,6 +9,9 @@ bundle exec rake
 popd
 
 rm -rf src/main/c/yarp/src/yarp
+# Copy under src/yarp and not just src/ because we also have yarp_bindings.c
+mkdir src/main/c/yarp/src/yarp
+cp -R ../../yarp/include src/main/c/yarp/src/yarp
 cp -R ../../yarp/src src/main/c/yarp/src/yarp
 
 rm -rf src/yarp/java
