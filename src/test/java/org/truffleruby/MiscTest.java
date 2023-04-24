10d68c79f775ad9731dcd55dce3775676cdf2c3e
Benoit Daloze <benoit.daloze@oracle.com>
2023-04-24 18:16:20 +0200

    Use assertThrows() in MiscTest
    * There is no exception for the caller of Context#close(true)

--- src/test/java/org/truffleruby/MiscTest.java
+++ src/test/java/org/truffleruby/MiscTest.java
@@ -15,9 +15,7 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Value;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class MiscTest {
@@ -49,26 +47,18 @@ public class MiscTest {
         TestingThread thread = new TestingThread(() -> {
             try {
                 Thread.sleep(1000);
-                context.close(true);
             } catch (InterruptedException e) {
                 throw new Error(e);
-            } catch (PolyglotException e) {
-                if (e.isCancelled()) {
-                    assertTrue(e.isCancelled());
-                } else {
-                    throw e;
-                }
             }
+            context.close(true);
         });
 
         context.eval("ruby", "init = 1");
         thread.start();
         try {
             String maliciousCode = "while true; end";
-            context.eval("ruby", maliciousCode);
-            Assert.fail();
-        } catch (PolyglotException e) {
-            assertTrue(e.isCancelled());
+            RubyTest.assertThrows(() -> context.eval("ruby", maliciousCode),
+                    e -> assertTrue(e.isCancelled()));
         } finally {
             thread.join();
         }
