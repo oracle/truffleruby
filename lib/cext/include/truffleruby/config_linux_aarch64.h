4f685af13085a83daa467f2bc4ff747d35eee40a
Benoit Daloze <benoit.daloze@oracle.com>
2023-04-26 11:44:04 +0200

    Update config files for the new LLVM toolchain
    * Since the graal import now includes a newer LLVM toolchain

--- lib/cext/include/truffleruby/config_linux_aarch64.h
+++ lib/cext/include/truffleruby/config_linux_aarch64.h
@@ -165,8 +165,10 @@
 #define RUBY_ALIAS_FUNCTION_VOID(prot, name, args) RUBY_ALIAS_FUNCTION_TYPE(void, prot, name, args)
 #define HAVE_GCC_ATOMIC_BUILTINS 1
 #define HAVE_GCC_SYNC_BUILTINS 1
+#define UNREACHABLE __builtin_unreachable()
 #define RUBY_FUNC_EXPORTED __attribute__ ((__visibility__("default"))) extern
 #define RUBY_FUNC_NONNULL(n,x) __attribute__ ((__nonnull__(n))) x
+#define RUBY_FUNCTION_NAME_STRING __func__
 #define ENUM_OVER_INT 1
 #define HAVE_DECL_SYS_NERR 1
 #define HAVE_DECL_GETENV 1
