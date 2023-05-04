807a013e98311d6dd02f4ceebc68a875bb11a937
Benoit Daloze <benoit.daloze@oracle.com>
2023-05-04 14:53:58 +0200

    [GR-45708] Update graal to pick SVM fix for Assumption#ALWAYS_VALID

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "a81ecbe6def2084b669872f76207535ab62fd8d6",
+                "version": "a3e84712de5299897890a882227d540324ba2e93",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "a81ecbe6def2084b669872f76207535ab62fd8d6",
+                "version": "a3e84712de5299897890a882227d540324ba2e93",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
