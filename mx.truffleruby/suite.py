2568efe0fcc8b25b0cc419f30b39e8b1dd84cb74
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2023-06-02 08:47:04 +0200

    Update graal imports.

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "faff236da4ee3085721970fcd6de0d187e4fe611",
+                "version": "8e346652d0b2022a8416076c6e6c90a9acee7830",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "faff236da4ee3085721970fcd6de0d187e4fe611",
+                "version": "8e346652d0b2022a8416076c6e6c90a9acee7830",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
