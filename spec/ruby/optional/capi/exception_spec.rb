21ced619b05b24ec3fff138c51749da116645a53
Randy Stauner <randy.stauner@shopify.com>
2023-07-24 09:21:26 -0700

    Add a spec for c function rb_syserr_new_str

--- spec/ruby/optional/capi/exception_spec.rb
+++ spec/ruby/optional/capi/exception_spec.rb
@@ -100,6 +100,23 @@ describe "C-API Exception function" do
     end
   end
 
+  describe "rb_syserr_new_str" do
+    it "returns system error with nil message" do
+      const = Errno::ENOENT
+      exception = @s.rb_syserr_new_str(const::Errno, nil)
+      exception.message.should include("No such file or directory")
+      exception.class.should == Errno::ENOENT
+      exception.should.is_a?(SystemCallError)
+    end
+
+    it "returns system error with custom message" do
+      exception = @s.rb_syserr_new_str(Errno::ENOENT::Errno, "custom message")
+      exception.message.should include("custom message")
+      exception.class.should == Errno::ENOENT
+      exception.should.is_a?(SystemCallError)
+    end
+  end
+
   describe "rb_make_exception" do
     it "returns a RuntimeError when given a String argument" do
       e = @s.rb_make_exception(["Message"])
