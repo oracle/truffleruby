bd34bcd046c9a1ba7f60e9567f50e5a44a2ce728
Benoit Daloze <benoit.daloze@oracle.com>
2023-08-05 17:01:58 +0200

    Bundler 1 no longer works on Ruby 3.2
    * Specifically it fails due to:
    ruby-3.2.2/lib/ruby/gems/3.2.0/gems/bundler-1.17.3/lib/bundler/shared_helpers.rb:272:in `search_up': undefined method `untaint' for "truffleruby/test/truffle/gems/bundler-version/one":String (NoMethodError)
    
          current  = File.expand_path(SharedHelpers.pwd).untaint
                                                        ^^^^^^^^
    	from ruby-3.2.2/lib/ruby/gems/3.2.0/gems/bundler-1.17.3/lib/bundler/shared_helpers.rb:259:in `find_file'
    	from ruby-3.2.2/lib/ruby/gems/3.2.0/gems/bundler-1.17.3/lib/bundler/shared_helpers.rb:251:in `find_gemfile'
    	from ruby-3.2.2/lib/ruby/gems/3.2.0/gems/bundler-1.17.3/lib/bundler/shared_helpers.rb:60:in `in_bundle?'
    	from ruby-3.2.2/lib/ruby/gems/3.2.0/gems/bundler-1.17.3/lib/bundler/setup.rb:5:in `<top (required)>'

--- test/truffle/gems/bundler-version.sh
+++ test/truffle/gems/bundler-version.sh
@@ -2,24 +2,7 @@
 
 source test/truffle/common.sh.inc
 
-jt gem-test-pack
-gem_test_pack=$(jt gem-test-pack)
-
-mkdir -p temp-gem-home
-
-GEM_HOME=$PWD/temp-gem-home jt gem install --local "$gem_test_pack/gem-cache/bundler-1.17.3.gem"
-
-output=$(GEM_HOME=$PWD/temp-gem-home jt ruby -Ctest/truffle/gems/bundler-version/one bundler-version.rb)
-
-if [ "$output" = 'true' ]; then
-  echo Success
-else
-  echo Unexpected bundler 1 output
-  echo "$output"
-  exit 1
-fi
-
-output=$(GEM_HOME=$PWD/temp-gem-home jt ruby -Ctest/truffle/gems/bundler-version/two bundler-version.rb)
+output=$(jt ruby -Ctest/truffle/gems/bundler-version/two bundler-version.rb)
 
 if [ "$output" = 'true' ]; then
   echo Success
@@ -28,5 +11,3 @@ else
   echo "$output"
   exit 1
 fi
-
-rm -r temp-gem-home
--- test/truffle/gems/bundler-version/one/bundler-version.rb
+++
@@ -1,2 +0,0 @@
-require 'bundler/setup'
-p Bundler::VERSION == "1.17.3"
