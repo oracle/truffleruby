d35649d48f0e47cb00096e7525f8c4da1687650e
Imane Mamri <imane.mamri@oracle.com>
2023-08-22 12:37:07 +0100

    update graal import

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -20,7 +20,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "e7b40b1010e34bde2e64993ba8cece3833ec60e8",
+                "version": "ce7e2791bb11eee493e6c332efe73f589e289e5f",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -29,7 +29,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "e7b40b1010e34bde2e64993ba8cece3833ec60e8",
+                "version": "ce7e2791bb11eee493e6c332efe73f589e289e5f",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
