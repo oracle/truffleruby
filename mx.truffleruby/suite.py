d8548df4c6af8daea09b52efa1158be0c0ea59af
Benoit Daloze <benoit.daloze@oracle.com>
2023-08-30 12:34:10 +0200

    Update graal import to latest commit on release/graal-vm/23.1

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -20,7 +20,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "ce7e2791bb11eee493e6c332efe73f589e289e5f",
+                "version": "4bcb7c9a9061d0948609412048a6906fa0cb826c",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -29,7 +29,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "ce7e2791bb11eee493e6c332efe73f589e289e5f",
+                "version": "4bcb7c9a9061d0948609412048a6906fa0cb826c",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
