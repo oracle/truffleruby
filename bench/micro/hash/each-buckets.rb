2c653aeffd2d457d4e23a09fcd5449cbbfad2b72
Benoit Daloze <benoit.daloze@oracle.com>
2023-10-13 15:56:02 +0200

    Also keep the key in each-buckets.rb benchmark

--- bench/micro/hash/each-buckets.rb
+++ bench/micro/hash/each-buckets.rb
@@ -13,6 +13,6 @@
 if RUBY_ENGINE == 'truffleruby'
   hash = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10 }
   benchmark 'core-hash-each-buckets' do
-    hash.each { |k, v| Primitive.blackhole(v) }
+    hash.each { |k, v| Primitive.blackhole(k); Primitive.blackhole(v) }
   end
 end
