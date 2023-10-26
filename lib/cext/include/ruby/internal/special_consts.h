6c5fe8b8f31b418674b17e42b6cd35558de4e87d
Benoit Daloze <benoit.daloze@oracle.com>
2023-10-26 13:33:21 +0200

    Add a fast-path for RB_SPECIAL_CONST_P()

--- lib/cext/include/ruby/internal/special_consts.h
+++ lib/cext/include/ruby/internal/special_consts.h
@@ -33,7 +33,7 @@
 
 #ifdef TRUFFLERUBY
 RBIMPL_SYMBOL_EXPORT_BEGIN()
-bool rb_tr_special_const_p(VALUE object);
+bool rb_tr_special_const_symbol_p(VALUE object);
 RBIMPL_SYMBOL_EXPORT_END()
 #endif
 
@@ -339,7 +339,7 @@ static inline bool
 RB_SPECIAL_CONST_P(VALUE obj)
 {
 #ifdef TRUFFLERUBY
-    return rb_tr_special_const_p(obj);
+    return RB_IMMEDIATE_P(obj) || obj == RUBY_Qfalse || rb_tr_special_const_symbol_p(obj);
 #else
     return RB_IMMEDIATE_P(obj) || obj == RUBY_Qfalse;
 #endif
