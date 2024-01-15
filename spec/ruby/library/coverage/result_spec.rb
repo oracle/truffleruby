27ce948070f4a201ef976f44eb896ccd073cd9df
Andrew Konchin <andrii.konchyn@oracle.com>
2024-01-15 18:35:08 +0200

    Add missing specs for stop and clear options

--- spec/ruby/library/coverage/result_spec.rb
+++ spec/ruby/library/coverage/result_spec.rb
@@ -8,10 +8,16 @@ describe 'Coverage.result' do
     @eval_code_file = fixture __FILE__, 'eval_code.rb'
   end
 
+  before :each do
+    Coverage.running?.should == false
+  end
+
   after :each do
     $LOADED_FEATURES.delete(@class_file)
     $LOADED_FEATURES.delete(@config_file)
     $LOADED_FEATURES.delete(@eval_code_file)
+
+    Coverage.result if Coverage.running?
   end
 
   it 'gives the covered files as a hash with arrays of count or nil' do
@@ -157,5 +163,195 @@ describe 'Coverage.result' do
         }
       }
     end
+  end
+
+  it "disables coverage measurement when stop option is not specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result
+    Coverage.running?.should == false
+  end
+
+  it "disables coverage measurement when stop: true option is specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    -> {
+      Coverage.result(stop: true)
+    }.should complain /warning: stop implies clear/
+
+    Coverage.running?.should == false
+  end
+
+  it "does not disable coverage measurement when stop: false option is specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(stop: false)
+    Coverage.running?.should == true
+  end
+
+  it "does not disable coverage measurement when stop option is not specified but clear: true specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(clear: true)
+    Coverage.running?.should == true
+  end
+
+  it "does not disable coverage measurement when stop option is not specified but clear: false specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(clear: false)
+    Coverage.running?.should == true
+  end
+
+  it "disables coverage measurement when stop: true and clear: true specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(stop: true, clear: true)
+    Coverage.running?.should == false
+  end
+
+  it "disables coverage measurement when stop: true and clear: false specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    -> {
+      Coverage.result(stop: true, clear: false)
+    }.should complain /warning: stop implies clear/
+
+    Coverage.running?.should == false
+  end
+
+  it "does not disable coverage measurement when stop: false and clear: true specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(stop: false, clear: true)
+    Coverage.running?.should == true
+  end
+
+  it "does not disable coverage measurement when stop: false and clear: false specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(stop: false, clear: false)
+    Coverage.running?.should == true
+  end
+
+  it "resets counters (remove them) when stop: true specified but clear option is not specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    -> {
+      Coverage.result(stop: true) # clears counters
+    }.should complain /warning: stop implies clear/
+
+    Coverage.start
+    Coverage.peek_result.should == {}
+  end
+
+  it "resets counters (remove them) when stop: true and clear: true specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(stop: true, clear: true) # clears counters
+
+    Coverage.start
+    Coverage.peek_result.should == {}
+  end
+
+  it "resets counters (remove them) when stop: true and clear: false specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    -> {
+      Coverage.result(stop: true, clear: false) # clears counters
+    }.should complain /warning: stop implies clear/
+
+    Coverage.start
+    Coverage.peek_result.should == {}
+  end
+
+  it "resets counters (remove them) when both stop and clear options are not specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result # clears counters
+
+    Coverage.start
+    Coverage.peek_result.should == {}
+  end
+
+  it "clears counters (sets 0 values) when stop is not specified but clear: true specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(clear: true) # clears counters
+
+    Coverage.peek_result.should == {
+      @class_file => [
+        nil, nil, 0, nil, nil, 0, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil
+      ]
+    }
+  end
+
+  it "does not clear counters when stop is not specified but clear: false specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    result = Coverage.result(clear: false) # doesn't clear counters
+    result.should == {
+      @class_file => [
+        nil, nil, 1, nil, nil, 1, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil
+      ]
+    }
+
+    Coverage.peek_result.should == result
+  end
+
+  it "does not clear counters when stop: false and clear is not specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    result = Coverage.result(stop: false) # doesn't clear counters
+    result.should == {
+      @class_file => [
+        nil, nil, 1, nil, nil, 1, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil
+      ]
+    }
+
+    Coverage.peek_result.should == result
+  end
+
+  it "clears counters (sets 0 values) when stop: false and clear: true specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    Coverage.result(stop: false, clear: true) # clears counters
+
+    Coverage.peek_result.should == {
+      @class_file => [
+        nil, nil, 0, nil, nil, 0, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil
+      ]
+    }
+  end
+
+  it "does not clear counters when stop: false and clear: false specified" do
+    Coverage.start
+    require @class_file.chomp('.rb')
+
+    result = Coverage.result(stop: false, clear: false) # doesn't clear counters
+    result.should == {
+      @class_file => [
+        nil, nil, 1, nil, nil, 1, nil, nil, 0, nil, nil, nil, nil, nil, nil, nil
+      ]
+    }
+
+    Coverage.peek_result.should == result
   end
 end
