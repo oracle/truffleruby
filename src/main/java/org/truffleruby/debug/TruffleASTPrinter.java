de97f0ca710d1389ff2648e797f111662d88bed3
Benoit Daloze <benoit.daloze@oracle.com>
2024-02-02 12:17:21 +0100

    Include sourceCharIndex & sourceLength in TruffleASTPrinter

--- src/main/java/org/truffleruby/debug/TruffleASTPrinter.java
+++ src/main/java/org/truffleruby/debug/TruffleASTPrinter.java
@@ -47,9 +47,8 @@ import org.truffleruby.parser.BlockDescriptorInfo;
 public abstract class TruffleASTPrinter {
 
     // Skip the following node attributes:
-    // - "sourceCharIndex", "sourceLength" - are incorrect and will have correct values in YARP
     // - "RubyRootNode.bodyCopy" - is set if clone-uninitialized is forced
-    private static final Set<String> attributesToIgnore = Set.of("sourceCharIndex", "sourceLength", "bodyCopy");
+    private static final Set<String> attributesToIgnore = Set.of("bodyCopy");
 
     public static String dump(RubyRootNode rootNode, String focusedNodeClassName, int index) {
         final Node node;
