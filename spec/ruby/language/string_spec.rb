29f358e0559465c86cadd3efb430afe7de77b72a
Andrew Konchin <andrii.konchyn@oracle.com>
2024-04-01 15:11:19 +0300

    Update to ruby/spec@573cf97

--- spec/ruby/language/string_spec.rb
+++ spec/ruby/language/string_spec.rb
@@ -231,16 +231,14 @@ describe "Ruby String literals" do
       ruby_exe(fixture(__FILE__, "freeze_magic_comment_across_files.rb")).chomp.should == "true"
     end
 
-    guard -> { !eval("'test'").frozen? } do
+    guard -> { !(eval("'test'").frozen? && "test".equal?("test")) } do
       it "produces different objects for literals with the same content in different files if the other file doesn't have the comment and String literals aren't frozen by default" do
-        frozen_literals_by_default = eval("'test'").frozen?
         ruby_exe(fixture(__FILE__, "freeze_magic_comment_across_files_no_comment.rb")).chomp.should == "true"
       end
     end
 
-    guard -> { eval("'test'").frozen? } do
+    guard -> { eval("'test'").frozen? && "test".equal?("test") } do
       it "produces the same objects for literals with the same content in different files if the other file doesn't have the comment and String literals are frozen by default" do
-        frozen_literals_by_default = eval("'test'").frozen?
         ruby_exe(fixture(__FILE__, "freeze_magic_comment_across_files_no_comment.rb")).chomp.should == "false"
       end
     end
