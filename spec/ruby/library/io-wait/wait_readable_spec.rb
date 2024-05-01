a83830755820ddc9db2bd1f3693db3e6685d00a8
Andrew Konchin <andrii.konchyn@oracle.com>
2024-05-01 18:55:40 +0300

    Repair IO#wait_readable, IO#wait_writable, IO#wait to be interruptible
    * Fixes https://github.com/oracle/truffleruby/issues/3504

--- spec/ruby/library/io-wait/wait_readable_spec.rb
+++ spec/ruby/library/io-wait/wait_readable_spec.rb
@@ -24,4 +24,23 @@ describe "IO#wait_readable" do
   it "waits for the IO to become readable with the given large timeout" do
     @io.wait_readable(365 * 24 * 60 * 60).should == @io
   end
+
+  it "can be interrupted" do
+    rd, _wr = IO.pipe
+    start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+    t = Thread.new do
+      rd.wait_readable(10)
+    end
+
+    Thread.pass until t.stop?
+    t.kill
+    t.join
+
+    finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    (finish - start).should < 9
+  ensure
+    rd.close
+    _wr.close
+  end
 end
