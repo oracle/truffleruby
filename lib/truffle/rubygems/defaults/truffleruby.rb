321a7586f14168b4e5428a5eba7e807a7636eab8
なつき <i@ntk.me>
2024-05-06 12:25:15 -0700

    Add sass-embedded to REUSE_AS_BINARY_ON_TRUFFLERUBY

--- lib/truffle/rubygems/defaults/truffleruby.rb
+++ lib/truffle/rubygems/defaults/truffleruby.rb
@@ -30,5 +30,5 @@ end
 
 class Gem::Platform
   # The list of gems we want to install precompiled (using the local platform) on TruffleRuby
-  REUSE_AS_BINARY_ON_TRUFFLERUBY = %w[libv8 libv8-node sorbet-static]
+  REUSE_AS_BINARY_ON_TRUFFLERUBY = %w[libv8 libv8-node sass-embedded sorbet-static]
 end
