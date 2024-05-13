c6873dc97ac8becf6cbfbdf80918da12fc20925f
Andrew Konchin <andrii.konchyn@oracle.com>
2024-05-13 15:53:48 +0300

    Tag failing MRI tests

--- test/mri/excludes/Test_StrSetLen.rb
+++ test/mri/excludes/Test_StrSetLen.rb
@@ -1,3 +1,5 @@
-exclude :test_shared, "needs investigation"
-exclude :test_capacity_equals_to_new_size, "needs investigation"
+exclude :test_capacity_equals_to_new_size, "RangeError: cannot convert endless range to an array"
+exclude :test_coderange_after_append, "RangeError: cannot convert endless range to an array"
+exclude :test_coderange_after_trunc, "RangeError: cannot convert endless range to an array"
 exclude :test_non_shared, "RangeError: cannot convert endless range to an array"
+exclude :test_shared, "RangeError: cannot convert endless range to an array"
