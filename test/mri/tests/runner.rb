3eeae3d04e655ac2baafe3960829c9b15ad7f94f
Andrew Konchin <andrii.konchyn@oracle.com>
2024-10-08 15:47:39 +0300

    Adjust test files to use tool/test/init.rb and tool/test/runner.rb

--- test/mri/tests/runner.rb
+++ test/mri/tests/runner.rb
@@ -2,29 +2,4 @@
 
 # NOTE: Do not add any settings here for test-all. Instead, please add it to ../tool/test/init.rb.
 
-# TruffleRuby: we inline the logic of tool/test/runner.rb here because we do not currently import that file
-# require_relative '../tool/test/runner'
-require 'rbconfig'
-
-# TruffleRuby: adapt test lib path
-$LOAD_PATH.unshift File.expand_path("lib", __dir__)
-# $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-
-require 'test/unit'
-
-# require "profile_test_all" if ENV.key?('RUBY_TEST_ALL_PROFILE')
-# require "tracepointchecker"
-# require "zombie_hunter"
-# require "iseq_loader_checker"
-# require "gc_checker"
-# require_relative "../test-coverage.rb" if ENV.key?('COVERAGE')
-
-case $0
-when __FILE__
-  dir = __dir__
-when "-e"
-  # No default directory
-else
-  dir = File.expand_path("..", $0)
-end
-exit Test::Unit::AutoRunner.run(true, dir)
+require_relative 'tool/test/runner'
