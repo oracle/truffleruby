134473436b0a4026af8373511014797334a77e97
Andrew Konchin <andrii.konchyn@oracle.com>
2024-10-08 14:47:21 +0300

    Fix lib/cext/include/internal_all.h and add missing headers

--- lib/cext/include/internal_all.h
+++ lib/cext/include/internal_all.h
@@ -20,6 +20,7 @@
 
 // ls lib/cext/include/stubs/internal | ruby -e 'puts STDIN.readlines.map { |l| "#include <internal/#{l.chomp}>" }'
 #include <internal/array.h>
+#include <internal/encoding.h>
 #include <internal/file.h>
 #include <internal/gc.h>
 #include <internal/hash.h>
