24d702741742f3051a196a0db5e7398e6862622f
Andrew Konchin <andrii.konchyn@oracle.com>
2025-01-16 15:48:52 +0200

    Refactor specs for Dir#chdir

--- spec/ruby/core/dir/chdir_spec.rb
+++ spec/ruby/core/dir/chdir_spec.rb
@@ -95,10 +95,10 @@ describe "Dir.chdir" do
   end
 
   it "raises an Errno::ENOENT if the original directory no longer exists" do
-    dir1 = tmp('/testdir1')
-    dir2 = tmp('/testdir2')
-    File.should_not.exist?(dir1)
-    File.should_not.exist?(dir2)
+    dir1 = tmp('testdir1')
+    dir2 = tmp('testdir2')
+    Dir.should_not.exist?(dir1)
+    Dir.should_not.exist?(dir2)
     Dir.mkdir dir1
     Dir.mkdir dir2
     begin
@@ -108,8 +108,8 @@ describe "Dir.chdir" do
         end
       }.should raise_error(Errno::ENOENT)
     ensure
-      Dir.unlink dir1 if File.exist?(dir1)
-      Dir.unlink dir2 if File.exist?(dir2)
+      Dir.unlink dir1 if Dir.exist?(dir1)
+      Dir.unlink dir2 if Dir.exist?(dir2)
     end
   end
 
@@ -178,10 +178,10 @@ ruby_version_is '3.3' do
     end
 
     it "raises an Errno::ENOENT if the original directory no longer exists" do
-      dir_name1 = tmp('/testdir1')
-      dir_name2 = tmp('/testdir2')
-      File.should_not.exist?(dir_name1)
-      File.should_not.exist?(dir_name2)
+      dir_name1 = tmp('testdir1')
+      dir_name2 = tmp('testdir2')
+      Dir.should_not.exist?(dir_name1)
+      Dir.should_not.exist?(dir_name2)
       Dir.mkdir dir_name1
       Dir.mkdir dir_name2
 
@@ -194,8 +194,8 @@ ruby_version_is '3.3' do
           end
         }.should raise_error(Errno::ENOENT)
       ensure
-        Dir.unlink dir_name1 if File.exist?(dir_name1)
-        Dir.unlink dir_name2 if File.exist?(dir_name2)
+        Dir.unlink dir_name1 if Dir.exist?(dir_name1)
+        Dir.unlink dir_name2 if Dir.exist?(dir_name2)
       end
     ensure
       dir1.close
