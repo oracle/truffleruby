fa82827e33e760248e348ffc03c6300e3e7478e5
Andrew Konchin <andrii.konchyn@oracle.com>
2025-02-07 15:37:21 +0200

    Exclude MRI tests that constantly fail on GitHub Actions

--- test/mri/excludes/TestFiddle.rb
+++ test/mri/excludes/TestFiddle.rb
@@ -1,2 +1,3 @@
 exclude :test_dlopen_linker_script_group_linux, "NotImplementedError: NotImplementedError" if RUBY_PLATFORM.include?('linux')
+exclude :test_dlopen_linker_script_input_linux, "retain-on-retag(fails on GitHub Actions only): NotImplementedError: NotImplementedError"
 exclude :test_nil_true_etc, "NameError: uninitialized constant Fiddle::Qtrue"
