a736e04574b79fda4c7a134ffb719acd75bfb577
Andrew Konchin <andrii.konchyn@oracle.com>
2025-05-13 19:59:52 +0300

    Exclude TestIRB::LsTest#test_ls_grep

---
+++ test/mri/excludes/TestIRB/LsTest.rb
@@ -0,0 +1,1 @@
+exclude :test_ls_grep, "Expected /times/ to match \"/b/b/e/main/mxbuild/truffleruby-jvm/lib/truffle/truffle/cext.rb:1315:in `rb_exc_raise': Resource temporarily unavailable (Errno::EAGAIN)\\n\" +"
