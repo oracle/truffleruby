0df2e60242d6ddfc5b870d24700ba3c71844ec12
Benoit Daloze <benoit.daloze@oracle.com>
2025-05-29 13:57:58 +0200

    Update to latest graal

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -22,7 +22,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "ed067f1f7c575a638991d34bcab15f895a4b26b7",
+                "version": "244745f9c31b747826fc7ca75f2b4c1732bbf215",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -31,7 +31,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "ed067f1f7c575a638991d34bcab15f895a4b26b7",
+                "version": "244745f9c31b747826fc7ca75f2b4c1732bbf215",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
